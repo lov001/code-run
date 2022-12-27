998f88f09809166cf3cfaae374423c3f48d1e685
Lov Verma <lov.verma@greyamp.com>
2022-12-27 09:37:32 +0530

    Added unit tests for moving zeroes problem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
-import java.util.Arrays;
-import leetcode.problem283.MoveZeroesToEnd;
-
 public class Main {
 
     public static void main(String[] args) {
-        MoveZeroesToEnd object = new MoveZeroesToEnd();
-        int[] input = {0, 1, 0, 3, 12};
-        System.out.println("Output: -> " + Arrays.toString(object.moveZeroes(input)));
+        System.out.println("Welcome to Leet Code Problems......");
     }
 }
---
+++ src/test/java/leetcode/problem283/MoveZeroesToEndTest.java
@@ -0,0 +1,47 @@
+package leetcode.problem283;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+
+public class MoveZeroesToEndTest {
+
+    MoveZeroesToEnd classObj = new MoveZeroesToEnd();
+
+    @Test
+    void test_moveZeroesToEnd_expect0_when0() {
+        int[] input = {0};
+        int[] output = {0};
+        assertEquals(Arrays.toString(output), Arrays.toString(classObj.moveZeroes(input)));
+    }
+
+    @Test
+    void test_moveZeroesToEnd_expect1_when1() {
+        int[] input = {1};
+        int[] output = {1};
+        assertEquals(Arrays.toString(output), Arrays.toString(classObj.moveZeroes(input)));
+    }
+
+    @Test
+    void test_moveZeroesToEnd_expect10_when01() {
+        int[] input = {0, 1};
+        int[] output = {1, 0};
+        assertEquals(Arrays.toString(output), Arrays.toString(classObj.moveZeroes(input)));
+    }
+
+    @Test
+    void test_moveZeroesToEnd_expect10_when10() {
+        int[] input = {1, 0};
+        int[] output = {1, 0};
+        assertEquals(Arrays.toString(output), Arrays.toString(classObj.moveZeroes(input)));
+    }
+
+    @Test
+    void test_moveZeroesToEnd_whenRandomInput() {
+        int[] input = {0, 1, 0, 3, 12};
+        int[] output = {1, 3, 12, 0, 0};
+        assertEquals(Arrays.toString(output), Arrays.toString(classObj.moveZeroes(input)));
+    }
+}
