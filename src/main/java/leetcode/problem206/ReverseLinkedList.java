6e312db4e0583aaecb4b67262439cbd4646a9edc
Lov Verma <lov.verma@greyamp.com>
2023-01-09 22:35:12 +0530

    [PROBLEM-206]: Reversing a Linked List

---
+++ src/main/java/leetcode/problem206/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+package leetcode.problem206;
+
+public class ReverseLinkedList {
+
+    public ListNode reversedList(ListNode head) {
+        ListNode prev = null;
+        ListNode q;
+        ListNode current = head;
+        while (current != null) {
+            q = current.next;
+            current.next = prev;
+            prev = current;
+            current = q;
+        }
+        head = prev;
+        return head;
+    }
+}
