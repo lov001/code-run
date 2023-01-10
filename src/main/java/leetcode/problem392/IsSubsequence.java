9fe7e77dbffc799fd30ca3842924c51cdbaa729a
Lov Verma <lov.verma@greyamp.com>
2023-01-10 08:23:47 +0530

    [PROBLEM-392]: IsSubsequence problem

---
+++ src/main/java/leetcode/problem392/IsSubsequence.java
@@ -0,0 +1,24 @@
+package leetcode.problem392;
+
+public class IsSubsequence {
+
+    public boolean isSubsequence(String s, String t) {
+        int count = 0;
+        if (s.length() == 0 && t.length() == 0) {
+            return true;
+        } else if (s.length() > t.length()) {
+            return false;
+        } else if (s.length() == 0) {
+            return true;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(count) == t.charAt(i)) {
+                count++;
+            }
+            if (count == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
