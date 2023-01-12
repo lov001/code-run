c121da4d1c556600e9fc8864d7195f94a08193d7
Lov Verma <lov.verma@greyamp.com>
2023-01-12 16:43:07 +0530

    [PROBLEM-142]: Circular linked list 2 - solved

--- src/main/java/leetcode/problem142/CircularLinkedList.java
+++ src/main/java/leetcode/problem142/CircularLinkedList.java
@@ -6,18 +6,18 @@ public class CircularLinkedList {
         if (head == null || head.next == null) {
             return null;
         }
-        ListNode slow = head, fast = head.next;
-        while (slow != null && slow != fast) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-        }
-        if (slow == null) {
-            return null;
-        } else {
-            while (head.next != slow) {
-                head = head.next;
+            if (slow == fast) {
+                while (head != slow) {
+                    slow = slow.next;
+                    head = head.next;
+                }
+                return slow;
             }
-            return head;
         }
+        return null;
     }
 }
