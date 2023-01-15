f8be5708b653af8e086376d44e412fec3b65647d
Lov Verma <lov.verma@greyamp.com>
2023-01-15 13:29:06 +0530

    [PROBLEM-409]: Greedy Approach as per leetcode

--- src/main/java/leetcode/problem409/LongestPalindrome.java
+++ src/main/java/leetcode/problem409/LongestPalindrome.java
@@ -1,33 +1,19 @@
 package leetcode.problem409;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
 public class LongestPalindrome {
 
     public int longestPalindrome(String s) {
-        Map<Character, Integer> characterMap = new HashMap<>();
-        int evenCount = 0, oddCount = 0, uniqueOddCount = 0;
+        int[] characterMap = new int[128];
+        int result = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (characterMap.get(s.charAt(i)) == null) {
-                characterMap.put(s.charAt(i), 1);
-            } else {
-                int count = characterMap.get(s.charAt(i));
-                characterMap.put(s.charAt(i), count + 1);
+            characterMap[s.charAt(i)]++;
+        }
+        for (int value : characterMap) {
+            result += value / 2 * 2;
+            if (result % 2 == 0 && value % 2 == 1) {
+                result++;
             }
         }
-
-        for (Entry<Character, Integer> entry : characterMap.entrySet()) {
-            if (entry.getValue() % 2 == 0) {
-                evenCount += entry.getValue();
-            } else {
-                oddCount += entry.getValue();
-                uniqueOddCount++;
-            }
-        }
-        return oddCount >= 1 ? (characterMap.size() == 1 ? oddCount :
-            evenCount + (uniqueOddCount % 2 == 0 ? 1 : uniqueOddCount))
-            : (evenCount > 0 ? evenCount : 1);
+        return result;
     }
 }
