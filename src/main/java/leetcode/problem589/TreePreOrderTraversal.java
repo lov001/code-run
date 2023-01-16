0de3a3a9b29ff0ff4f7e680b94435f7810750a8d
Lov Verma <lov.verma@greyamp.com>
2023-01-16 21:23:25 +0530

    [PROBLEM-589]: WIP- To be picked later Pre order traversal

---
+++ src/main/java/leetcode/problem589/TreePreOrderTraversal.java
@@ -0,0 +1,12 @@
+package leetcode.problem589;
+
+import domain.Node;
+import java.util.List;
+
+public class TreePreOrderTraversal {
+
+    public List<Integer> preorder(Node root) {
+        return null;
+    }
+
+}
