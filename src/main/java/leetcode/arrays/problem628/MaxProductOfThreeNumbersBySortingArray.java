fc1eba3bb60aaaefb4ba8ae8e28ca57f8993dd6b
Lov Verma <lov.verma@greyamp.com>
2023-01-16 23:25:46 +0530

    [PROBLEM-628]: approach 2- Sorting the input array

---
+++ src/main/java/leetcode/arrays/problem628/MaxProductOfThreeNumbersBySortingArray.java
@@ -0,0 +1,13 @@
+package leetcode.arrays.problem628;
+
+import java.util.Arrays;
+
+public class MaxProductOfThreeNumbersBySortingArray {
+
+    public int maximumProduct(int[] nums) {
+        int length = nums.length;
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[length - 1],
+            nums[length - 1] * nums[length - 2] * nums[length - 3]);
+    }
+}
