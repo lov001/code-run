16d63bfde53521b29b8aa0f51884b48aad38094a
Lov Verma <lov.verma@greyamp.com>
2023-01-17 10:35:02 +0530

    [PROBLEM-2053]: WIP Kth Distinct String - LinkedHashSet Approach

--- src/main/java/leetcode/arrays/problem2053/KthDistinctString.java
+++ src/main/java/leetcode/arrays/problem2053/KthDistinctString.java
@@ -1,8 +1,27 @@
 package leetcode.arrays.problem2053;
 
+import java.util.LinkedHashSet;
+
 public class KthDistinctString {
 
     public String kthDistinct(String[] arr, int k) {
+        LinkedHashSet<String> stringSet = new LinkedHashSet<>();
+        for (String str : arr) {
+            if (stringSet.contains(str)) {
+                stringSet.remove(str);
+            } else {
+                stringSet.add(str);
+            }
+        }
+        if (stringSet.size() >= k) {
+            int count = 0;
+            for (String itr : stringSet) {
+                ++count;
+                if (count == k) {
+                    return itr;
+                }
+            }
+        }
         return "";
     }
 }
