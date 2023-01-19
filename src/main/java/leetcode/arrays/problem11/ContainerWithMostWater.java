c569df2ae0d7bcc6793f1b75bbc9182948252219
Lov Verma <lov.verma@greyamp.com>
2023-01-19 17:24:47 +0530

    [PROBLEM-11]: Completed - Container with most water

--- src/main/java/leetcode/arrays/problem11/ContainerWithMostWater.java
+++ src/main/java/leetcode/arrays/problem11/ContainerWithMostWater.java
@@ -1,7 +1,21 @@
 package leetcode.arrays.problem11;
 
 public class ContainerWithMostWater {
+
     public int maxArea(int[] height) {
-        return 0;
+        int maxArea = Integer.MIN_VALUE;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            int area = (j - i) * Math.min(height[i], height[j]);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+            if (area > maxArea) {
+                maxArea = area;
+            }
+        }
+        return maxArea;
     }
 }
