e2b572a78731e189a309058b3f31c49489b63ce0
Lov Verma <lov.verma@greyamp.com>
2023-01-20 10:38:42 +0530

    [PROBLEM-15]: Complete solution

--- src/main/java/leetcode/arrays/problem15/ThreeSum.java
+++ src/main/java/leetcode/arrays/problem15/ThreeSum.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class ThreeSum {
 
-    public List<List<Integer>> threeSum(int[] nums) {
+    /*public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -14,6 +14,32 @@ public class ThreeSum {
                     if (nums[i] + nums[j] + nums[k] == 0) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     }
+                }
+            }
+        }
+        return result;
+    }*/
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    ++j;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        ++j;
+                    }
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    ++j;
+                } else {
+                    --k;
                 }
             }
         }
