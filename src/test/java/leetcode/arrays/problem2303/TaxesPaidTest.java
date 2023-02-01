f909c1fb74b9642d345adf030951aec84a3c1fb3
Lov Verma <lov.verma@greyamp.com>
2023-02-01 22:56:49 +0530

    [PROBLEM-2303]: WIP tests

--- src/test/java/leetcode/arrays/problem2303/TaxesPaidTest.java
+++ src/test/java/leetcode/arrays/problem2303/TaxesPaidTest.java
@@ -8,9 +8,23 @@ public class TaxesPaidTest {
     TaxesPaid classObj = new TaxesPaid();
 
     @Test
-    void test_calculateTax_expect_when() {
+    void test_calculateTax_expectTwoPointSixFive_whenIncomeIs10() {
         int[][] brackets = {{3, 50}, {7, 10}, {12, 25}};
         int income = 10;
         Assertions.assertEquals(2.65, classObj.calculateTax(brackets, income));
+    }
+
+    @Test
+    void test_calculateTax_expectZeroPointTwoFive_whenIncomeIs2() {
+        int[][] brackets = {{1, 0}, {4, 25}, {5, 50}};
+        int income = 2;
+        Assertions.assertEquals(0.25, classObj.calculateTax(brackets, income));
+    }
+
+    @Test
+    void test_calculateTax_expectZeroPointZero_whenIncomeIs0() {
+        int[][] brackets = {{2, 50}};
+        int income = 0;
+        Assertions.assertEquals(0.00, classObj.calculateTax(brackets, income));
     }
 }
