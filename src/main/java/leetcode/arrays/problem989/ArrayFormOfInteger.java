3b9cfa5487696ebc94fdd1236a769186eb7d35c7
Lov Verma <lov.verma@greyamp.com>
2023-02-15 23:39:29 +0530

    [PROBLEM-989]: Brute force approach

--- src/main/java/leetcode/arrays/problem989/ArrayFormOfInteger.java
+++ src/main/java/leetcode/arrays/problem989/ArrayFormOfInteger.java
@@ -1,11 +1,29 @@
 package leetcode.arrays.problem989;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ArrayFormOfInteger {
 
     public List<Integer> addToArrayForm(int[] num, int k) {
-        return Collections.emptyList();
+        int end = num.length - 1;
+        List<Integer> output = new ArrayList<>();
+        int carryForward = 0;
+        while (k != 0) {
+            int remainder = k % 10;
+            int sum = num[end--] + remainder + carryForward;
+            output.add(sum > 9 ? sum % 10 : sum);
+            carryForward = sum > 9 ? 1 : 0;
+            k = k / 10;
+        }
+        while (end >= 0) {
+            output.add(num[end--]);
+        }
+        if (carryForward == 1) {
+            output.add(1);
+        }
+        Collections.reverse(output);
+        return output;
     }
 }
--- src/test/java/leetcode/arrays/problem989/ArrayFormOfIntegerTest.java
+++ src/test/java/leetcode/arrays/problem989/ArrayFormOfIntegerTest.java
@@ -11,18 +11,18 @@ public class ArrayFormOfIntegerTest {
     @Test
     void test_addToArrayForm_expect1234_whenKIs34() {
         int[] input = {1, 2, 0, 0};
-        Assertions.assertEquals(classObj.addToArrayForm(input, 34), Arrays.asList(1, 2, 3, 4));
+        Assertions.assertEquals(Arrays.asList(1, 2, 3, 4), classObj.addToArrayForm(input, 34));
     }
 
     @Test
     void test_addToArrayForm_expect455_whenKIs181() {
         int[] input = {2, 7, 4};
-        Assertions.assertEquals(classObj.addToArrayForm(input, 181), Arrays.asList(4, 5, 5));
+        Assertions.assertEquals(Arrays.asList(4, 5, 5), classObj.addToArrayForm(input, 181));
     }
 
     @Test
     void test_addToArrayForm_expect1021_whenKIs806() {
         int[] input = {2, 1, 5};
-        Assertions.assertEquals(classObj.addToArrayForm(input, 806), Arrays.asList(1, 0, 2, 1));
+        Assertions.assertEquals(Arrays.asList(1, 0, 2, 1), classObj.addToArrayForm(input, 806));
     }
 }
