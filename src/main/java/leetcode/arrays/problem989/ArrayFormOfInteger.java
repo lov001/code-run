c6c464368596e054db8eb82a17e48dd83f5c5104
Lov Verma <lov.verma@greyamp.com>
2023-02-15 23:25:55 +0530

    [PROBLEM-989]: WIP array from integer

---
+++ src/main/java/leetcode/arrays/problem989/ArrayFormOfInteger.java
@@ -0,0 +1,11 @@
+package leetcode.arrays.problem989;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayFormOfInteger {
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        return Collections.emptyList();
+    }
+}
