96fa235b6d874f8756e11be186a869325cc1b646
Lov Verma <lov.verma@greyamp.com>
2023-02-17 19:49:09 +0530

    [PROBLEM-995]: WIP for k-consecutive bit flips

---
+++ src/main/java/leetcode/arrays/problem995/ConsecutiveBitFlips.java
@@ -0,0 +1,9 @@
+package leetcode.arrays.problem995;
+
+public class ConsecutiveBitFlips {
+
+    public int minKBitFlips(int[] nums, int k) {
+        int bitFlips = -1;
+        return bitFlips;
+    }
+}
