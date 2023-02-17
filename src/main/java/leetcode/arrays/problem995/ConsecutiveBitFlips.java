65c5e5557f9e0fc82295750c898e782f2b741b5b
Lov Verma <lov.verma@greyamp.com>
2023-02-17 23:03:34 +0530

    [PROBLEM-995]: online solution

--- src/main/java/leetcode/arrays/problem995/ConsecutiveBitFlips.java
+++ src/main/java/leetcode/arrays/problem995/ConsecutiveBitFlips.java
@@ -3,7 +3,26 @@ package leetcode.arrays.problem995;
 public class ConsecutiveBitFlips {
 
     public int minKBitFlips(int[] nums, int k) {
-        int bitFlips = -1;
-        return bitFlips;
+        int n = nums.length;
+        int[] flipped = new int[n];
+        int flipCount = 0;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i >= k) {
+                flipCount -= flipped[i - k];
+            }
+            if ((nums[i] + flipCount) % 2 == 0) {
+                if (i + k > n) {
+                    return -1;
+                }
+                flipped[i] = 1;
+                flipCount++;
+                result++;
+            }
+        }
+
+        return result;
+
     }
 }
