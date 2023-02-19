9c7b558e5293fa8089df5b6d9add940337f09f9f
Lov Verma <lov.verma@greyamp.com>
2023-02-19 20:43:22 +0530

    [PROBLEM-57]: WIP

--- src/main/java/leetcode/arrays/problem57/InsertInterval.java
+++ src/main/java/leetcode/arrays/problem57/InsertInterval.java
@@ -3,8 +3,21 @@ package leetcode.arrays.problem57;
 public class InsertInterval {
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int[][] output = new int[intervals.length][intervals.length];
-
+        int length = intervals.length;
+        int[][] output = new int[length][length];
+        int firstIndex = -1, lastIndex = -1, i=0;
+        for (i = 0; i < length - 2; i++) {
+            if (intervals[i][0] < newInterval[i] && newInterval[i] <= intervals[i + 1][0]) {
+                firstIndex = i;
+                break;
+            }
+        }
+        for (int j=i; j < length - 2; j++) {
+            if (intervals[i][0] < newInterval[i] && newInterval[i] <= intervals[i + 1][0]) {
+                firstIndex = i;
+                break;
+            }
+        }
         return output;
     }
 }
