d29c355b498387749119c749954b698bab69857b
Lov Verma <lov.verma@greyamp.com>
2023-02-21 22:05:25 +0530

    [PROBLEM-1790]: optimal solution

--- src/main/java/leetcode/strings/problem1790/SingleSwap.java
+++ src/main/java/leetcode/strings/problem1790/SingleSwap.java
@@ -1,9 +1,5 @@
 package leetcode.strings.problem1790;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
 public class SingleSwap {
 
     public boolean areAlmostEqual(String s1, String s2) {
@@ -12,20 +8,21 @@ public class SingleSwap {
         } else if (s1.length() == 1) {
             return s1.charAt(0) == s2.charAt(0);
         }
-        List<Character> list = new ArrayList<>();
-        Stack<Character> stack = new Stack<>();
+        int[] differences = new int[2];
         int count = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                list.add(s1.charAt(i));
-                stack.push(s2.charAt(i));
-                count++;
+                if (count == 2) {
+                    return false;
+                }
+                differences[count++] = i;
             }
         }
         if (count == 0) {
             return true;
         } else if (count == 2) {
-            return list.get(0).equals(stack.pop()) && list.get(1).equals(stack.pop());
+            return s1.charAt(differences[0]) == s2.charAt(differences[1]) &&
+                s1.charAt(differences[1]) == s2.charAt(differences[0]);
         }
         return false;
     }
