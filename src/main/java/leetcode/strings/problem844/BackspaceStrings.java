c6d67d6c619a79a526c19f915953b071a564c121
Lov Verma <lov.verma@greyamp.com>
2023-02-22 20:44:23 +0530

    [PROBLEM-844]: Working solution using stack

--- src/main/java/leetcode/strings/problem844/BackspaceStrings.java
+++ src/main/java/leetcode/strings/problem844/BackspaceStrings.java
@@ -1,8 +1,29 @@
 package leetcode.strings.problem844;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class BackspaceStrings {
 
     public boolean backspaceCompare(String s, String t) {
-        return true;
+        Stack<Character> stack1 = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '#') {
+                stack1.push(s.charAt(i));
+            } else {
+                stack1.pop();
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) != '#') {
+                stack2.push(t.charAt(i));
+            } else {
+                stack2.pop();
+            }
+        }
+        System.out.println("Stack 1 : " + Arrays.toString(stack1.toArray()));
+        System.out.println("Stack 2 : " + Arrays.toString(stack2.toArray()));
+        return Arrays.toString(stack1.toArray()).equals(Arrays.toString(stack2.toArray()));
     }
 }
