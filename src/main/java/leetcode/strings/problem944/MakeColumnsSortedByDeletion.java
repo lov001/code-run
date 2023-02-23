f10ace72ac19b6172bc0de575913ebbcb4d40005
Lov Verma <lov.verma@greyamp.com>
2023-02-23 20:13:49 +0530

    [PROBLEM-944]: working solution

--- src/main/java/leetcode/strings/problem944/MakeColumnsSortedByDeletion.java
+++ src/main/java/leetcode/strings/problem944/MakeColumnsSortedByDeletion.java
@@ -4,6 +4,15 @@ public class MakeColumnsSortedByDeletion {
 
     public int minDeletionSize(String[] strs) {
         int count = 0;
-        return 0;
+        int strLength = strs[0].length();
+        for (int i = 0; i < strLength; i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
