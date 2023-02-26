4eee2801268304570af05dc30dc3160daeea2a45
Lov Verma <lov.verma@greyamp.com>
2023-02-26 19:49:36 +0530

    [PROBLEM-20]: solution with basic tests passing

--- src/main/java/leetcode/strings/problem20/ValidParentheses.java
+++ src/main/java/leetcode/strings/problem20/ValidParentheses.java
@@ -1,8 +1,22 @@
 package leetcode.strings.problem20;
 
+import java.util.Stack;
+
 public class ValidParentheses {
 
-    public boolean isValid(String s) {
-        return false;
+    public boolean isValid(String inputString) {
+        Stack<Character> inputStack = new Stack<>();
+        for (char ch : inputString.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                inputStack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                char poppedChar = inputStack.pop();
+                if (!((poppedChar == '(' && ch == ')') || (poppedChar == '{' && ch == '}')
+                    || (poppedChar == '[' && ch == ']'))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
