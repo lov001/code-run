02a656c54322e14ed7013819ed4587c381e74c88
Lov Verma <lov.verma@greyamp.com>
2023-02-26 20:06:40 +0530

    [PROBLEM-20]: approach with string builder

--- src/main/java/leetcode/strings/problem20/ValidParentheses.java
+++ src/main/java/leetcode/strings/problem20/ValidParentheses.java
@@ -5,18 +5,23 @@ import java.util.Stack;
 public class ValidParentheses {
 
     public boolean isValid(String inputString) {
+        if (inputString.length() % 2 != 0) {
+            return false;
+        }
         Stack<Character> inputStack = new Stack<>();
         for (char ch : inputString.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 inputStack.push(ch);
-            } else if (ch == ')' || ch == '}' || ch == ']') {
+            } else if (!inputStack.isEmpty() && (ch == ')' || ch == '}' || ch == ']')) {
                 char poppedChar = inputStack.pop();
                 if (!((poppedChar == '(' && ch == ')') || (poppedChar == '{' && ch == '}')
                     || (poppedChar == '[' && ch == ']'))) {
                     return false;
                 }
+            } else {
+                inputStack.push(ch);
             }
         }
-        return true;
+        return inputStack.isEmpty();
     }
 }
