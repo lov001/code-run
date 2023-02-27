c3d34a380a6e48d53c806f7d792a7fb89a40b35d
Lov Verma <lov.verma@greyamp.com>
2023-02-27 17:06:37 +0530

    [PROBLEM-168]: working solution

--- src/main/java/leetcode/strings/problem168/ExcelSheetColumnTile.java
+++ src/main/java/leetcode/strings/problem168/ExcelSheetColumnTile.java
@@ -3,6 +3,16 @@ package leetcode.strings.problem168;
 public class ExcelSheetColumnTile {
 
     public String convertToTitle(int columnNumber) {
-        return "";
+        if (columnNumber <= 26) {
+            char ch = (char) (columnNumber + 64);
+            return String.valueOf(ch);
+        }
+        StringBuilder string = new StringBuilder();
+        while (columnNumber != 0) {
+            int remainder = columnNumber % 26;
+            string.append((char) ((remainder == 0 ? 26 : remainder) + 64));
+            columnNumber = --columnNumber / 26;
+        }
+        return string.reverse().toString();
     }
 }
