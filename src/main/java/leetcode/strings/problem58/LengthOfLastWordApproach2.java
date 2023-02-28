df1a049ac845aa4b2bbfabba7fbcb128d8445ee9
Lov Verma <lov.verma@greyamp.com>
2023-02-28 20:46:27 +0530

    [PROBLEM-58]: another approach with single loop

---
+++ src/main/java/leetcode/strings/problem58/LengthOfLastWordApproach2.java
@@ -0,0 +1,18 @@
+package leetcode.strings.problem58;
+
+public class LengthOfLastWordApproach2 {
+
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                if (length > 0) {
+                    return length;
+                }
+            }
+        }
+        return length;
+    }
+}
--- src/test/java/leetcode/strings/problem58/LengthOfLastWordTest.java
+++ src/test/java/leetcode/strings/problem58/LengthOfLastWordTest.java
@@ -6,40 +6,47 @@ import org.junit.jupiter.api.Test;
 public class LengthOfLastWordTest {
 
     LengthOfLastWord classObj = new LengthOfLastWord();
+    LengthOfLastWordApproach2 classObj2 = new LengthOfLastWordApproach2();
 
     @Test
     void test_lengthOfLastWord_expect5_whenLastWordIsWorld() {
         String input = "Hello World";
         Assertions.assertEquals(5, classObj.lengthOfLastWord(input));
+        Assertions.assertEquals(5, classObj2.lengthOfLastWord(input));
     }
 
     @Test
     void test_lengthOfLastWord_expect4_whenLastWordIsMoon() {
         String input = "   fly me   to   the moon  ";
         Assertions.assertEquals(4, classObj.lengthOfLastWord(input));
+        Assertions.assertEquals(4, classObj2.lengthOfLastWord(input));
     }
 
     @Test
     void test_lengthOfLastWord_expect6_whenLastWordIsJoyboy() {
         String input = "luffy is still joyboy";
         Assertions.assertEquals(6, classObj.lengthOfLastWord(input));
+        Assertions.assertEquals(6, classObj2.lengthOfLastWord(input));
     }
 
     @Test
     void test_lengthOfLastWord_expect1_whenLastWordIsA() {
         String input = "a";
         Assertions.assertEquals(1, classObj.lengthOfLastWord(input));
+        Assertions.assertEquals(1, classObj2.lengthOfLastWord(input));
     }
 
     @Test
     void test_lengthOfLastWord_expect3_whenLastWordIsDay() {
         String input = "day";
         Assertions.assertEquals(3, classObj.lengthOfLastWord(input));
+        Assertions.assertEquals(3, classObj2.lengthOfLastWord(input));
     }
 
     @Test
     void test_lengthOfLastWord_expect1_whenLastWordIsA_() {
         String input = "a ";
         Assertions.assertEquals(1, classObj.lengthOfLastWord(input));
+        Assertions.assertEquals(1, classObj2.lengthOfLastWord(input));
     }
 }
