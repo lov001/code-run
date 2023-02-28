0a3c7bc2ff00a9fd3caa0b88d3a96619852c20c4
Lov Verma <lov.verma@greyamp.com>
2023-02-28 19:08:56 +0530

    [PROBLEM-14]: working solution with basic tests passing

--- src/main/java/leetcode/strings/problem14/LongestCommonPrefix.java
+++ src/main/java/leetcode/strings/problem14/LongestCommonPrefix.java
@@ -3,6 +3,26 @@ package leetcode.strings.problem14;
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
-        return "";
+        String longestCommonPrefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            longestCommonPrefix = commonPrefix(longestCommonPrefix, strs[i]);
+            if (longestCommonPrefix.length() == 0) {
+                return "";
+            }
+        }
+        return longestCommonPrefix;
+    }
+
+    private String commonPrefix(String prevLongestPrefix, String currentWord) {
+        StringBuilder prefix = new StringBuilder();
+        for (int i = 0, j = 0; i < prevLongestPrefix.length() && j < currentWord.length();
+            i++, j++) {
+            if (prevLongestPrefix.charAt(i) == currentWord.charAt(j)) {
+                prefix.append(prevLongestPrefix.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return prefix.toString();
     }
 }
