22a039c995763fb7bcdb81e1dc95541868eccab0
Lov Verma <lov.verma@greyamp.com>
2023-03-01 10:54:58 +0530

    [PROBLEM-125]: solution with basic tests passing

--- src/main/java/leetcode/strings/problem125/ValidPalindrome.java
+++ src/main/java/leetcode/strings/problem125/ValidPalindrome.java
@@ -3,7 +3,24 @@ package leetcode.strings.problem125;
 public class ValidPalindrome {
 
     public boolean isPalindrome(String s) {
-
+        int start = 0, end = s.length() - 1;
+        while (start < end) {
+            while (s.charAt(start) < 65 || (s.charAt(start) > 90 && s.charAt(start) < 97)
+                || s.charAt(start) > 122) {
+                start++;
+            }
+            while (s.charAt(end) < 65 || (s.charAt(end) > 90 && s.charAt(end) < 97)
+                || s.charAt(end) > 122) {
+                end--;
+            }
+            if (!(String.valueOf(s.charAt(start))
+                .equalsIgnoreCase(String.valueOf(s.charAt(end))))) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
         return true;
     }
 }
