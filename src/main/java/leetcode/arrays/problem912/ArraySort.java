6ade8ae9d8f2221eca8dc121884d4280759971d1
Lov Verma <lov.verma@greyamp.com>
2023-03-01 23:29:11 +0530

    [PROBLEM-912]: solution with quick sort

--- src/main/java/leetcode/arrays/problem912/ArraySort.java
+++ src/main/java/leetcode/arrays/problem912/ArraySort.java
@@ -1,17 +1,32 @@
 package leetcode.arrays.problem912;
 
+import static utils.SwapNumbersInArray.swap;
+
 public class ArraySort {
 
     public int[] sortArray(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length - 1; j++) {
-                if (nums[j] >= nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int pivot = partition(nums, left, right);
+            quickSort(nums, left, pivot - 1);
+            quickSort(nums, pivot + 1, right);
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
             }
         }
-        return nums;
+        swap(nums, i + 1, right);
+        return i + 1;
     }
 }
