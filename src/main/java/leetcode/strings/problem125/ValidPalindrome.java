41958cf7b1b96859006f8510044dd8bae73f8a21
Lov Verma <lov.verma@greyamp.com>
2023-03-01 11:46:40 +0530

    [PROBLEM-125]: solution with 462/485 test cases passing

--- src/main/java/leetcode/strings/problem125/ValidPalindrome.java
+++ src/main/java/leetcode/strings/problem125/ValidPalindrome.java
@@ -5,21 +5,18 @@ public class ValidPalindrome {
     public boolean isPalindrome(String s) {
         int start = 0, end = s.length() - 1;
         while (start < end) {
-            while (s.charAt(start) < 65 || (s.charAt(start) > 90 && s.charAt(start) < 97)
-                || s.charAt(start) > 122) {
+            while ((start < end) && !Character.isLetter(s.charAt(start))) {
                 start++;
             }
-            while (s.charAt(end) < 65 || (s.charAt(end) > 90 && s.charAt(end) < 97)
-                || s.charAt(end) > 122) {
+            while ((start < end) && !Character.isLetter(s.charAt(end))) {
                 end--;
             }
             if (!(String.valueOf(s.charAt(start))
                 .equalsIgnoreCase(String.valueOf(s.charAt(end))))) {
                 return false;
-            } else {
-                start++;
-                end--;
             }
+            start++;
+            end--;
         }
         return true;
     }
--- src/test/java/leetcode/strings/problem125/ValidPalindromeTest.java
+++ src/test/java/leetcode/strings/problem125/ValidPalindromeTest.java
@@ -24,4 +24,22 @@ public class ValidPalindromeTest {
         String input = " ";
         Assertions.assertTrue(classObj.isPalindrome(input));
     }
+
+    @Test
+    void test_isPalindrome_expectTrue_whenInputStringIsOnlySpecialCharacters() {
+        String input = ".,";
+        Assertions.assertTrue(classObj.isPalindrome(input));
+    }
+
+    @Test
+    void test_isPalindrome_expectTrue_whenInputStringIsADot() {
+        String input = "a.";
+        Assertions.assertTrue(classObj.isPalindrome(input));
+    }
+
+    @Test
+    void test_isPalindrome_expectFalse_whenInputStringIs0P() {
+        String input = "0P";
+        Assertions.assertFalse(classObj.isPalindrome(input));
+    }
 }
