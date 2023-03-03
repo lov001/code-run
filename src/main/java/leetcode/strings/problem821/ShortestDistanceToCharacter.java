f0fc9241e1fe0c196513997660d3ea9a530e6ffe
Lov Verma <lov.verma@greyamp.com>
2023-03-03 23:39:47 +0530

    [PROBLEM-821]: WIP - shortest distance to character

---
+++ src/main/java/leetcode/strings/problem821/ShortestDistanceToCharacter.java
@@ -0,0 +1,9 @@
+package leetcode.strings.problem821;
+
+public class ShortestDistanceToCharacter {
+
+    public int[] shortestToChar(String s, char c) {
+        int[] output = new int[s.length()];
+        return output;
+    }
+}
---
+++ src/test/java/leetcode/strings/problem821/ShortestDistanceToCharacterTest.java
@@ -0,0 +1,26 @@
+package leetcode.strings.problem821;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShortestDistanceToCharacterTest {
+
+    ShortestDistanceToCharacter classObj = new ShortestDistanceToCharacter();
+
+    @Test
+    void test_shortestToChar_expect_whenLetterIsE() {
+        String input = "loveleetcode";
+        int[] output = {3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0};
+        Assertions.assertEquals(Arrays.toString(output),
+            Arrays.toString(classObj.shortestToChar(input, 'e')));
+    }
+
+    @Test
+    void test_shortestToChar_expect_whenLetterIsB() {
+        String input = "aaab";
+        int[] output = {3, 2, 1, 0};
+        Assertions.assertEquals(Arrays.toString(output),
+            Arrays.toString(classObj.shortestToChar(input, 'b')));
+    }
+}
