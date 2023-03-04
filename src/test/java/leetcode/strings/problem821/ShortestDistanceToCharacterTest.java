445dd23394046b57cf69a61d9c6f0ba10fdc42d9
Lov Verma <lov.verma@greyamp.com>
2023-03-04 20:06:09 +0530

    [PROBLEM-821]: more cleaner approach

--- src/test/java/leetcode/strings/problem821/ShortestDistanceToCharacterTest.java
+++ src/test/java/leetcode/strings/problem821/ShortestDistanceToCharacterTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 public class ShortestDistanceToCharacterTest {
 
     ShortestDistanceToCharacter classObj = new ShortestDistanceToCharacter();
+    ShortestDistanceToCharacterApproach2 classObj2 = new ShortestDistanceToCharacterApproach2();
 
     @Test
     void test_shortestToChar_expect_whenLetterIsE() {
@@ -14,6 +15,8 @@ public class ShortestDistanceToCharacterTest {
         int[] output = {3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0};
         Assertions.assertEquals(Arrays.toString(output),
             Arrays.toString(classObj.shortestToChar(input, 'e')));
+        Assertions.assertEquals(Arrays.toString(output),
+            Arrays.toString(classObj2.shortestToChar(input, 'e')));
     }
 
     @Test
@@ -22,5 +25,7 @@ public class ShortestDistanceToCharacterTest {
         int[] output = {3, 2, 1, 0};
         Assertions.assertEquals(Arrays.toString(output),
             Arrays.toString(classObj.shortestToChar(input, 'b')));
+        Assertions.assertEquals(Arrays.toString(output),
+            Arrays.toString(classObj2.shortestToChar(input, 'b')));
     }
 }
