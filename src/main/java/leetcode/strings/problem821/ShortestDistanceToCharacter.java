f87e1bf656b6b7217e3f36e701c1d023c6c85e88
Lov Verma <lov.verma@greyamp.com>
2023-03-04 19:55:22 +0530

    [PROBLEM-821]: basic tests passing

--- src/main/java/leetcode/strings/problem821/ShortestDistanceToCharacter.java
+++ src/main/java/leetcode/strings/problem821/ShortestDistanceToCharacter.java
@@ -4,6 +4,24 @@ public class ShortestDistanceToCharacter {
 
     public int[] shortestToChar(String s, char c) {
         int[] output = new int[s.length()];
+        int[] indexArr = new int[s.length()];
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                indexArr[index++] = i;
+            }
+        }
+        int prev = -1, current = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (j == indexArr[current]) {
+                output[j] = 0;
+                prev = current;
+                current++;
+            } else {
+                output[j] = Math.min(Math.abs(indexArr[prev == -1 ? 0 : prev] - j),
+                    Math.abs(indexArr[current] - j));
+            }
+        }
         return output;
     }
 }
