f7d30c33567f351b1716687be63b5fcb8787fed3
Lov Verma <lov.verma@greyamp.com>
2023-03-05 15:20:20 +0530

    [PROBLEM-925]: WIP - Long pressed name

---
+++ src/main/java/leetcode/strings/problem925/LongPressedName.java
@@ -0,0 +1,9 @@
+package leetcode.strings.problem925;
+
+public class LongPressedName {
+
+    public boolean isLongPressedName(String name, String typed) {
+
+        return true;
+    }
+}
---
+++ src/test/java/leetcode/strings/problem925/LongPressedNameTest.java
@@ -0,0 +1,23 @@
+package leetcode.strings.problem925;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LongPressedNameTest {
+
+    LongPressedName classObj = new LongPressedName();
+
+    @Test
+    void test_isLongPressedName_expectTrue_whenLongPressedName() {
+        String input = "alex";
+        String typed = "aaleex";
+        Assertions.assertTrue(classObj.isLongPressedName(input, typed));
+    }
+
+    @Test
+    void test_isLongPressedName_expectFalse_whenNotLongPressedName() {
+        String input = "saeed";
+        String typed = "ssaaedd";
+        Assertions.assertFalse(classObj.isLongPressedName(input, typed));
+    }
+}
