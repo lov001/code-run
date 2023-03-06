315ee2aaf233b54813d57adf4bd9661e400cfc68
Lov Verma <lov.verma@greyamp.com>
2023-03-06 11:10:02 +0530

    [PROBLEM-1539]: WIP - Kth Missing number

---
+++ src/main/java/leetcode/arrays/problem1539/KthMissingNumber.java
@@ -0,0 +1,8 @@
+package leetcode.arrays.problem1539;
+
+public class KthMissingNumber {
+
+    public int findKthPositive(int[] arr, int k) {
+        return 0;
+    }
+}
---
+++ src/test/java/leetcode/arrays/problem1539/KthMissingNumberTest.java
@@ -0,0 +1,21 @@
+package leetcode.arrays.problem1539;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class KthMissingNumberTest {
+
+    KthMissingNumber classObj = new KthMissingNumber();
+
+    @Test
+    void test_findKthPositive_expect9_whenKIs5AndMissingNumberIs9() {
+        int[] input = {2, 3, 4, 7, 11};
+        Assertions.assertEquals(9, classObj.findKthPositive(input, 5));
+    }
+
+    @Test
+    void test_findKthPositive_expect6_whenKIs2AndMissingNumberIs6() {
+        int[] input = {1, 2, 3, 4};
+        Assertions.assertEquals(6, classObj.findKthPositive(input, 2));
+    }
+}
