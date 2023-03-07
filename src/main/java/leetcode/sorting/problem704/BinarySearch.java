b130dcaf8053f44dc0ff25d4fdc6f44931d3bad3
Lov Verma <lov.verma@greyamp.com>
2023-03-07 21:53:54 +0530

    [PROBLEM-704]: WIP - binary search

---
+++ src/main/java/leetcode/sorting/problem704/BinarySearch.java
@@ -0,0 +1,9 @@
+package leetcode.sorting.problem704;
+
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+
+        return -1;
+    }
+}
---
+++ src/test/java/leetcode/sorting/problem704/BinarySearchTest.java
@@ -0,0 +1,21 @@
+package leetcode.sorting.problem704;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTest {
+
+    BinarySearch classObj = new BinarySearch();
+
+    @Test
+    void test_search_expect4_whenElement9IsAtIndex4() {
+        int[] input = {-1, 0, 3, 5, 9, 12};
+        Assertions.assertEquals(4, classObj.search(input, 9));
+    }
+
+    @Test
+    void test_search_expectMinus2_whenElement2IsNotPresent() {
+        int[] input = {-1, 0, 3, 5, 9, 12};
+        Assertions.assertEquals(-1, classObj.search(input, 2));
+    }
+}
