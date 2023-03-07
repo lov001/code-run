2a1660c1ca3327f7808dd38244d83ad895408425
Lov Verma <lov.verma@greyamp.com>
2023-03-07 21:46:33 +0530

    [PROBLEM-2187]: WIP - minimum time to complete trips

--- src/main/java/leetcode/arrays/problem2187/MinimumTimeToCompleteTrips.java
+++ src/main/java/leetcode/arrays/problem2187/MinimumTimeToCompleteTrips.java
@@ -1,8 +1,22 @@
 package leetcode.arrays.problem2187;
 
+import java.util.Arrays;
+
 public class MinimumTimeToCompleteTrips {
 
     public long minimumTime(int[] time, int totalTrips) {
-        return 0;
+        long[] minimumTime = new long[time.length];
+        for (int i = 0; i < time.length; i++) {
+            minimumTime[i] = findIthTime(time[i], time[time.length - 1], totalTrips);
+        }
+        return Arrays.stream(minimumTime).max().getAsLong();
+    }
+
+    private long findIthTime(long time, long maxTime, int totalTrips) {
+        long iThTime = maxTime / time;
+        while (iThTime > totalTrips) {
+            iThTime -= time;
+        }
+        return iThTime;
     }
 }
