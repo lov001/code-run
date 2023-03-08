6a83e6799fd8813a737d426e7f1fac66ce0b42a8
Lov Verma <lov.verma@greyamp.com>
2023-03-08 15:06:34 +0530

    [PROBLEM-35]: final accepted solution

--- src/main/java/leetcode/searching/problem35/SearchInsertPosition.java
+++ src/main/java/leetcode/searching/problem35/SearchInsertPosition.java
@@ -4,22 +4,16 @@ public class SearchInsertPosition {
 
     public int searchInsert(int[] nums, int target) {
         int beg = 0, end = nums.length - 1;
-        int mid = 0;
-        while (beg < end) {
-            mid = beg + (end - beg) / 2;
-            if (nums[mid] < target) {
-                beg = mid + 1;
-            } else if (nums[mid] > target) {
-                end = mid - 1;
-            } else {
+        while (beg <= end) {
+            int mid = beg + (end - beg) / 2;
+            if (nums[mid] == target) {
                 return mid;
+            } else if (nums[mid] < target) {
+                beg = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
-        if (beg == end && beg == 0) {
-            return target > nums[beg] ? 1 : 0;
-        } else if (beg == end && beg == nums.length - 1) {
-            return nums.length;
-        }
-        return mid;
+        return beg;
     }
 }
--- src/test/java/leetcode/searching/problem35/SearchInsertPositionTest.java
+++ src/test/java/leetcode/searching/problem35/SearchInsertPositionTest.java
@@ -30,4 +30,28 @@ public class SearchInsertPositionTest {
         int[] input = {2, 3, 5, 6};
         Assertions.assertEquals(0, classObj.searchInsert(input, 1));
     }
+
+    @Test
+    void test_searchInsert_expect1_whenElement2IsToBeInsertedAtIndex1_() {
+        int[] input = {1, 3};
+        Assertions.assertEquals(1, classObj.searchInsert(input, 2));
+    }
+
+    @Test
+    void test_searchInsert_expect1_whenElement2IsToBeInsertedAtIndex1InBigArray() {
+        int[] input = {1, 2, 3, 4, 5, 10};
+        Assertions.assertEquals(1, classObj.searchInsert(input, 2));
+    }
+
+    @Test
+    void test_searchInsert_expect0_whenElement0IsToBeInsertedAtIndex0() {
+        int[] input = {1, 3};
+        Assertions.assertEquals(0, classObj.searchInsert(input, 0));
+    }
+
+    @Test
+    void test_searchInsert_expect1_whenElement3IsToBeInsertedAtIndex2() {
+        int[] input = {1, 2, 4, 6, 7};
+        Assertions.assertEquals(2, classObj.searchInsert(input, 3));
+    }
 }
