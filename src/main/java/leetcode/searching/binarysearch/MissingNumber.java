a127d4e4e81bffd169fd0eae9bb3c958a0ce30d5
Lov Verma <lov.verma@greyamp.com>
2023-03-09 11:17:50 +0530

    [PROBLEM-268]: brute force solution

--- src/main/java/leetcode/searching/binarysearch/MissingNumber.java
+++ src/main/java/leetcode/searching/binarysearch/MissingNumber.java
@@ -3,6 +3,17 @@ package leetcode.searching.binarysearch;
 public class MissingNumber {
 
     public int missingNumber(int[] nums) {
+        int[] output = new int[nums.length + 1];
+        for (int number : nums) {
+            if (output[number] == 0) {
+                output[number] = 1;
+            }
+        }
+        for (int i = 0; i < output.length; i++) {
+            if (output[i] == 0) {
+                return i;
+            }
+        }
         return 0;
     }
 }
