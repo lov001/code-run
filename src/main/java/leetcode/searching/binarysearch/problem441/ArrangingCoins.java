a3212f3ba968b349ddbf384004fe2794b907775e
Lov Verma <lov.verma@greyamp.com>
2023-03-10 09:54:03 +0530

    [PROBLEM-441]: working solution with all test cases passing

--- src/main/java/leetcode/searching/binarysearch/problem441/ArrangingCoins.java
+++ src/main/java/leetcode/searching/binarysearch/problem441/ArrangingCoins.java
@@ -3,6 +3,15 @@ package leetcode.searching.binarysearch.problem441;
 public class ArrangingCoins {
 
     public int arrangeCoins(int n) {
-        return 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n - i >= 0) {
+                count++;
+                n -= i;
+            } else {
+                break;
+            }
+        }
+        return count;
     }
 }
