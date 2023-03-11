18fd7285c4aa949201b5d544618c9ff8c63307b7
Lov Verma <lov.verma@greyamp.com>
2023-03-11 12:41:37 +0530

    [PROBLEM-367]: complete solution with all tests passing

--- src/main/java/leetcode/searching/binarysearch/problem367/PerfectSquare.java
+++ src/main/java/leetcode/searching/binarysearch/problem367/PerfectSquare.java
@@ -3,19 +3,19 @@ package leetcode.searching.binarysearch.problem367;
 public class PerfectSquare {
 
     public boolean isPerfectSquare(int num) {
-        if (num == 1) {
-            return false;
+        if(num == 1){
+            return true;
         }
-        int left = 2, right = num / 2;
+        long left = 1, right = num / 2;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int square = mid * mid;
-            if (square < num) {
-                left = mid + 1;
-            } else if (square > num) {
-                right = mid - 1;
-            } else {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+            if (square == num) {
                 return true;
+            } else if (square < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return false;
--- src/test/java/leetcode/searching/binarysearch/problem367/PerfectSquareTest.java
+++ src/test/java/leetcode/searching/binarysearch/problem367/PerfectSquareTest.java
@@ -16,4 +16,9 @@ public class PerfectSquareTest {
     void test_isPerfectSquare_expectFalse_whenNumberIs14() {
         Assertions.assertFalse(classObj.isPerfectSquare(14));
     }
+
+    @Test
+    void test_isPerfectSquare_expectTrue_whenNumberIs808201() {
+        Assertions.assertTrue(classObj.isPerfectSquare(808201));
+    }
 }
