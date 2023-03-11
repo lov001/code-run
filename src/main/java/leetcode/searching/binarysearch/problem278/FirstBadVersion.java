4824629380a66355bc38734f0ff2583100f8271c
Lov Verma <lov.verma@greyamp.com>
2023-03-11 12:01:39 +0530

    [PROBLEM-278]: refactored

--- src/main/java/leetcode/searching/binarysearch/problem278/FirstBadVersion.java
+++ src/main/java/leetcode/searching/binarysearch/problem278/FirstBadVersion.java
@@ -19,9 +19,7 @@ public class FirstBadVersion {
     private boolean isBadVersion(int n, long num) {
         if (n == 5 && num >= 4) {
             return true;
-        } else if (n == 1) {
-            return true;
-        }
-        return false;
+        } else
+            return n == 1;
     }
 }
