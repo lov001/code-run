bde65b161137208b2be2c3b0be4ad1d76caf69c2
Lov Verma <lov.verma@greyamp.com>
2023-03-12 15:21:52 +0530

    [PROBLEM-1346]: brute force solution - all tests passing

--- src/main/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDouble.java
+++ src/main/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDouble.java
@@ -1,8 +1,22 @@
 package leetcode.searching.binarysearch.problem1346;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberAndItsDouble {
 
     public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i] * 2, map.getOrDefault(arr[i] * 2, 0) + 1);
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (map.containsKey(arr[i])) {
+                return arr[i] == 0 && map.get(0) == 1 ? false : true;
+            }
+        }
         return false;
     }
 }
--- src/test/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDoubleTest.java
+++ src/test/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDoubleTest.java
@@ -18,4 +18,16 @@ public class NumberAndItsDoubleTest {
         int[] input = {3, 1, 7, 11};
         Assertions.assertFalse(classObj.checkIfExist(input));
     }
+
+    @Test
+    void test_checkIfExist_expectFalse_ifNoDoubleExistsInArrayHavingNegativeValues() {
+        int[] input = {-2, 0, 10, -19, 4, 6, -8};
+        Assertions.assertFalse(classObj.checkIfExist(input));
+    }
+
+    @Test
+    void test_checkIfExist_expectTrue_if0Double0Exists() {
+        int[] input = {0, 0};
+        Assertions.assertTrue(classObj.checkIfExist(input));
+    }
 }
