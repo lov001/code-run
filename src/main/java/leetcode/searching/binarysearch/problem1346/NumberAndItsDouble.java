57a3398b4f3e86c28cb0aa06f084458aa4cc3619
Lov Verma <lov.verma@greyamp.com>
2023-03-12 15:37:01 +0530

    [PROBLEM-1346]: binary search solution - all tests passing

--- src/main/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDouble.java
+++ src/main/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDouble.java
@@ -9,12 +9,12 @@ public class NumberAndItsDouble {
     public boolean checkIfExist(int[] arr) {
         Arrays.sort(arr);
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i] * 2, map.getOrDefault(arr[i] * 2, 0) + 1);
+        for (int j : arr) {
+            map.put(j * 2, map.getOrDefault(j * 2, 0) + 1);
         }
         for (int i = arr.length - 1; i >= 0; i--) {
             if (map.containsKey(arr[i])) {
-                return arr[i] == 0 && map.get(0) == 1 ? false : true;
+                return arr[i] != 0 || map.get(0) != 1;
             }
         }
         return false;
--- src/test/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDoubleTest.java
+++ src/test/java/leetcode/searching/binarysearch/problem1346/NumberAndItsDoubleTest.java
@@ -6,28 +6,33 @@ import org.junit.jupiter.api.Test;
 public class NumberAndItsDoubleTest {
 
     NumberAndItsDouble classObj = new NumberAndItsDouble();
+    NumberAndItsDoubleUsingBinarySearch classObj2 = new NumberAndItsDoubleUsingBinarySearch();
 
     @Test
     void test_checkIfExist_expectTrue_if5Double10Exists() {
         int[] input = {10, 2, 5, 3};
         Assertions.assertTrue(classObj.checkIfExist(input));
+        Assertions.assertTrue(classObj2.checkIfExist(input));
     }
 
     @Test
     void test_checkIfExist_expectFalse_ifNoDoubleExists() {
         int[] input = {3, 1, 7, 11};
         Assertions.assertFalse(classObj.checkIfExist(input));
+        Assertions.assertFalse(classObj2.checkIfExist(input));
     }
 
     @Test
     void test_checkIfExist_expectFalse_ifNoDoubleExistsInArrayHavingNegativeValues() {
         int[] input = {-2, 0, 10, -19, 4, 6, -8};
         Assertions.assertFalse(classObj.checkIfExist(input));
+        Assertions.assertFalse(classObj2.checkIfExist(input));
     }
 
     @Test
     void test_checkIfExist_expectTrue_if0Double0Exists() {
         int[] input = {0, 0};
         Assertions.assertTrue(classObj.checkIfExist(input));
+        Assertions.assertTrue(classObj2.checkIfExist(input));
     }
 }
