6099e8133625facda61fc903228adf6c56701ac3
Lov Verma <lov.verma@greyamp.com>
2023-03-13 10:12:02 +0530

    [PROBLEM-69]: optimal solution using binary search

--- src/main/java/leetcode/searching/binarysearch/problem69/SquareRoot.java
+++ src/main/java/leetcode/searching/binarysearch/problem69/SquareRoot.java
@@ -3,6 +3,18 @@ package leetcode.searching.binarysearch.problem69;
 public class SquareRoot {
 
     public int mySqrt(int x) {
-        return 1;
+        int left = 1;
+        int right = x;
+        int ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 }
