791d8ebf37a8124403cbde835b05bc4e1946be4a
Lov Verma <lov.verma@greyamp.com>
2023-03-15 19:22:24 +0530

    [PROBLEM-160]: complete solution with basic tests passing

--- src/main/java/leetcode/linkedlist/problem160/IntersectionOfTwoLinkedLists.java
+++ src/main/java/leetcode/linkedlist/problem160/IntersectionOfTwoLinkedLists.java
@@ -1,9 +1,36 @@
 package leetcode.linkedlist.problem160;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntersectionOfTwoLinkedLists {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        List<ListNode> list1 = new ArrayList<>();
+        ListNode ptr1 = headA, ptr2 = headB;
+        int intersection = 0;
+        boolean intersected = false;
+        while (ptr1 != null) {
+            list1.add(ptr1);
+            ptr1 = ptr1.next;
+        }
+        while (ptr2 != null) {
+            if (list1.contains(ptr2)) {
+                intersected = true;
+                break;
 
+            }
+            ptr2 = ptr2.next;
+            intersection++;
+        }
+        if (intersected) {
+            ListNode temp = headB;
+            while (intersection != 0) {
+                temp = temp.next;
+                intersection--;
+            }
+            return temp;
+        }
         return null;
     }
 }
