a4e0bc6f646e8d4f5ad84dfbaa58c3b1c33724f6
Lov Verma <lov.verma@greyamp.com>
2023-03-15 19:45:00 +0530

    [PROBLEM-160]: approach 3, using the diffrence of length

--- src/test/java/leetcode/linkedlist/problem160/IntersectionOfTwoLinkedListsTest.java
+++ src/test/java/leetcode/linkedlist/problem160/IntersectionOfTwoLinkedListsTest.java
@@ -8,6 +8,8 @@ public class IntersectionOfTwoLinkedListsTest {
     IntersectionOfTwoLinkedLists classObj = new IntersectionOfTwoLinkedLists();
     IntersectionOfTwoLinkedListsApproach2WithoutExtraSpace classObj2 =
         new IntersectionOfTwoLinkedListsApproach2WithoutExtraSpace();
+    IntersectionOfTwoLinkedListsApproach3LengthDifference classObj3 =
+        new IntersectionOfTwoLinkedListsApproach3LengthDifference();
 
     @Test
     void test_getIntersectionNode_expect8_whenListsIntersectAt8() {
@@ -28,6 +30,7 @@ public class IntersectionOfTwoLinkedListsTest {
         val22.next = val2;
         Assertions.assertEquals(val2, classObj.getIntersectionNode(list1, list2));
         Assertions.assertEquals(val2, classObj2.getIntersectionNode(list1, list2));
+        Assertions.assertEquals(val2, classObj3.getIntersectionNode(list1, list2));
     }
 
     @Test
@@ -41,6 +44,7 @@ public class IntersectionOfTwoLinkedListsTest {
         list2.next = new ListNode(5);
         Assertions.assertNull(classObj.getIntersectionNode(list1, list2));
         Assertions.assertNull(classObj2.getIntersectionNode(list1, list2));
+        Assertions.assertNull(classObj3.getIntersectionNode(list1, list2));
     }
 
     @Test
@@ -58,5 +62,6 @@ public class IntersectionOfTwoLinkedListsTest {
         list2.next = val3;
         Assertions.assertEquals(val3, classObj.getIntersectionNode(list1, list2));
         Assertions.assertEquals(val3, classObj2.getIntersectionNode(list1, list2));
+        Assertions.assertEquals(val3, classObj3.getIntersectionNode(list1, list2));
     }
 }
