f65e3d05f61db6abb7518d03a577efd620b77bf1
Lov Verma <lov.verma@greyamp.com>
2023-03-17 11:00:36 +0530

    [PROBLEM-169]: Brute force approach will all test cases passing

--- src/main/java/leetcode/arrays/problem169/MajorityElement.java
+++ src/main/java/leetcode/arrays/problem169/MajorityElement.java
@@ -1,8 +1,19 @@
 package leetcode.arrays.problem169;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MajorityElement {
 
     public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int number : nums) {
+            int newValue = map.getOrDefault(number, 0) + 1;
+            map.put(number, newValue);
+            if (newValue > nums.length / 2) {
+                return number;
+            }
+        }
         return 0;
     }
 }
