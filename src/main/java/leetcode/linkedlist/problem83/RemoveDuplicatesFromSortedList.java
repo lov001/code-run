3224c7fd433f21ed0be72ef4a10faa013cc1a225
Lov Verma <lov.verma@greyamp.com>
2023-03-18 14:57:29 +0530

    [PROBLEM-83]: WIP - Remove Duplicates from sorted linked list

---
+++ src/main/java/leetcode/linkedlist/problem83/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,11 @@
+package leetcode.linkedlist.problem83;
+
+import leetcode.linkedlist.ListNode;
+
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        return head;
+    }
+}
