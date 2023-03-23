1ede4abf835dac9986b53c349110fc3fe742e8e2
Lov Verma <lov.verma@greyamp.com>
2023-03-23 18:53:23 +0530

    [PROBLEM-101]: complete working solution - recursive approach

---
+++ src/main/java/leetcode/tree/binarytree/problem101/SymmetricTree.java
@@ -0,0 +1,30 @@
+package leetcode.tree.binarytree.problem101;
+
+import leetcode.tree.TreeNode;
+
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        if (root.left == null || root.right == null) {
+            return false;
+        }
+        return isSymmetricHelper(root.left, root.right);
+    }
+
+    private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right,
+            right.left);
+    }
+}
