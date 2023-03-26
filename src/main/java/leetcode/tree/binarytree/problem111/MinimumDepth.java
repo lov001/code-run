c252c9de2346bb259f2aad53a68e4af25cfadad4
Lov Verma <lov.verma@greyamp.com>
2023-03-26 09:46:22 +0530

    [PROBLEM-111]: WIP - basic tests passing

--- src/main/java/leetcode/tree/binarytree/problem111/MinimumDepth.java
+++ src/main/java/leetcode/tree/binarytree/problem111/MinimumDepth.java
@@ -5,7 +5,26 @@ import leetcode.tree.TreeNode;
 public class MinimumDepth {
 
     public int minDepth(TreeNode root) {
-        int min = -1;
-        return min;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == 0 && rightHeight != 0) {
+            return rightHeight + 1;
+        }
+        if (leftHeight != 0 && rightHeight == 0) {
+            return leftHeight + 1;
+        }
+        return 1 + Math.min(leftHeight, rightHeight);
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        return 1 + Math.max(left, right);
     }
 }
