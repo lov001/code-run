6a83e5769bfc0d10e084fa0c0c08a5e32ebc03cd
Lov Verma <lov.verma@greyamp.com>
2023-03-26 09:20:56 +0530

    [PROBLEM-111]: WIP - finding minimum depth of a binary tree

---
+++ src/main/java/leetcode/tree/binarytree/problem111/MinimumDepth.java
@@ -0,0 +1,11 @@
+package leetcode.tree.binarytree.problem111;
+
+import leetcode.tree.TreeNode;
+
+public class MinimumDepth {
+
+    public int minDepth(TreeNode root) {
+        int min = -1;
+        return min;
+    }
+}
---
+++ src/test/java/leetcode/tree/binarytree/problem111/MinimumDepthTest.java
@@ -0,0 +1,32 @@
+package leetcode.tree.binarytree.problem111;
+
+import leetcode.tree.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MinimumDepthTest {
+
+    MinimumDepth classObj = new MinimumDepth();
+
+    @Test
+    void test_minDepth_expect2_whenMinimumDepthIs2() {
+        TreeNode right3 = new TreeNode(7, null, null);
+        TreeNode left3 = new TreeNode(15, null, null);
+        TreeNode right1 = new TreeNode(20, left3, right3);
+        TreeNode left1 = new TreeNode(9, null, null);
+        TreeNode tree = new TreeNode(3, left1, right1);
+
+        Assertions.assertEquals(2, classObj.minDepth(tree));
+    }
+
+    @Test
+    void test_minDepth_expect5_whenMinimumDepthIs5() {
+        TreeNode right5 = new TreeNode(4, null, null);
+        TreeNode right4 = new TreeNode(5, null, right5);
+        TreeNode right3 = new TreeNode(4, null, right4);
+        TreeNode right1 = new TreeNode(3, null, right3);
+        TreeNode tree = new TreeNode(2, null, right1);
+
+        Assertions.assertEquals(5, classObj.minDepth(tree));
+    }
+}
