116d0ce98218c99303657f342ec5b155b5d78a90
Lov Verma <lov.verma@greyamp.com>
2023-04-01 11:52:00 +0530

    [PROBLEM-543]: wip - diameter of a binary tree

---
+++ src/main/java/leetcode/tree/binarytree/problem543/DiameterOfBinaryTree.java
@@ -0,0 +1,10 @@
+package leetcode.tree.binarytree.problem543;
+
+import leetcode.tree.TreeNode;
+
+public class DiameterOfBinaryTree {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        return 0;
+    }
+}
---
+++ src/test/java/leetcode/tree/binarytree/problem543/DiameterOfBinaryTreeTest.java
@@ -0,0 +1,29 @@
+package leetcode.tree.binarytree.problem543;
+
+import leetcode.tree.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DiameterOfBinaryTreeTest {
+
+    DiameterOfBinaryTree classObj = new DiameterOfBinaryTree();
+
+    @Test
+    void test_diameterOfBinaryTree_expect3_whenDiameterIs3() {
+        TreeNode right3 = new TreeNode(5, null, null);
+        TreeNode left3 = new TreeNode(4, null, null);
+        TreeNode right1 = new TreeNode(3, null, null);
+        TreeNode left1 = new TreeNode(2, left3, right3);
+        TreeNode tree = new TreeNode(1, left1, right1);
+
+        Assertions.assertEquals(3, classObj.diameterOfBinaryTree(tree));
+    }
+
+    @Test
+    void test_diameterOfBinaryTree_expect1_whenDiameterIs1() {
+        TreeNode left1 = new TreeNode(2, null, null);
+        TreeNode tree = new TreeNode(1, left1, null);
+
+        Assertions.assertEquals(1, classObj.diameterOfBinaryTree(tree));
+    }
+}
