25418373b6b06f7c2379418082361b6340fe59d2
Lov Verma <lov.verma@greyamp.com>
2023-04-02 17:26:59 +0530

    [PROBLEM-2300]: all working solution

--- src/main/java/leetcode/arrays/problem2300/SpellAndPortions.java
+++ src/main/java/leetcode/arrays/problem2300/SpellAndPortions.java
@@ -1,18 +1,26 @@
 package leetcode.arrays.problem2300;
 
+import java.util.Arrays;
+
 public class SpellAndPortions {
 
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int[] result = new int[spells.length];
-        for (int i = 0; i < spells.length; i++) {
-            int count = 0;
-            for (int portion : potions) {
-                if ((long) spells[i] * portion >= success) {
-                    count++;
+        int n = spells.length, m = potions.length;
+        Arrays.sort(potions);
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int spell = spells[i];
+            int low = 0, high = m - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (((long) potions[mid] * spell) >= success) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
             }
-            result[i] = count;
+            ans[i] = m - low;
         }
-        return result;
+        return ans;
     }
 }
