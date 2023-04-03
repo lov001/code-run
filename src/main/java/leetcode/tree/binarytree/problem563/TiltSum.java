8577491bfc91da9e9402a7d2f122247c8a814c23
Lov Verma <lov.verma@greyamp.com>
2023-04-03 10:46:28 +0530

    [PROBLEM-563]: complete working solution recursive

--- src/main/java/leetcode/tree/binarytree/problem563/TiltSum.java
+++ src/main/java/leetcode/tree/binarytree/problem563/TiltSum.java
@@ -4,8 +4,23 @@ import leetcode.tree.TreeNode;
 
 public class TiltSum {
 
-    public int findTilt(TreeNode root) {
+    private int sum = 0;
 
-        return 0;
+    public int findTilt(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        traverse(root);
+        return sum;
+    }
+
+    private int traverse(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = traverse(root.left);
+        int rightSum = traverse(root.right);
+        sum += Math.abs(leftSum - rightSum);
+        return leftSum + rightSum + root.val;
     }
 }
