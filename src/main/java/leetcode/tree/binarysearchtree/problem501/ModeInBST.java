2d430cd9d78c93c1f3aa2f0a96f911b55e6ef2ce
Lov Verma <lov.verma@greyamp.com>
2023-04-08 22:36:27 +0530

    [PROBLEM-501]: WIP - mode of a BST

---
+++ src/main/java/leetcode/tree/binarysearchtree/problem501/ModeInBST.java
@@ -0,0 +1,10 @@
+package leetcode.tree.binarysearchtree.problem501;
+
+import leetcode.tree.TreeNode;
+
+public class ModeInBST {
+
+    public int[] findMode(TreeNode root) {
+        return null;
+    }
+}
