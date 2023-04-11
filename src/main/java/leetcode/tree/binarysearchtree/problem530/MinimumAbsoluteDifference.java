1d408cd12545592bf240e3882e2af3080b973031
Lov Verma <lov.verma@greyamp.com>
2023-04-11 20:46:26 +0530

    [PROBLEM-530]: WIP - Minimum absolute difference of bst

---
+++ src/main/java/leetcode/tree/binarysearchtree/problem530/MinimumAbsoluteDifference.java
@@ -0,0 +1,10 @@
+package leetcode.tree.binarysearchtree.problem530;
+
+import leetcode.tree.TreeNode;
+
+public class MinimumAbsoluteDifference {
+    public int getMinimumDifference(TreeNode root) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/leetcode/tree/binarysearchtree/problem530/MinimumAbsoluteDifferenceTest.java
@@ -0,0 +1,30 @@
+package leetcode.tree.binarysearchtree.problem530;
+
+import leetcode.tree.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MinimumAbsoluteDifferenceTest {
+
+    MinimumAbsoluteDifference classObj = new MinimumAbsoluteDifference();
+
+    @Test
+    void tes_getMinimumDifference_expect1_whenMinimumAbsoluteDifferenceIs1() {
+        TreeNode right2 = new TreeNode(3, null, null);
+        TreeNode left2 = new TreeNode(1, null, null);
+        TreeNode right1 = new TreeNode(6, null, null);
+        TreeNode left1 = new TreeNode(2, left2, right2);
+        TreeNode tree = new TreeNode(4, left1, right1);
+        Assertions.assertEquals(1, classObj.getMinimumDifference(tree));
+    }
+
+    @Test
+    void tes_getMinimumDifference_expect1_whenMinimumAbsoluteDifferenceIs1_() {
+        TreeNode right2 = new TreeNode(49, null, null);
+        TreeNode left2 = new TreeNode(12, null, null);
+        TreeNode right1 = new TreeNode(0, null, null);
+        TreeNode left1 = new TreeNode(48, left2, right2);
+        TreeNode tree = new TreeNode(1, right1, left1);
+        Assertions.assertEquals(1, classObj.getMinimumDifference(tree));
+    }
+}
