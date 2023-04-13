6d299aa8ea55d347dd36d127a49db4084d75cb10
Lov Verma <lov.verma@greyamp.com>
2023-04-13 21:57:33 +0530

    [PROBLEM-387]: complete working solution - first unique character

---
+++ src/main/java/leetcode/queue/problem387/FirstUniqueCharacter.java
@@ -0,0 +1,20 @@
+package leetcode.queue.problem387;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueCharacter {
+
+    public int firstUniqueChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
