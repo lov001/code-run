8def9439493eda15b4c04abf2c3298768aa3f08e
Lov Verma <lovverma001@gmail.com>
2023-07-20 20:13:11 +0530

    [PROBLEM-122]: WIP - Best Time to Buy and Sell Stock II

---
+++ src/main/java/leetcode/arrays/problem122/MaxProfitOnSellingAStock2.java
@@ -0,0 +1,11 @@
+package leetcode.arrays.problem122;
+
+public class MaxProfitOnSellingAStock2 {
+
+
+   public int maxProfit(int[] prices) {
+      int maxProfit = 0;
+      return maxProfit;
+   }
+
+}
