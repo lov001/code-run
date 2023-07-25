5008c295e9f10e92e51b462265a2b5c9aa5d476c
Lov Verma <lovverma001@gmail.com>
2023-07-25 23:59:38 +0530

    [PROBLEM-45]:

--- src/main/java/leetcode/arrays/problem45/JumpGameII.java
+++ src/main/java/leetcode/arrays/problem45/JumpGameII.java
@@ -4,13 +4,20 @@ public class JumpGameII {
 
 
    public int canJump(int[] nums) {
-      int reachable = 0;
-      for (int i = 0; i < nums.length; i++) {
-         if (i > reachable) {
-            return reachable;
+      int result = 0;
+      int left = 0, right = 0;
+      while (right < nums.length - 1) {
+         int farthest = 0;
+         for (int i = left; i < right + 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
          }
-         reachable = Math.max(reachable, i + nums[i]);
+         left = right + 1;
+         right = farthest;
+         result++;
+         if (farthest >= nums.length - 1) {
+            return result;
+         }
       }
-      return reachable;
+      return result;
    }
 }
