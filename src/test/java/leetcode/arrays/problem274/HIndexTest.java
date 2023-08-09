8224c06ac7f88fcbc45a2c9319ae8a642e6c132f
Lov Verma <lovverma001@gmail.com>
2023-08-09 20:31:44 +0530

    [PROBLEM-274]: solution using binary search

--- src/test/java/leetcode/arrays/problem274/HIndexTest.java
+++ src/test/java/leetcode/arrays/problem274/HIndexTest.java
@@ -15,8 +15,32 @@ public class HIndexTest {
    }
 
    @Test
+   void test_hIndex_expect3_whenInputSizeIs5_() {
+      int[] input = {1, 2, 4, 8, 9};
+      Assertions.assertEquals(3, classObj.hIndex(input));
+   }
+
+   @Test
+   void test_hIndex_expect2_whenInputSizeIs5() {
+      int[] input = {0, 1, 2, 3, 4};
+      Assertions.assertEquals(2, classObj.hIndex(input));
+   }
+
+   @Test
+   void test_hIndex_expect3_whenInputSizeIs4() {
+      int[] input = {1, 8, 9, 10};
+      Assertions.assertEquals(3, classObj.hIndex(input));
+   }
+
+   @Test
    void test_hIndex_expect1_whenInputSizeIs3() {
       int[] input = {1, 3, 1};
+      Assertions.assertEquals(1, classObj.hIndex(input));
+   }
+
+   @Test
+   void test_hIndex_expect1_whenInputSizeIs1() {
+      int[] input = {100};
       Assertions.assertEquals(1, classObj.hIndex(input));
    }
 }
