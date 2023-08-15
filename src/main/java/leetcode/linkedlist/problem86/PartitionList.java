333723ed9f43e8434a01e74ad25c3555e8fd265b
Lov Verma <lovverma001@gmail.com>
2023-08-15 10:24:30 +0530

    [PROBLEM-86]: WIP - partition a linked list around x

---
+++ src/main/java/leetcode/linkedlist/problem86/PartitionList.java
@@ -0,0 +1,12 @@
+package leetcode.linkedlist.problem86;
+
+import leetcode.linkedlist.ListNode;
+
+public class PartitionList {
+
+
+   public ListNode partition(ListNode head, int x) {
+      return null;
+   }
+
+}
