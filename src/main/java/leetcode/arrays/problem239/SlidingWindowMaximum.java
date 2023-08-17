61257ac730899d75c49f1743705d1f5da6e77065
Lov Verma <lovverma001@gmail.com>
2023-08-17 22:40:58 +0530

    [PROBLEM-239]: using priority queue, TLE

--- src/main/java/leetcode/arrays/problem239/SlidingWindowMaximum.java
+++ src/main/java/leetcode/arrays/problem239/SlidingWindowMaximum.java
@@ -1,19 +1,28 @@
 package leetcode.arrays.problem239;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class SlidingWindowMaximum {
 
 
    public int[] maxSlidingWindow(int[] nums, int k) {
+      Queue<Integer> queue = new LinkedList<>();
+      PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
       List<Integer> result = new ArrayList<>();
-      for (int i = 0; i <= nums.length - k; i++) {
-         int max = Integer.MIN_VALUE;
-         for (int j = i; j < i + k; j++) {
-            max = Math.max(nums[j], max);
+      for (int num : nums) {
+         priorityQueue.offer(num);
+         queue.add(num);
+         if (priorityQueue.size() == k) {
+            int max = priorityQueue.peek();
+            int toRemove = queue.remove();
+            priorityQueue.remove(toRemove);
+            result.add(max);
          }
-         result.add(max);
       }
       return result.stream().mapToInt(Integer::intValue).toArray();
    }
