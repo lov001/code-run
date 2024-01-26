4a530a485fe78aacb3e4d2834c3d4bff9c356c53
Lov Verma <lovverma001@gmail.com>
2024-01-26 12:54:47 +0530

    [practice/arrays-and-strings]: rotate matrix 90 degree using transpose and in place

--- src/main/java/practice/arraysandstrings/RotateMatrix90Degree.java
+++ src/main/java/practice/arraysandstrings/RotateMatrix90Degree.java
@@ -3,7 +3,7 @@ package practice.arraysandstrings;
 public class RotateMatrix90Degree {
 
 
-   public int[][] rotate(int[][] matrix, int m, int n) {
+   public int[][] rotateWithExtraSpace(int[][] matrix, int m, int n) {
       int[][] result = new int[m][n];
       for (int i = 0; i < m; i++) {
          for (int j = 0; j < n; j++) {
@@ -13,4 +13,40 @@ public class RotateMatrix90Degree {
       return result;
    }
 
+   public void rotateInPlace(int matrix[][], int n) {
+      for (int i = 0; i < n / 2; i++) {
+         for (int j = i; j < n - i - 1; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][n - 1 - i];
+            matrix[j][n - 1 - i] = matrix[n - 1 - i][n - 1 - j];
+            matrix[n - 1 - i][n - 1 - j] = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = temp;
+         }
+      }
+   }
+
+   public void rotateInPlaceUsingTranspose(int matrix[][], int n) {
+      for (int i = 0; i < n; i++) {
+         reverseRow(matrix, n, i);
+      }
+      for (int i = 0; i < n; i++) {
+         for (int j = i; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+         }
+      }
+   }
+
+   public void reverseRow(int matrix[][], int n, int i) {
+      int start = 0, end = n - 1;
+      while (start < end) {
+         int temp = matrix[i][start];
+         matrix[i][start] = matrix[i][end];
+         matrix[i][end] = temp;
+
+         start++;
+         end--;
+      }
+   }
 }
--- src/test/java/practice/arraysandstrings/RotateMatrix90DegreeTest.java
+++ src/test/java/practice/arraysandstrings/RotateMatrix90DegreeTest.java
@@ -21,7 +21,14 @@ public class RotateMatrix90DegreeTest {
           {8, 5, 2},
           {9, 6, 3}
       };
-      Assertions.assertTrue(Arrays.deepEquals(output, classObj.rotate(input, 3, 3)));
+      Assertions.assertTrue(Arrays.deepEquals(output, classObj.rotateWithExtraSpace(input, 3, 3)));
+      classObj.rotateInPlace(input, 3);
+      int[][] output2 = {
+          {3, 6, 9},
+          {2, 5, 8},
+          {1, 4, 7}
+      };
+      Assertions.assertTrue(Arrays.deepEquals(output2, input));
    }
 
    @Test
@@ -38,7 +45,49 @@ public class RotateMatrix90DegreeTest {
           {15, 11, 7, 3},
           {16, 12, 8, 4}
       };
-      Assertions.assertTrue(Arrays.deepEquals(output, classObj.rotate(input, 4, 4)));
+      Assertions.assertTrue(Arrays.deepEquals(output, classObj.rotateWithExtraSpace(input, 4, 4)));
+      classObj.rotateInPlace(input, 4);
+      int[][] output2 = {
+          {4, 8, 12, 16},
+          {3, 7, 11, 15},
+          {2, 6, 10, 14},
+          {1, 5, 9, 13}
+      };
+      Assertions.assertTrue(Arrays.deepEquals(output2, input));
+   }
+
+   @Test
+   void test_rotateUsingTranspose_expectRotatedMatrix_when3x3Matrix() {
+      int[][] input = {
+          {1, 2, 3},
+          {4, 5, 6},
+          {7, 8, 9}
+      };
+      classObj.rotateInPlaceUsingTranspose(input, 3);
+      int[][] output2 = {
+          {3, 6, 9},
+          {2, 5, 8},
+          {1, 4, 7}
+      };
+      Assertions.assertTrue(Arrays.deepEquals(output2, input));
+   }
+
+   @Test
+   void test_rotateUsingTranspose_expectRotatedMatrix_when4x4Matrix() {
+      int[][] input = {
+          {1, 2, 3, 4},
+          {5, 6, 7, 8},
+          {9, 10, 11, 12},
+          {13, 14, 15, 16}
+      };
+      classObj.rotateInPlaceUsingTranspose(input, 4);
+      int[][] output2 = {
+          {4, 8, 12, 16},
+          {3, 7, 11, 15},
+          {2, 6, 10, 14},
+          {1, 5, 9, 13}
+      };
+      Assertions.assertTrue(Arrays.deepEquals(output2, input));
    }
 
 }
