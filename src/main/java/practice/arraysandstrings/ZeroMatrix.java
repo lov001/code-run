a543ae97481f254c53f062bb3f54dec70a8e0439
Lov Verma <lovverma001@gmail.com>
2024-01-29 18:06:28 +0530

    [practice/arrays-and-strings]: updated logic to have boolean

--- src/main/java/practice/arraysandstrings/ZeroMatrix.java
+++ src/main/java/practice/arraysandstrings/ZeroMatrix.java
@@ -1,31 +1,42 @@
 package practice.arraysandstrings;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class ZeroMatrix {
 
 
    public void zeroMatrix(int[][] matrix, int m, int n) {
-      List<Integer> rows = new ArrayList<>(m);
-      List<Integer> columns = new ArrayList<>(n);
+      boolean[] rows = new boolean[m];
+      boolean[] columns = new boolean[n];
 
       for (int i = 0; i < m; i++) {
          for (int j = 0; j < n; j++) {
             if (matrix[i][j] == 0) {
-               rows.add(i);
-               columns.add(j);
+               rows[i] = true;
+               columns[j] = true;
             }
          }
       }
 
-      for (int i = 0; i < m; i++) {
-         for (int j = 0; j < n; j++) {
-            if (rows.contains(i) || columns.contains(j)) {
-               matrix[i][j] = 0;
-            }
+      for(int i=0; i<m; i++){
+         if(rows[i]){
+            nullifyRow(matrix, i);
          }
+      }
+      for(int j=0; j<n; j++){
+         if(columns[j]){
+            nullifyColumn(matrix, j);
+         }
+      }
+   }
+
+   private void nullifyColumn(int[][] matrix, int column) {
+      for(int i=0; i<matrix.length; i++){
+         matrix[i][column] = 0;
+      }
+   }
+
+   private void nullifyRow(int[][] matrix, int row) {
+      for(int j=0; j<matrix[0].length; j++){
+         matrix[row][j] = 0;
       }
    }
 
