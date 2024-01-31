472398265a209492371c317b23de9d5e4cb3745a
Lov Verma <lovverma001@gmail.com>
2024-01-31 09:35:58 +0530

    [practice/linked-list]: remove duplicates using a set

---
+++ src/test/java/practice/linkedlist/RemoveDuplicatesTest.java
@@ -0,0 +1,43 @@
+package practice.linkedlist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RemoveDuplicatesTest {
+
+
+   RemoveDuplicates classObj = new RemoveDuplicates();
+
+   @Test
+   void test_removeDuplicates_expectSameLinkedList_whenNoDuplicates() {
+      Node fifth = new Node(5, null);
+      Node fourth = new Node(4, fifth);
+      Node third = new Node(3, fourth);
+      Node second = new Node(2, third);
+      Node first = new Node(1, second);
+      classObj.removeDuplicates(first);
+
+      Assertions.assertEquals(1, first.data);
+      Assertions.assertEquals(2, first.next.data);
+      Assertions.assertEquals(3, first.next.next.data);
+      Assertions.assertEquals(4, first.next.next.next.data);
+      Assertions.assertEquals(5, first.next.next.next.next.data);
+   }
+
+   @Test
+   void test_removeDuplicates_expectLinkedListWithNoDuplicate_whenDuplicatesExists() {
+      Node fifth = new Node(5, null);
+      Node fourth = new Node(5, fifth);
+      Node copy = new Node(4, fourth);
+      Node third = new Node(2, copy);
+      Node second = new Node(2, third);
+      Node first = new Node(1, second);
+      classObj.removeDuplicates(first);
+
+      Assertions.assertEquals(1, first.data);
+      Assertions.assertEquals(2, first.next.data);
+      Assertions.assertEquals(4, first.next.next.data);
+      Assertions.assertEquals(5, first.next.next.next.data);
+   }
+
+}
