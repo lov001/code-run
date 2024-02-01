7e805febc6da1622f84ac4115c8c41c478798181
Lov Verma <lovverma001@gmail.com>
2024-02-01 09:27:09 +0530

    [practice/linked-list]: WIP last kth element

---
+++ src/main/java/practice/linkedlist/LastKthElement.java
@@ -0,0 +1,10 @@
+package practice.linkedlist;
+
+public class LastKthElement {
+
+
+   public int lastKthElement(Node head, int k) {
+      return -1;
+   }
+
+}
---
+++ src/test/java/practice/linkedlist/LastKthElementTest.java
@@ -0,0 +1,34 @@
+package practice.linkedlist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LastKthElementTest {
+
+
+   LastKthElement classObj = new LastKthElement();
+
+   @Test
+   void test_lastKthElement_expect4_whenKIs3() {
+      Node fifth = new Node(5, null);
+      Node fourth = new Node(5, fifth);
+      Node copy = new Node(4, fourth);
+      Node third = new Node(2, copy);
+      Node second = new Node(2, third);
+      Node first = new Node(1, second);
+
+      Assertions.assertEquals(4, classObj.lastKthElement(first, 3));
+   }
+
+   @Test
+   void test_lastKthElement_expect10_whenKIs2() {
+      Node fourth = new Node(50, null);
+      Node copy = new Node(10, fourth);
+      Node third = new Node(1, copy);
+      Node second = new Node(9, third);
+      Node first = new Node(3, second);
+
+      Assertions.assertEquals(10, classObj.lastKthElement(first, 2));
+   }
+
+}
