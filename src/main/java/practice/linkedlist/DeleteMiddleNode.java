d32d420e84c2267ad3b542f5a98ed1ccc8460a0c
Lov Verma <lovverma001@gmail.com>
2024-02-02 10:43:21 +0530

    [practice/linked-list]: deleting middle element using iterative approach

--- src/main/java/practice/linkedlist/DeleteMiddleNode.java
+++ src/main/java/practice/linkedlist/DeleteMiddleNode.java
@@ -2,8 +2,24 @@ package practice.linkedlist;
 
 public class DeleteMiddleNode {
 
-   public void deleteMiddle(Node head){
 
+   public void deleteMiddle(Node head) {
+      if (head == null || head.next == null) {
+         return;
+      }
+      Node slow = head;
+      Node fast = head.next.next;
+      while (fast != null) {
+         fast = fast.next;
+         if (fast == null) {
+            Node ptr = head;
+            while (ptr.next != slow) {
+               ptr = ptr.next;
+            }
+            ptr.next = slow.next;
+         }
+         slow = slow.next;
+      }
    }
 
 }
--- src/test/java/practice/linkedlist/DeleteMiddleNodeTest.java
+++ src/test/java/practice/linkedlist/DeleteMiddleNodeTest.java
@@ -9,7 +9,7 @@ public class DeleteMiddleNodeTest {
    DeleteMiddleNode classObj = new DeleteMiddleNode();
 
    @Test
-   void test_deleteMiddle_expectSameLinkedList_whenNoDuplicates() {
+   void test_deleteMiddle_expect3ToBeRemoved_whenNoOfElementsIs5() {
       Node fifth = new Node(5, null);
       Node fourth = new Node(4, fifth);
       Node third = new Node(3, fourth);
@@ -25,27 +25,11 @@ public class DeleteMiddleNodeTest {
    }
 
    @Test
-   void test_deleteMiddle_expectLinkedListWithNoDuplicate_whenDuplicatesExists() {
-      Node fifth = new Node(5, null);
+   void test_deleteMiddle_expect4ToBeRemoved_whenNoOfElementsIs6() {
+      Node fifth = new Node(6, null);
       Node fourth = new Node(5, fifth);
       Node copy = new Node(4, fourth);
-      Node third = new Node(2, copy);
-      Node second = new Node(2, third);
-      Node first = new Node(1, second);
-      classObj.deleteMiddle(first);
-
-      Assertions.assertEquals(1, first.data);
-      Assertions.assertEquals(2, first.next.data);
-      Assertions.assertEquals(2, first.next.next.data);
-      Assertions.assertEquals(5, first.next.next.next.data);
-      Assertions.assertEquals(5, first.next.next.next.next.data);
-   }
-
-   @Test
-   void test_removeDuplicatesWithoutExtraSpace_expectSameLinkedList_whenNoDuplicates() {
-      Node fifth = new Node(5, null);
-      Node fourth = new Node(4, fifth);
-      Node third = new Node(3, fourth);
+      Node third = new Node(3, copy);
       Node second = new Node(2, third);
       Node first = new Node(1, second);
       classObj.deleteMiddle(first);
@@ -53,41 +37,28 @@ public class DeleteMiddleNodeTest {
       Assertions.assertEquals(1, first.data);
       Assertions.assertEquals(2, first.next.data);
       Assertions.assertEquals(3, first.next.next.data);
-      Assertions.assertEquals(4, first.next.next.next.data);
-      Assertions.assertEquals(5, first.next.next.next.next.data);
+      Assertions.assertEquals(5, first.next.next.next.data);
+      Assertions.assertEquals(6, first.next.next.next.next.data);
    }
 
    @Test
-   void test_removeDuplicatesWithoutExtraSpace_expectLinkedListWithNoDuplicate_whenDuplicatesExists() {
-      Node fifth = new Node(5, null);
-      Node fourth = new Node(5, fifth);
-      Node copy = new Node(4, fourth);
-      Node third = new Node(2, copy);
-      Node second = new Node(2, third);
+   void test_deleteMiddle_expectNothingToBeRemoved_whenOnlyHeadNode() {
+      Node first = new Node(1, null);
+      classObj.deleteMiddle(first);
+
+      Assertions.assertEquals(1, first.data);
+      Assertions.assertNull(first.next);
+   }
+
+   @Test
+   void test_deleteMiddle_expectNothingToBeRemoved_whenNoOfElementsIs2() {
+      Node second = new Node(2, null);
       Node first = new Node(1, second);
       classObj.deleteMiddle(first);
 
       Assertions.assertEquals(1, first.data);
       Assertions.assertEquals(2, first.next.data);
-      Assertions.assertEquals(4, first.next.next.data);
-      Assertions.assertEquals(5, first.next.next.next.data);
-   }
-
-   @Test
-   void test_deleteMiddle_expectLinkedListWithNoDuplicate_whenDuplicatesExists2() {
-      Node fifth = new Node(2, null);
-      Node fourth = new Node(5, fifth);
-      Node copy = new Node(4, fourth);
-      Node third = new Node(2, copy);
-      Node second = new Node(2, third);
-      Node first = new Node(1, second);
-      classObj.deleteMiddle(first);
-
-      Assertions.assertEquals(1, first.data);
-      Assertions.assertEquals(2, first.next.data);
-      Assertions.assertEquals(4, first.next.next.data);
-      Assertions.assertEquals(5, first.next.next.next.data);
-      Assertions.assertNull(first.next.next.next.next);
+      Assertions.assertNull(first.next.next);
    }
 
 }
