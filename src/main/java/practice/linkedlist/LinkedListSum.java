639e27bb6aab9c94b4540e4d48c6bd68f6581afc
Lov Verma <lovverma001@gmail.com>
2024-02-04 15:40:41 +0530

    [practice/linked-list]: sum of two linked list working solution

--- src/main/java/practice/linkedlist/LinkedListSum.java
+++ src/main/java/practice/linkedlist/LinkedListSum.java
@@ -3,10 +3,65 @@ package practice.linkedlist;
 public class LinkedListSum {
 
 
-   public Node sumList(Node head1, Node head2) {
-      Node sum = new Node();
+   public Node sumList(Node ptr1, Node ptr2) {
+      if (ptr1 == null) {
+         return ptr2;
+      } else if (ptr2 == null) {
+         return ptr1;
+      }
+      Node head = new Node();
+      Node ptr = head;
+      int carry = 0;
+      int sum = ptr1.data + ptr2.data + carry;
+      if (sum > 9) {
+         carry = 1;
+         sum = sum % 10;
+      }
+      head.data = sum;
+      ptr1 = ptr1.next;
+      ptr2 = ptr2.next;
+      while (ptr1 != null && ptr2 != null) {
+         sum = ptr1.data + ptr2.data + carry;
+         if (sum > 9) {
+            carry = 1;
+            sum = sum % 10;
+         } else {
+            carry = 0;
+         }
+         Node node = new Node(sum);
+         ptr.next = node;
+         ptr = node;
+         ptr1 = ptr1.next;
+         ptr2 = ptr2.next;
+      }
+      while (ptr1 != null) {
+         sum = ptr1.data + carry;
+         if (sum > 9) {
+            carry = 1;
+            sum = sum % 10;
+         } else {
+            carry = 0;
+         }
+         Node node = new Node(sum);
+         ptr.next = node;
+         ptr = node;
+         ptr1 = ptr1.next;
+      }
+      while (ptr2 != null) {
+         sum = ptr2.data + carry;
+         if (sum > 9) {
+            carry = 1;
+            sum = sum % 10;
+         } else {
+            carry = 0;
+         }
+         Node node = new Node(sum);
+         ptr.next = node;
+         ptr = node;
+         ptr2 = ptr2.next;
+      }
 
-      return sum;
+      return head;
    }
 
 }
--- src/test/java/practice/linkedlist/LinkedListSumTest.java
+++ src/test/java/practice/linkedlist/LinkedListSumTest.java
@@ -18,14 +18,35 @@ public class LinkedListSumTest {
       Node l22 = new Node(9, l23);
       Node l21 = new Node(5, l22);
 
-      Node l33 = new Node(9, null);
-      Node l32 = new Node(1, l33);
-      Node l31 = new Node(2, l32);
+      Node output = classObj.sumList(l11, l21);
+      Assertions.assertEquals(2, output.data);
+      Assertions.assertEquals(1, output.next.data);
+      Assertions.assertEquals(9, output.next.next.data);
+   }
+
+   @Test
+   void test_sumList_expectListSum7_whenList1Is3AndList2Is4() {
+      Node l11 = new Node(3, null);
+      Node l21 = new Node(4, null);
 
       Node output = classObj.sumList(l11, l21);
-      Assertions.assertEquals(l31, output);
-      Assertions.assertEquals(l32, output.next);
-      Assertions.assertEquals(l33, output.next.next);
+      Assertions.assertEquals(7, output.data);
+      Assertions.assertNull(output.next);
+   }
+
+   @Test
+   void test_sumList_expectListSum640_whenList1Is617AndList2Is23() {
+      Node l13 = new Node(6, null);
+      Node l12 = new Node(1, l13);
+      Node l11 = new Node(7, l12);
+
+      Node l22 = new Node(2, null);
+      Node l21 = new Node(3, l22);
+
+      Node output = classObj.sumList(l11, l21);
+      Assertions.assertEquals(0, output.data);
+      Assertions.assertEquals(4, output.next.data);
+      Assertions.assertEquals(6, output.next.next.data);
    }
 
 }
