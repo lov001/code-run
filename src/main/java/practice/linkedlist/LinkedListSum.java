a5c9af37bde85dabef9569426acfc2871877b5c8
Lov Verma <lovverma001@gmail.com>
2024-02-04 21:00:51 +0530

    [practice/linked-list]: linked list sum when numbers are in actual order

--- src/main/java/practice/linkedlist/LinkedListSum.java
+++ src/main/java/practice/linkedlist/LinkedListSum.java
@@ -55,4 +55,84 @@ public class LinkedListSum {
       return head;
    }
 
+   public Node sumListRecursively(Node l1, Node l2, int carry) {
+      if (l1 == null && l2 == null && carry == 0) {
+         return null;
+      }
+      Node node = new Node();
+      int value = carry;
+      if (l1 != null) {
+         value += l1.data;
+      }
+      if (l2 != null) {
+         value += l2.data;
+      }
+      node.data = value % 10;
+      if (l1 != null || l2 != null) {
+         node.next = sumListRecursively(
+             l1 == null ? null : l1.next,
+             l2 == null ? null : l2.next,
+             value > 9 ? 1 : 0);
+      }
+      return node;
+   }
+
+   class PartialSum {
+
+
+      public Node sum = null;
+      public int carry = 0;
+   }
+
+   public Node addListReverse(Node l1, Node l2) {
+      int length1 = length(l1);
+      int length2 = length(l2);
+      if (length1 < length2) {
+         l1 = padList(l1, length2 - length1);
+      } else if (length1 > length2) {
+         l2 = padList(l2, length1 - length2);
+      }
+      PartialSum sum = addListsHelper(l1, l2);
+      if (sum.carry == 0) {
+         return sum.sum;
+      } else {
+         return insertBefore(sum.sum, sum.carry);
+      }
+   }
+
+   private int length(Node l) {
+      int length = 0;
+      while (l != null) {
+         length++;
+         l = l.next;
+      }
+      return length;
+   }
+
+   private Node padList(Node ptr, int padLength) {
+      Node head = ptr;
+      for (int i = 0; i < padLength; i++) {
+         head = insertBefore(head, 0);
+      }
+      return head;
+   }
+
+   private Node insertBefore(Node list, int data) {
+      Node node = new Node(data);
+      if (list != null) {
+         node.next = list;
+      }
+      return node;
+   }
+
+   private PartialSum addListsHelper(Node l1, Node l2) {
+      if (l1 == null && l2 == null) {
+         return new PartialSum();
+      }
+      PartialSum sum = addListsHelper(l1.next, l2.next);
+      int value = sum.carry + l1.data + l2.data;
+      sum.sum = insertBefore(sum.sum, value % 10);
+      sum.carry = value / 10;
+      return sum;
+   }
 }
--- src/test/java/practice/linkedlist/LinkedListSumTest.java
+++ src/test/java/practice/linkedlist/LinkedListSumTest.java
@@ -22,6 +22,24 @@ public class LinkedListSumTest {
       Assertions.assertEquals(2, output.data);
       Assertions.assertEquals(1, output.next.data);
       Assertions.assertEquals(9, output.next.next.data);
+
+      Node outputRecursively = classObj.sumListRecursively(l11, l21, 0);
+      Assertions.assertEquals(2, outputRecursively.data);
+      Assertions.assertEquals(1, outputRecursively.next.data);
+      Assertions.assertEquals(9, outputRecursively.next.next.data);
+
+      Node ll13 = new Node(7, null);
+      Node ll12 = new Node(1, ll13);
+      Node ll11 = new Node(6, ll12);
+
+      Node ll23 = new Node(5, null);
+      Node ll22 = new Node(9, ll23);
+      Node ll21 = new Node(2, ll22);
+
+      Node outputRecursivelyReverse = classObj.addListReverse(ll11, ll21);
+      Assertions.assertEquals(9, outputRecursivelyReverse.data);
+      Assertions.assertEquals(1, outputRecursivelyReverse.next.data);
+      Assertions.assertEquals(2, outputRecursivelyReverse.next.next.data);
    }
 
    @Test
@@ -39,6 +57,26 @@ public class LinkedListSumTest {
       Assertions.assertEquals(6, output.next.data);
       Assertions.assertEquals(4, output.next.next.data);
       Assertions.assertEquals(1, output.next.next.next.data);
+
+      Node outputRecursively = classObj.sumListRecursively(l11, l21, 0);
+      Assertions.assertEquals(5, outputRecursively.data);
+      Assertions.assertEquals(6, outputRecursively.next.data);
+      Assertions.assertEquals(4, outputRecursively.next.next.data);
+      Assertions.assertEquals(1, outputRecursively.next.next.next.data);
+
+      Node ll13 = new Node(9, null);
+      Node ll12 = new Node(7, ll13);
+      Node ll11 = new Node(8, ll12);
+
+      Node ll23 = new Node(6, null);
+      Node ll22 = new Node(8, ll23);
+      Node ll21 = new Node(5, ll22);
+
+      Node outputRecursivelyReverse = classObj.addListReverse(ll11, ll21);
+      Assertions.assertEquals(1, outputRecursivelyReverse.data);
+      Assertions.assertEquals(4, outputRecursivelyReverse.next.data);
+      Assertions.assertEquals(6, outputRecursivelyReverse.next.next.data);
+      Assertions.assertEquals(5, outputRecursivelyReverse.next.next.next.data);
    }
 
 
@@ -50,6 +88,14 @@ public class LinkedListSumTest {
       Node output = classObj.sumList(l11, l21);
       Assertions.assertEquals(7, output.data);
       Assertions.assertNull(output.next);
+
+      Node outputRecursively = classObj.sumListRecursively(l11, l21, 0);
+      Assertions.assertEquals(7, outputRecursively.data);
+      Assertions.assertNull(outputRecursively.next);
+
+      Node outputRecursivelyReverse = classObj.addListReverse(l11, l21);
+      Assertions.assertEquals(7, outputRecursivelyReverse.data);
+      Assertions.assertNull(outputRecursivelyReverse.next);
    }
 
    @Test
@@ -65,6 +111,23 @@ public class LinkedListSumTest {
       Assertions.assertEquals(0, output.data);
       Assertions.assertEquals(4, output.next.data);
       Assertions.assertEquals(6, output.next.next.data);
+
+      Node outputRecursively = classObj.sumListRecursively(l11, l21, 0);
+      Assertions.assertEquals(0, outputRecursively.data);
+      Assertions.assertEquals(4, outputRecursively.next.data);
+      Assertions.assertEquals(6, outputRecursively.next.next.data);
+
+      Node ll13 = new Node(7, null);
+      Node ll12 = new Node(1, ll13);
+      Node ll11 = new Node(6, ll12);
+
+      Node ll22 = new Node(3, null);
+      Node ll21 = new Node(2, ll22);
+
+      Node outputRecursivelyReverse = classObj.addListReverse(ll11, ll21);
+      Assertions.assertEquals(6, outputRecursivelyReverse.data);
+      Assertions.assertEquals(4, outputRecursivelyReverse.next.data);
+      Assertions.assertEquals(0, outputRecursivelyReverse.next.next.data);
    }
 
    @Test
@@ -80,6 +143,23 @@ public class LinkedListSumTest {
       Assertions.assertEquals(0, output.data);
       Assertions.assertEquals(4, output.next.data);
       Assertions.assertEquals(6, output.next.next.data);
+
+      Node outputRecursively = classObj.sumListRecursively(l11, l21, 0);
+      Assertions.assertEquals(0, outputRecursively.data);
+      Assertions.assertEquals(4, outputRecursively.next.data);
+      Assertions.assertEquals(6, outputRecursively.next.next.data);
+
+      Node ll13 = new Node(7, null);
+      Node ll12 = new Node(1, ll13);
+      Node ll11 = new Node(6, ll12);
+
+      Node ll22 = new Node(3, null);
+      Node ll21 = new Node(2, ll22);
+
+      Node outputRecursivelyReverse = classObj.addListReverse(ll11, ll21);
+      Assertions.assertEquals(6, outputRecursivelyReverse.data);
+      Assertions.assertEquals(4, outputRecursivelyReverse.next.data);
+      Assertions.assertEquals(0, outputRecursivelyReverse.next.next.data);
    }
 
 }
