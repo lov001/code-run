eeafdf290a1b4e0dbdd74a96e9ca8e1ee03cc61f
Lov Verma <lovverma001@gmail.com>
2024-02-06 09:53:08 +0530

    [practice/linked-list]: finding corrupted node in a circular linked list

--- src/main/java/practice/linkedlist/LoopDetection.java
+++ src/main/java/practice/linkedlist/LoopDetection.java
@@ -4,6 +4,22 @@ public class LoopDetection {
 
 
    public Node circularList(Node head) {
+      if (head == null || head.next == null) {
+         return null;
+      }
+      Node slow = head, fast = head;
+      while (fast != null && fast.next != null) {
+         slow = slow.next;
+         fast = fast.next.next;
+         if (fast == slow) {
+            Node ptr = head;
+            while (ptr != slow) {
+               ptr = ptr.next;
+               slow = slow.next;
+            }
+            return slow;
+         }
+      }
       return null;
    }
 
--- src/test/java/practice/linkedlist/LoopDetectionTest.java
+++ src/test/java/practice/linkedlist/LoopDetectionTest.java
@@ -7,7 +7,23 @@ import org.junit.jupiter.api.Test;
 
 public class LoopDetectionTest {
 
+
    LoopDetection classObj = new LoopDetection();
+
+   @Test
+   void test_circularList_expectThree_whenCycleBeginsFromThirdNode() {
+      Node list1 = new Node(1);
+      Node val1 = new Node(2);
+      Node val2 = new Node(3);
+      Node val3 = new Node(4);
+      Node val4 = new Node(5);
+      list1.next = val1;
+      val1.next = val2;
+      val2.next = val3;
+      val3.next = val4;
+      val4.next = val2;
+      assertEquals(val2, classObj.circularList(list1));
+   }
 
    @Test
    void test_circularList_expectOne_whenCycleBeginsFromSecondNode() {
@@ -32,13 +48,13 @@ public class LoopDetectionTest {
    }
 
    @Test
-   void test_circularList_expectMinusOne_whenInputHasNoCycle() {
+   void test_circularList_expectNull_whenInputHasNoCycle() {
       Node list1 = new Node(1);
       assertNull(classObj.circularList(list1));
    }
 
    @Test
-   void test_circularList_expectMinusOne_whenInputHasTwoNodesAndNoCycleExists() {
+   void test_circularList_expectNull_whenInputHasTwoNodesAndNoCycleExists() {
       Node val1 = new Node(1);
       val1.next = new Node(2);
       assertNull(classObj.circularList(val1));
