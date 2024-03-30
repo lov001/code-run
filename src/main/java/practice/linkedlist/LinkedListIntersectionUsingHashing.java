ff1e8a84a6ddc6aebe843f2e997dbb87a39ed990
23076 <23076@omnicell.com>
2024-03-30 13:22:05 +0530

    [practice/linked-list]: intersection using two pointer approach

--- src/main/java/practice/linkedlist/LinkedListIntersectionUsingHashing.java
+++ src/main/java/practice/linkedlist/LinkedListIntersectionUsingHashing.java
@@ -6,19 +6,41 @@ import java.util.Set;
 public class LinkedListIntersectionUsingHashing {
 
 
-   public Node intersection(Node listA, Node listB) {
+   public Node intersectionWithHashSet(Node listA, Node listB) {
       Set<Node> listSet = new HashSet<>();
       while (listA != null) {
          listSet.add(listA);
          listA = listA.next;
       }
       while (listB != null) {
-         if(listSet.contains(listB)){
+         if (listSet.contains(listB)) {
             return listB;
          }
          listB = listB.next;
       }
       return null;
+   }
+
+   public Node intersectionWithTwoPointers(Node listA, Node listB) {
+      Node ptr1 = listA;
+      Node ptr2 = listB;
+      if (ptr1 == null || ptr2 == null) {
+         return null;
+      }
+      while (ptr1 != ptr2) {
+         ptr1 = ptr1.next;
+         ptr2 = ptr2.next;
+         if (ptr1 == ptr2) {
+            return ptr1;
+         }
+         if (ptr1 == null) {
+            ptr1 = listB;
+         }
+         if (ptr2 == null) {
+            ptr2 = listA;
+         }
+      }
+      return ptr1;
    }
 
 }
