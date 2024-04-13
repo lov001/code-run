4546ae1294b331c35e9fe0daac3980584f7a3343
23076 <23076@omnicell.com>
2024-04-13 13:55:33 +0530

    [practice/trees-and-graphs]: pre-orde traversal of binary tree

--- src/test/java/practice/trees/InOrderTraversalTest.java
+++ src/test/java/practice/trees/InOrderTraversalTest.java
@@ -9,25 +9,23 @@ import org.junit.jupiter.api.Test;
 
 public class InOrderTraversalTest {
 
-   InOrderTraversal classObj = new InOrderTraversal();
-
    @Test
    void test_inorderTraversal_expectOrderedList_whenTreeHasNodes() {
       BinaryTreeNode left = new BinaryTreeNode(3, null, null);
       BinaryTreeNode right = new BinaryTreeNode(2, left, null);
       BinaryTreeNode root = new BinaryTreeNode(1, null, right);
-      Assertions.assertEquals(Arrays.asList(1, 3, 2), classObj.inOrderTraversal(root));
+      Assertions.assertEquals(Arrays.asList(1, 3, 2), InOrderTraversal.inOrderTraversal(root));
    }
 
    @Test
    void test_inorderTraversal_expectEmptyList_whenTreeHasNoNodes() {
-      Assertions.assertEquals(Collections.emptyList(), classObj.inOrderTraversal(null));
+      Assertions.assertEquals(Collections.emptyList(), InOrderTraversal.inOrderTraversal(null));
    }
 
    @Test
    void test_inorderTraversal_expectOnlyRootNode_whenTreeHasOnlyRootNode() {
       BinaryTreeNode root = new BinaryTreeNode(1, null, null);
-      Assertions.assertEquals(List.of(1), classObj.inOrderTraversal(root));
+      Assertions.assertEquals(List.of(1), InOrderTraversal.inOrderTraversal(root));
    }
 
    @Test
@@ -39,7 +37,8 @@ public class InOrderTraversalTest {
       BinaryTreeNode left1 = new BinaryTreeNode(14, left11, null);
       BinaryTreeNode tree1 = new BinaryTreeNode(25, left1, right1);
 
-      Assertions.assertEquals(List.of(8, 14, 25, 28, 30, 50), classObj.inOrderTraversal(tree1));
+      Assertions.assertEquals(List.of(8, 14, 25, 28, 30, 50),
+         InOrderTraversal.inOrderTraversal(tree1));
    }
 
 }
