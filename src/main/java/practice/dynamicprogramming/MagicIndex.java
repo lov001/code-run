28a52a1f34835730dec1a6fcd1b63922cb44d8e6
23076 <23076@omnicell.com>
2024-05-19 17:03:08 +0530

    [practice/dynamic-programming-recursion]: magic index optimised solution with binary search

--- src/main/java/practice/dynamicprogramming/MagicIndex.java
+++ src/main/java/practice/dynamicprogramming/MagicIndex.java
@@ -10,4 +10,29 @@ public class MagicIndex {
       }
       return -1;
    }
+
+   public int magicIndexUsingBinarySearch(int[] array) {
+      return search(array, 0, array.length - 1);
+   }
+
+   private int search(int[] array, int start, int end) {
+      if (start > end) {
+         return -1;
+      }
+      int midIndex = (start + end) / 2;
+      int midValue = array[midIndex];
+
+      if (midValue == midIndex) {
+         return midIndex;
+      }
+
+      int leftIndex = Math.min(midIndex - 1, midValue);
+      int left = search(array, start, leftIndex);
+      if (left >= 0) {
+         return left;
+      }
+
+      int rightIndex = Math.max(midIndex + 1, midValue);
+      return search(array, rightIndex, end);
+   }
 }
--- src/test/java/practice/dynamicprogramming/MagicIndexTest.java
+++ src/test/java/practice/dynamicprogramming/MagicIndexTest.java
@@ -11,11 +11,27 @@ public class MagicIndexTest {
    void test_expectMinusOne_whenMagicIndexNotPresent() {
       int[] input = {1, 2, 3, 4, 5};
       Assertions.assertEquals(-1, classObj.magicIndexBF(input));
+      Assertions.assertEquals(-1, classObj.magicIndexUsingBinarySearch(input));
    }
 
    @Test
    void test_expect3_whenMagicIndexPresent() {
       int[] input = {-5, 0, 1, 3, 8, 15};
       Assertions.assertEquals(3, classObj.magicIndexBF(input));
+      Assertions.assertEquals(3, classObj.magicIndexUsingBinarySearch(input));
+   }
+
+   @Test
+   void test_expect7_whenMagicIndexPresent() {
+      int[] input = {-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13};
+      Assertions.assertEquals(7, classObj.magicIndexBF(input));
+      Assertions.assertEquals(7, classObj.magicIndexUsingBinarySearch(input));
+   }
+
+   @Test
+   void test_expect2_whenMagicIndexPresentAndArrayContainsDuplicate() {
+      int[] input = {-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13};
+      Assertions.assertEquals(2, classObj.magicIndexBF(input));
+      Assertions.assertEquals(2, classObj.magicIndexUsingBinarySearch(input));
    }
 }
