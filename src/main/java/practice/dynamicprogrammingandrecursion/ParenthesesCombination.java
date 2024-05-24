69d2d5e8fd1279da23698eacd0e896247bd08b5e
23076 <23076@omnicell.com>
2024-05-24 18:59:24 +0530

    [practice/dynamic-programming-recursion]: combination of parentheses recursive approach

--- src/main/java/practice/dynamicprogrammingandrecursion/ParenthesesCombination.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/ParenthesesCombination.java
@@ -1,6 +1,8 @@
 package practice.dynamicprogrammingandrecursion;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ParenthesesCombination {
@@ -29,5 +31,27 @@ public class ParenthesesCombination {
       String left = str.substring(0, leftIndex + 1);
       String right = str.substring(leftIndex + 1);
       return left + "()" + right;
+   }
+
+   public List<String> generateParenthesesRecursive(int count) {
+      char[] str = new char[count * 2];
+      List<String> result = new ArrayList<>();
+      addParentheses(result, count, count, str, 0);
+      return result;
+   }
+
+   private void addParentheses(List<String> result, int leftRem, int rightRem, char[] str,
+      int index) {
+      if (leftRem < 0 || rightRem < leftRem) {
+         return;
+      } else if (leftRem == 0 && rightRem == 0) {
+         result.add(String.copyValueOf(str));
+      } else {
+         str[index] = '(';
+         addParentheses(result, leftRem - 1, rightRem, str, index + 1);
+
+         str[index] = ')';
+         addParentheses(result, leftRem, rightRem - 1, str, index + 1);
+      }
    }
 }
--- src/test/java/practice/dynamicprogrammingandrecursion/ParenthesesCombinationTest.java
+++ src/test/java/practice/dynamicprogrammingandrecursion/ParenthesesCombinationTest.java
@@ -1,5 +1,6 @@
 package practice.dynamicprogrammingandrecursion;
 
+import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,17 +13,22 @@ public class ParenthesesCombinationTest {
    void test_generateParentheses_expect_whenNIs1() {
       Set<String> output = Set.of("()");
       Assertions.assertEquals(output, classObj.generateParentheses(1));
+      Assertions.assertEquals(output.stream().toList(), classObj.generateParenthesesRecursive(1));
    }
 
    @Test
    void test_generateParentheses_expect_whenNIs2() {
       Set<String> output = Set.of("()()", "(())");
+      List<String> output2 = List.of("(())", "()()");
       Assertions.assertEquals(output, classObj.generateParentheses(2));
+      Assertions.assertEquals(output2.stream().toList(), classObj.generateParenthesesRecursive(2));
    }
 
    @Test
    void test_generateParentheses_expect_whenNIs3() {
       Set<String> output = Set.of("()()()", "((()))", "(()())", "()(())", "(())()");
+      List<String> output2 = List.of("((()))", "(()())", "(())()", "()(())", "()()()");
       Assertions.assertEquals(output, classObj.generateParentheses(3));
+      Assertions.assertEquals(output2.stream().toList(), classObj.generateParenthesesRecursive(3));
    }
 }
