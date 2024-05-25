251bbff176a0c66e3450f3d1f2f7433533cdaaef
23076 <23076@omnicell.com>
2024-05-25 19:12:09 +0530

    [practice/dynamic-programming-recursion]: coins to generate a change value - optimised approach

--- src/test/java/practice/dynamicprogrammingandrecursion/CoinsTest.java
+++ src/test/java/practice/dynamicprogrammingandrecursion/CoinsTest.java
@@ -10,15 +10,18 @@ public class CoinsTest {
    @Test
    void test_makeChange_expect242_whenNIs100() {
       Assertions.assertEquals(242, classObj.makeChange(100));
+      Assertions.assertEquals(242, classObj.makeChangeOptimised(100));
    }
 
    @Test
    void test_makeChange_expect1_whenNIs1() {
       Assertions.assertEquals(1, classObj.makeChange(1));
+      Assertions.assertEquals(1, classObj.makeChangeOptimised(1));
    }
 
    @Test
    void test_makeChange_expect_whenNIs33() {
       Assertions.assertEquals(18, classObj.makeChange(33));
+      Assertions.assertEquals(18, classObj.makeChangeOptimised(33));
    }
 }
