236993c78013fc1c7745ef930071aad9ad4003ed
23076 <23076@omnicell.com>
2024-05-26 18:32:32 +0530

    [practice/dynamic-programming-recursion]: placing stack of boxes one over another

--- src/main/java/practice/dynamicprogrammingandrecursion/StackOfBoxes.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/StackOfBoxes.java
@@ -1,5 +1,29 @@
 package practice.dynamicprogrammingandrecursion;
 
+import java.util.List;
+
 public class StackOfBoxes {
 
+   public int getMaxHeight(List<Box> boxes) {
+      boxes.sort(new BoxComparator());
+      int maxHeight = 0;
+      for (int i = 0; i < boxes.size(); i++) {
+         int height = createStack(boxes, i);
+         maxHeight = Math.max(maxHeight, height);
+      }
+      return maxHeight;
+   }
+
+   private int createStack(List<Box> boxes, int bottomBoxIndex) {
+      Box bottomBox = boxes.get(bottomBoxIndex);
+      int maxHeight = 0;
+      for (int i = bottomBoxIndex + 1; i < boxes.size(); i++) {
+         if (boxes.get(i).canBeAbove(bottomBox)) {
+            int height = createStack(boxes, i);
+            maxHeight = Math.max(height, maxHeight);
+         }
+      }
+      maxHeight += bottomBox.height;
+      return maxHeight;
+   }
 }
---
+++ src/test/java/practice/dynamicprogrammingandrecursion/StackOfBoxesTest.java
@@ -0,0 +1,47 @@
+package practice.dynamicprogrammingandrecursion;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StackOfBoxesTest {
+
+   StackOfBoxes classObj = new StackOfBoxes();
+
+   @Test
+   void test_getMaxHeight_expect11_when4Boxes() {
+      Box box1 = new Box(2, 4, 6);
+      Box box2 = new Box(5, 7, 7);
+      Box box3 = new Box(1, 1, 1);
+      Box box4 = new Box(3, 5, 7);
+      List<Box> boxes = new ArrayList<>();
+      boxes.add(box1);
+      boxes.add(box2);
+      boxes.add(box3);
+      boxes.add(box4);
+      Assertions.assertEquals(11, classObj.getMaxHeight(boxes));
+   }
+
+   @Test
+   void test_getMaxHeight_expect5_whenOnlyOneBoxe() {
+      Box box2 = new Box(5, 7, 7);
+      List<Box> boxes = new ArrayList<>();
+      boxes.add(box2);
+      Assertions.assertEquals(5, classObj.getMaxHeight(boxes));
+   }
+
+   @Test
+   void test_getMaxHeight_expect7_whenOnlyTwoBoxesSatisfyTheConstraint() {
+      Box box1 = new Box(2, 0, 6);
+      Box box2 = new Box(5, 2, 7);
+      Box box3 = new Box(1, 1, 1);
+      Box box4 = new Box(3, 5, 7);
+      List<Box> boxes = new ArrayList<>();
+      boxes.add(box1);
+      boxes.add(box2);
+      boxes.add(box3);
+      boxes.add(box4);
+      Assertions.assertEquals(7, classObj.getMaxHeight(boxes));
+   }
+}
