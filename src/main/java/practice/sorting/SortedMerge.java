7b1d9cf4c1ce3491f866d258256995ee20f88540
23076 <23076@omnicell.com>
2024-06-07 19:00:56 +0530

    [practice/sorting-and-searching]: merge two sorted arrays - optimised

--- src/main/java/practice/sorting/SortedMerge.java
+++ src/main/java/practice/sorting/SortedMerge.java
@@ -2,6 +2,23 @@ package practice.sorting;
 
 public class SortedMerge {
 
+
+   public void sortedMergeOptimised(int[] array1, int[] array2, int length1, int length2) {
+      int index = length1 + length2 - 1;
+      int index1 = length1 - 1;
+      int index2 = length2 - 1;
+      while (index2 >= 0) {
+         if (index1 >= 0 && array1[index1] > array2[index2]) {
+            array1[index] = array1[index1];
+            index1--;
+         } else {
+            array1[index] = array2[index2];
+            index2--;
+         }
+         index--;
+      }
+   }
+
    public void sortedMerge(int[] array1, int[] array2, int length1, int length2) {
       int index = length1 + length2 - 1;
       int index1 = length1 - 1;
