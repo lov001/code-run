f06c7f4233c1ab51e6de616677cd128dc2696d3d
23076 <23076@omnicell.com>
2024-06-14 15:55:57 +0530

    [practice/sorting-and-searching]: searching in a sparse array using modified binary search

--- src/main/java/practice/searching/SparseSearch.java
+++ src/main/java/practice/searching/SparseSearch.java
@@ -12,4 +12,42 @@ public class SparseSearch {
       }
       return -1;
    }
+
+   public int searchOptimised(String[] array, String word) {
+      if (array == null || word == null || word.isEmpty()) {
+         return -1;
+      }
+      return search(array, word, 0, array.length - 1);
+   }
+
+   private int search(String[] array, String word, int first, int last) {
+      if (last < first) {
+         return -1;
+      }
+      int mid = first + (last - first) / 2;
+      if (array[mid].isEmpty()) {
+         int left = mid - 1;
+         int right = mid + 1;
+         while (true) {
+            if (left < first && right > last) {
+               return -1;
+            } else if (left >= first && !array[left].isEmpty()) {
+               mid = left;
+               break;
+            } else if (right <= last && !array[right].isEmpty()) {
+               mid = right;
+               break;
+            }
+            left--;
+            right++;
+         }
+      }
+      if (array[mid].equalsIgnoreCase(word)) {
+         return mid;
+      } else if (array[mid].compareTo(word) < 0) {
+         return search(array, word, mid + 1, last);
+      } else {
+         return search(array, word, first, mid - 1);
+      }
+   }
 }
--- src/test/java/practice/searching/SparseSearchTest.java
+++ src/test/java/practice/searching/SparseSearchTest.java
@@ -11,23 +11,27 @@ public class SparseSearchTest {
    void test_search_expectMinusOne_whenElementNotFound() {
       String[] array = {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
       Assertions.assertEquals(-1, classObj.search(array, "hello"));
+      Assertions.assertEquals(-1, classObj.searchOptimised(array, "hello"));
    }
 
    @Test
    void test_search_expect0_whenFirstElement() {
       String[] array = {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
       Assertions.assertEquals(0, classObj.search(array, "at"));
+      Assertions.assertEquals(0, classObj.searchOptimised(array, "at"));
    }
 
    @Test
    void test_search_expect4_whenFourthElement() {
       String[] array = {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
       Assertions.assertEquals(4, classObj.search(array, "ball"));
+      Assertions.assertEquals(4, classObj.searchOptimised(array, "ball"));
    }
 
    @Test
    void test_search_expect3_whenLastElement() {
       String[] array = {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
       Assertions.assertEquals(10, classObj.search(array, "dad"));
+      Assertions.assertEquals(10, classObj.searchOptimised(array, "dad"));
    }
 }
