b1703f3cdb4e977b33d8c0c0ac3da8670ce2479d
23076 <23076@omnicell.com>
2024-07-07 18:36:50 +0530

    [practice/graph]: Cycle in an undirected graph using BFS - fixed for multiple components

--- src/main/java/practice/graphs/CycleInUndirectedGraph.java
+++ src/main/java/practice/graphs/CycleInUndirectedGraph.java
@@ -8,18 +8,30 @@ public class CycleInUndirectedGraph {
 
    public boolean isCycle(int V, List<List<Integer>> adj) {
       boolean[] isVisited = new boolean[V];
-      Queue<Pair> queue = new LinkedList<>();
-      queue.add(new Pair(0, -1));
-      isVisited[0] = true;
-      while (!queue.isEmpty()) {
-         Pair node = queue.poll();
-         if (isVisited[node.first] && node.second != -1) {
-            return true;
+      for (int i = 0; i < V; i++) {
+         if (!isVisited[i]) {
+            if (detectCycle(i, isVisited, adj)) {
+               return true;
+            }
          }
-         for (Integer neighbour : adj.get(node.first)) {
+      }
+      return false;
+   }
+
+   private boolean detectCycle(int src, boolean[] isVisited, List<List<Integer>> adj) {
+      Queue<Pair> queue = new LinkedList<>();
+      queue.add(new Pair(src, -1));
+      isVisited[src] = true;
+      while (!queue.isEmpty()) {
+         Pair pair = queue.poll();
+         int node = pair.first;
+         int parent = pair.second;
+         for (Integer neighbour : adj.get(node)) {
             if (!isVisited[neighbour]) {
                isVisited[neighbour] = true;
-               queue.add(new Pair(neighbour, node.first));
+               queue.add(new Pair(neighbour, node));
+            } else if (parent != neighbour) {
+               return true;
             }
          }
       }
