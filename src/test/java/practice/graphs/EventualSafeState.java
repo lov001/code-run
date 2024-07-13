12ba5311ff77eb0a9969f0cc2c264a12271e1d93
23076 <23076@omnicell.com>
2024-07-13 08:57:59 +0530

    [practice/graph]: Eventual Safe nodes using DFS

---
+++ src/test/java/practice/graphs/EventualSafeState.java
@@ -0,0 +1,42 @@
+package practice.graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventualSafeState {
+
+   List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj, String algorithm) {
+      List<Integer> safeNodes = new ArrayList<>();
+      boolean[] visited = new boolean[V];
+      int[] pathVisited = new int[V];
+      if ("dfs".equalsIgnoreCase(algorithm)) {
+         for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+               dfs(i, visited, pathVisited, adj);
+            }
+         }
+      }
+      for (int i = 0; i < V; i++) {
+         if (pathVisited[i] == 2) {
+            safeNodes.add(i);
+         }
+      }
+      return safeNodes;
+   }
+
+   private boolean dfs(int src, boolean[] visited, int[] pathVisited, List<List<Integer>> adj) {
+      visited[src] = true;
+      pathVisited[src] = 1;
+      for (Integer neighbour : adj.get(src)) {
+         if (!visited[neighbour]) {
+            if (dfs(neighbour, visited, pathVisited, adj)) {
+               return true;
+            }
+         } else if (pathVisited[neighbour] == 1) {
+            return true;
+         }
+      }
+      pathVisited[src] = 2;
+      return false;
+   }
+}
