2e09acac644227d626c3163633012addf45ec8b2
23076 <23076@omnicell.com>
2024-07-15 12:12:04 +0530

    [practice/graph]: eventual safe state solution using topological sort

--- src/main/java/practice/graphs/EventualSafeState.java
+++ src/main/java/practice/graphs/EventualSafeState.java
@@ -1,7 +1,10 @@
 package practice.graphs;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class EventualSafeState {
 
@@ -11,19 +14,48 @@ public class EventualSafeState {
     */
    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj, String algorithm) {
       List<Integer> safeNodes = new ArrayList<>();
-      boolean[] visited = new boolean[V];
-      int[] pathVisited = new int[V];
       if ("dfs".equalsIgnoreCase(algorithm)) {
+         boolean[] visited = new boolean[V];
+         int[] pathVisited = new int[V];
          for (int i = 0; i < V; i++) {
             if (!visited[i]) {
                dfs(i, visited, pathVisited, adj);
             }
          }
-      }
-      for (int i = 0; i < V; i++) {
-         if (pathVisited[i] == 2) {
-            safeNodes.add(i);
+         for (int i = 0; i < V; i++) {
+            if (pathVisited[i] == 2) {
+               safeNodes.add(i);
+            }
          }
+      } else {
+         List<List<Integer>> updatedAdj = new ArrayList<>();
+         int[] inDegree = new int[V];
+         for (int i = 0; i < V; i++) {
+            updatedAdj.add(new ArrayList<>());
+         }
+         for (int i = 0; i < V; i++) {
+            for (Integer j : adj.get(i)) {
+               updatedAdj.get(j).add(i);
+               inDegree[i]++;
+            }
+         }
+         Queue<Integer> queue = new LinkedList<>();
+         for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0) {
+               queue.add(i);
+            }
+         }
+         while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+            safeNodes.add(node);
+            for (Integer adjacent : updatedAdj.get(node)) {
+               inDegree[adjacent]--;
+               if (inDegree[adjacent] == 0) {
+                  queue.add(adjacent);
+               }
+            }
+         }
+         Collections.sort(safeNodes);
       }
       return safeNodes;
    }
--- src/test/java/practice/graphs/EventualSafeStateTest.java
+++ src/test/java/practice/graphs/EventualSafeStateTest.java
@@ -58,4 +58,54 @@ public class EventualSafeStateTest {
       Assertions.assertEquals(output.size(), actualOutput.size());
       Assertions.assertArrayEquals(new List[]{output}, new List[]{actualOutput});
    }
+
+   @Test
+   void test_eventualSafeNodes_expectListOfSize4_whenEventualNodesAre4AndUsingBfs() {
+      List<List<Integer>> adjacencyList = new ArrayList<>();
+      adjacencyList.add(List.of(1, 2));
+      adjacencyList.add(List.of(2, 3));
+      adjacencyList.add(List.of(5));
+      adjacencyList.add(List.of(0));
+      adjacencyList.add(List.of(5));
+      adjacencyList.add(List.of());
+      adjacencyList.add(List.of());
+      List<Integer> output = List.of(2, 4, 5, 6);
+      List<Integer> actualOutput = classObj.eventualSafeNodes(7, adjacencyList, "bfs");
+      Assertions.assertEquals(output.size(), actualOutput.size());
+      Assertions.assertArrayEquals(new List[]{output}, new List[]{actualOutput});
+   }
+
+   @Test
+   void test_eventualSafeNodes_expectListOfSize8_whenEventualNodesAre8AndUsingBfs() {
+      List<List<Integer>> adjacencyList = new ArrayList<>();
+      adjacencyList.add(List.of(1));
+      adjacencyList.add(List.of(2));
+      adjacencyList.add(List.of(3));
+      adjacencyList.add(List.of(4, 5));
+      adjacencyList.add(List.of(6));
+      adjacencyList.add(List.of(6));
+      adjacencyList.add(List.of(7));
+      adjacencyList.add(List.of());
+      adjacencyList.add(List.of(1, 9));
+      adjacencyList.add(List.of(10));
+      adjacencyList.add(List.of(8));
+      adjacencyList.add(List.of(9));
+      List<Integer> output = List.of(0, 1, 2, 3, 4, 5, 6, 7);
+      List<Integer> actualOutput = classObj.eventualSafeNodes(12, adjacencyList, "bfs");
+      Assertions.assertEquals(output.size(), actualOutput.size());
+      Assertions.assertArrayEquals(new List[]{output}, new List[]{actualOutput});
+   }
+
+   @Test
+   void test_eventualSafeNodes_expectListOfSize1_whenEventualNodesAre1AndUsingBfs() {
+      List<List<Integer>> adjacencyList = new ArrayList<>();
+      adjacencyList.add(List.of(1));
+      adjacencyList.add(List.of(2));
+      adjacencyList.add(List.of(0, 3));
+      adjacencyList.add(List.of());
+      List<Integer> output = List.of(3);
+      List<Integer> actualOutput = classObj.eventualSafeNodes(4, adjacencyList, "bfs");
+      Assertions.assertEquals(output.size(), actualOutput.size());
+      Assertions.assertArrayEquals(new List[]{output}, new List[]{actualOutput});
+   }
 }
