ba0937997f03a6427eef7543f33ac3c38e3883d9
23076 <23076@omnicell.com>
2024-07-23 10:11:14 +0530

    [practice/graph]: Path with minimum effort using Dijkstra's algorithm

--- src/test/java/practice/graphs/ShortestDistanceInBinaryMazeTest.java
+++ src/test/java/practice/graphs/ShortestDistanceInBinaryMazeTest.java
@@ -3,7 +3,7 @@ package practice.graphs;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ShortestDistanceInVinaryMazeTest {
+public class ShortestDistanceInBinaryMazeTest {
 
    ShortestDistanceInBinaryMaze classObj = new ShortestDistanceInBinaryMaze();
 
