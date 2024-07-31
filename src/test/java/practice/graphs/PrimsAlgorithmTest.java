3911d2217c1379f9031f13860869811ebd9b94aa
23076 <23076@omnicell.com>
2024-07-31 09:07:39 +0530

    [practice/graph]: minimum spanning tree using Prim's algorithm updated code with proper input tests

--- src/test/java/practice/graphs/PrimsAlgorithmTest.java
+++ src/test/java/practice/graphs/PrimsAlgorithmTest.java
@@ -11,25 +11,31 @@ public class PrimsAlgorithmTest {
 
    @Test
    void test_spanningTree_expect4_whenNIs3AndVIs3() {
-      int[] array0 = {0, 1, 5};
-      int[] array1 = {1, 2, 3};
-      int[] array2 = {0, 2, 1};
       List<int[]> innerList = new ArrayList<>();
-      innerList.add(array0);
-      innerList.add(array1);
-      innerList.add(array2);
+      innerList.add(new int[]{1, 5});
+      innerList.add(new int[]{2, 1});
+      List<int[]> innerList2 = new ArrayList<>();
+      innerList.add(new int[]{0, 5});
+      innerList.add(new int[]{2, 3});
+      List<int[]> innerList3 = new ArrayList<>();
+      innerList.add(new int[]{1, 3});
+      innerList.add(new int[]{0, 1});
       List<List<int[]>> input = new ArrayList<>();
       input.add(innerList);
+      input.add(innerList2);
+      input.add(innerList3);
       Assertions.assertEquals(4, classObj.spanningTree(3, 3, input));
    }
 
    @Test
    void test_spanningTree_expect5_whenNIs2AndVIs1() {
-      int[] array0 = {0, 1, 5};
       List<int[]> innerList = new ArrayList<>();
-      innerList.add(array0);
+      innerList.add(new int[]{1, 5});
+      List<int[]> innerList2 = new ArrayList<>();
+      innerList.add(new int[]{0, 5});
       List<List<int[]>> input = new ArrayList<>();
       input.add(innerList);
+      input.add(innerList2);
       Assertions.assertEquals(5, classObj.spanningTree(2, 1, input));
    }
 }
