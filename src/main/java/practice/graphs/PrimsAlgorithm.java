04f3f8bd185546473a6a9a0ebccf428d5eab780e
23076 <23076@omnicell.com>
2024-07-31 08:59:58 +0530

    [practice/graph]: minimum spanning tree using Prim's algorithm updated code

--- src/main/java/practice/graphs/PrimsAlgorithm.java
+++ src/main/java/practice/graphs/PrimsAlgorithm.java
@@ -1,6 +1,5 @@
 package practice.graphs;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -9,34 +8,26 @@ import java.util.Queue;
 public class PrimsAlgorithm {
 
    public int spanningTree(int V, int E, List<List<int[]>> adj) {
-      List<List<Pair>> adjacencyList = new ArrayList<>();
-      for (int i = 0; i < V; i++) {
-         adjacencyList.add(new ArrayList<>());
-      }
-      for (int i = 0; i < adj.size(); i++) {
-         for (int[] vertex : adj.get(i)) {
-            adjacencyList.get(vertex[0]).add(new Pair(vertex[1], vertex[2]));
-            adjacencyList.get(vertex[1]).add(new Pair(vertex[0], vertex[2]));
-         }
-      }
-      Queue<Pair> priorityQueue = new PriorityQueue<>(Comparator.comparing(x -> x.first));
-      priorityQueue.add(new Pair(0, 0));
+      Queue<Triplet> priorityQueue = new PriorityQueue<>(Comparator.comparing(x -> x.first));
+      priorityQueue.add(new Triplet(0, 0, -1));
       int minimumWeight = 0;
       int[] visited = new int[V];
+      System.out.println();
       while (!priorityQueue.isEmpty()) {
-         Pair pair = priorityQueue.poll();
-         int wt = pair.first;
-         int node = pair.second;
+         Triplet triplet = priorityQueue.poll();
+         int wt = triplet.first;
+         int node = triplet.second;
+         int parent = triplet.third;
          if (visited[node] == 1) {
             continue;
          }
          visited[node] = 1;
          minimumWeight += wt;
-         for (int i = 0; i < adjacencyList.get(node).size(); i++) {
-            int adjNode = adjacencyList.get(node).get(i).first;
-            int edgeWt = adjacencyList.get(node).get(i).second;
+         for (int i = 0; i < adj.get(node).size(); i++) {
+            int adjNode = adj.get(node).get(i)[0];
+            int edgeWt = adj.get(node).get(i)[1];
             if (visited[adjNode] == 0) {
-               priorityQueue.add(new Pair(edgeWt, adjNode));
+               priorityQueue.add(new Triplet(edgeWt, adjNode, parent));
             }
          }
       }
