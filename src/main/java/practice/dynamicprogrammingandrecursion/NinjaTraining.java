90455d3add238710a98843170a7ccb633aa76262
23076 <23076@omnicell.com>
2024-09-22 19:37:20 +0530

    [practice/dynamic-programming]: fixed one failing test case

--- src/main/java/practice/dynamicprogrammingandrecursion/NinjaTraining.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/NinjaTraining.java
@@ -14,18 +14,19 @@ public class NinjaTraining {
 
    private int helper(int index, int last, int[][] points, int[][] dp) {
       if (index == 0) {
-         int max = Integer.MIN_VALUE;
+         int max = 0;
          for (int i = 0; i < 3; i++) {
             if (i != last) {
                max = Math.max(max, points[0][i]);
             }
          }
+         dp[index][last] = max;
          return max;
       }
       if (dp[index][last] != -1) {
          return dp[index][last];
       }
-      int max = Integer.MIN_VALUE;
+      int max = 0;
       for (int i = 0; i < 3; i++) {
          if (i != last) {
             int currentMax = points[index][i] + helper(index - 1, i, points, dp);
