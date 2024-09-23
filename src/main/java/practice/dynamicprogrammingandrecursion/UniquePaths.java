7754244e7097ae807878e9913f41da251db565e4
23076 <23076@omnicell.com>
2024-09-23 06:37:55 +0530

    [practice/dynamic-programming]: unique paths in a grid using memoization

--- src/main/java/practice/dynamicprogrammingandrecursion/UniquePaths.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/UniquePaths.java
@@ -1,23 +1,32 @@
 package practice.dynamicprogrammingandrecursion;
 
+import java.util.Arrays;
+
 public class UniquePaths {
 
    public int uniquePaths(int m, int n) {
       if (m == 1 && n == 1) {
          return 1;
       }
-      return helper(m - 1, n - 1);
+      int[][] dp = new int[m][n];
+      for (int i = 0; i < m; i++) {
+         Arrays.fill(dp[i], -1);
+      }
+      return helper(m - 1, n - 1, dp);
    }
 
-   private int helper(int row, int column) {
+   private int helper(int row, int column, int[][] dp) {
       if (row == 0 && column == 0) {
          return 1;
       }
       if (row < 0 || column < 0) {
          return 0;
       }
-      int up = helper(row - 1, column);
-      int left = helper(row, column - 1);
-      return up + left;
+      if (dp[row][column] != -1) {
+         return dp[row][column];
+      }
+      int up = helper(row - 1, column, dp);
+      int left = helper(row, column - 1, dp);
+      return dp[row][column] = up + left;
    }
 }
