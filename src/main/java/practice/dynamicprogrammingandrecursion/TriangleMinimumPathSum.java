1c05276cbacbd956042792ce7479f5c68f229476
23076 <23076@omnicell.com>
2024-09-25 07:15:55 +0530

    [practice/dynamic-programming]: minimum path sum in triangle using memoization

--- src/main/java/practice/dynamicprogrammingandrecursion/TriangleMinimumPathSum.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/TriangleMinimumPathSum.java
@@ -1,18 +1,26 @@
 package practice.dynamicprogrammingandrecursion;
 
+import java.util.Arrays;
+
 public class TriangleMinimumPathSum {
 
    public int minimumPathSum(int[][] triangle, int n) {
-//      int[][] dp = new
-      return helper(0, 0, n, triangle);
+      int[][] dp = new int[n][n];
+      for (int[] r : dp) {
+         Arrays.fill(r, -1);
+      }
+      return helper(0, 0, n, triangle, dp);
    }
 
-   private int helper(int row, int column, int n, int[][] triangle) {
+   private int helper(int row, int column, int n, int[][] triangle, int[][] dp) {
       if (row == n - 1) {
          return triangle[row][column];
       }
-      int down = triangle[row][column] + helper(row + 1, column, n, triangle);
-      int diagonal = triangle[row][column] + helper(row + 1, column + 1, n, triangle);
-      return Math.min(down, diagonal);
+      if (dp[row][column] != -1) {
+         return dp[row][column];
+      }
+      int down = triangle[row][column] + helper(row + 1, column, n, triangle, dp);
+      int diagonal = triangle[row][column] + helper(row + 1, column + 1, n, triangle, dp);
+      return dp[row][column] = Math.min(down, diagonal);
    }
 }
