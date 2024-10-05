a00aeb648b0b9993345bce2a8ff610e8a5243b24
23076 <23076@omnicell.com>
2024-10-05 12:46:02 +0530

    [practice/dynamic-programming]: pre order traversal using iterative method

--- src/test/java/practice/trees/PreOrderTraversalTest.java
+++ src/test/java/practice/trees/PreOrderTraversalTest.java
@@ -17,17 +17,20 @@ public class PreOrderTraversalTest {
       BinaryTreeNode right = new BinaryTreeNode(2, left, null);
       BinaryTreeNode root = new BinaryTreeNode(1, null, right);
       Assertions.assertEquals(Arrays.asList(1, 2, 3), classObj.preOrderTraversal(root));
+      Assertions.assertEquals(Arrays.asList(1, 2, 3), classObj.preOrderTraversalIterative(root));
    }
 
    @Test
    void test_preOrderTraversal_expectEmptyList_whenTreeHasNoNodes() {
       Assertions.assertEquals(Collections.emptyList(), classObj.preOrderTraversal(null));
+      Assertions.assertEquals(Collections.emptyList(), classObj.preOrderTraversalIterative(null));
    }
 
    @Test
    void test_preOrderTraversal_expectOnlyRootNode_whenTreeHasOnlyRootNode() {
       BinaryTreeNode root = new BinaryTreeNode(1, null, null);
       Assertions.assertEquals(List.of(1), classObj.preOrderTraversal(root));
+      Assertions.assertEquals(List.of(1), classObj.preOrderTraversalIterative(root));
    }
 
    @Test
@@ -37,6 +40,7 @@ public class PreOrderTraversalTest {
       BinaryTreeNode right = new BinaryTreeNode(3, null, null);
       BinaryTreeNode root = new BinaryTreeNode(1, left, right);
       Assertions.assertEquals(Arrays.asList(1, 4, 2, 3), classObj.preOrderTraversal(root));
+      Assertions.assertEquals(Arrays.asList(1, 4, 2, 3), classObj.preOrderTraversalIterative(root));
    }
 
    @Test
@@ -49,6 +53,8 @@ public class PreOrderTraversalTest {
       BinaryTreeNode tree1 = new BinaryTreeNode(25, left1, right1);
 
       Assertions.assertEquals(List.of(25, 14, 8, 30, 28, 50), classObj.preOrderTraversal(tree1));
+      Assertions.assertEquals(List.of(25, 14, 8, 30, 28, 50),
+         classObj.preOrderTraversalIterative(tree1));
    }
 
 }
