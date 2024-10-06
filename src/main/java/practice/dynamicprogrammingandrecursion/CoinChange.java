58c006add853d0d73be64eff25d0e4fba070f0ea
23076 <23076@omnicell.com>
2024-10-06 13:31:23 +0530

    [practice/dynamic-programming]: number of ways in coins can make the amount using tabulation

--- src/main/java/practice/dynamicprogrammingandrecursion/CoinChange.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/CoinChange.java
@@ -4,6 +4,25 @@ import java.util.Arrays;
 
 public class CoinChange {
 
+   public long countWaysToMakeChangeTabulation(int[] denominations, int value) {
+      int n = denominations.length;
+      long[][] dp = new long[n][value + 1];
+      for (int T = 0; T <= value; T++) {
+         dp[0][T] = T % denominations[0] == 0 ? 1 : 0;
+      }
+      for (int index = 1; index < n; index++) {
+         for (int target = 0; target <= value; target++) {
+            long notPick = dp[index - 1][target];
+            long pick = 0;
+            if (denominations[index] <= target) {
+               pick = dp[index][target - denominations[index]];
+            }
+            dp[index][target] = notPick + pick;
+         }
+      }
+      return dp[n - 1][value];
+   }
+
    public long countWaysToMakeChange(int[] denominations, int value) {
       int n = denominations.length;
       long[][] dp = new long[n][value + 1];
