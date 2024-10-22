2e2d5531d886319cafde689be67efe5034b55baf
23076 <23076@omnicell.com>
2024-10-22 07:31:22 +0530

    [practice/binary-search]: best time to buy sell stock 3 using menoization

--- src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuyAndSellStock3.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuyAndSellStock3.java
@@ -1,5 +1,7 @@
 package practice.dynamicprogrammingandrecursion;
 
+import java.util.Arrays;
+
 public class BestTimeToBuyAndSellStock3 {
 
    public int maxProfit(int[] prices) {
@@ -7,24 +9,33 @@ public class BestTimeToBuyAndSellStock3 {
       if (n == 0) {
          return 0;
       }
-      return profit(0, true, 2, n, prices);
+      int[][][] dp = new int[n + 1][2][3];
+      for (int[][] r : dp) {
+         for (int[] column : r) {
+            Arrays.fill(column, -1);
+         }
+      }
+      return profit(0, 1, 2, n, prices, dp);
    }
 
-   private int profit(int index, boolean buy, int maxTransactions, int n, int[] prices) {
-      if (index == n) {
+   private int profit(int index, int buy, int maxTransactions, int n, int[] prices,
+      int[][][] dp) {
+      if (index == n || maxTransactions == 0) {
          return 0;
       }
-      if (maxTransactions == 0) {
-         return 0;
+      if (dp[index][buy][maxTransactions] != -1) {
+         return dp[index][buy][maxTransactions];
       }
       int profit;
-      if (buy) {
-         profit = Math.max(-prices[index] + profit(index + 1, false, maxTransactions, n, prices),
-            profit(index + 1, true, maxTransactions, n, prices));
+      if (buy == 1) {
+         profit = Math.max(
+            -prices[index] + profit(index + 1, 0, maxTransactions, n, prices, dp),
+            profit(index + 1, 1, maxTransactions, n, prices, dp));
       } else {
-         profit = Math.max(prices[index] + profit(index + 1, true, maxTransactions - 1, n, prices),
-            profit(index + 1, false, maxTransactions, n, prices));
+         profit = Math.max(
+            prices[index] + profit(index + 1, 1, maxTransactions - 1, n, prices, dp),
+            profit(index + 1, 0, maxTransactions, n, prices, dp));
       }
-      return profit;
+      return dp[index][buy][maxTransactions] = profit;
    }
 }
