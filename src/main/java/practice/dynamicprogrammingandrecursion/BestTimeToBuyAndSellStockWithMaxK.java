0eb849b56e3c8e923d7139c2dde31d845193a5ff
23076 <23076@omnicell.com>
2024-10-24 20:58:06 +0530

    [practice/binary-search]: best time to buy sell stock 4 using tabulation

--- src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuyAndSellStockWithMaxK.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuyAndSellStockWithMaxK.java
@@ -4,6 +4,29 @@ import java.util.Arrays;
 
 public class BestTimeToBuyAndSellStockWithMaxK {
 
+   public int maxProfitTabulation(int[] prices, int n, int k) {
+      if (n == 0) {
+         return 0;
+      }
+      int[][][] dp = new int[n + 1][2][k + 1];
+      for (int index = n - 1; index >= 0; index--) {
+         for (int buy = 0; buy < 2; buy++) {
+            for (int maxTransactions = 1; maxTransactions <= k; maxTransactions++) {
+               if (buy == 1) {
+                  dp[index][buy][maxTransactions] = Math.max(
+                     -prices[index] + dp[index + 1][0][maxTransactions],
+                     dp[index + 1][1][maxTransactions]);
+               } else {
+                  dp[index][buy][maxTransactions] += Math.max(
+                     prices[index] + dp[index + 1][1][maxTransactions - 1],
+                     dp[index + 1][0][maxTransactions]);
+               }
+            }
+         }
+      }
+      return dp[0][1][k];
+   }
+
    public int maxProfit(int[] prices, int n, int k) {
       if (n == 0) {
          return 0;
