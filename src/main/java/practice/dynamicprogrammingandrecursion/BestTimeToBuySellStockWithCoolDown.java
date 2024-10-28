92c6e76eab5c46473dcff3667855e7188598e981
23076 <23076@omnicell.com>
2024-10-28 18:54:27 +0530

    [practice/binary-search]: buy and sell stock with cooldown with memoization

--- src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithCoolDown.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithCoolDown.java
@@ -1,24 +1,32 @@
 package practice.dynamicprogrammingandrecursion;
 
+import java.util.Arrays;
+
 public class BestTimeToBuySellStockWithCoolDown {
 
    public int stockProfit(int[] prices) {
       int n = prices.length;
-      return helper(0, 1, n, prices);
+      int[][] dp = new int[n + 1][2];
+      for (int[] rows : dp) {
+         Arrays.fill(rows, -1);
+      }
+      return helper(0, 1, n, prices, dp);
    }
 
-   private int helper(int index, int buy, int n, int[] prices) {
+   private int helper(int index, int buy, int n, int[] prices, int[][] dp) {
       if (index >= n) {
          return 0;
       }
-      int profit;
-      if (buy == 1) {
-         profit = Math.max(-prices[index] + helper(index + 1, 0, n, prices),
-            helper(index + 1, buy, n, prices));
-      } else {
-         profit = Math.max(prices[index] + helper(index + 2, 1, n, prices),
-            helper(index + 1, buy, n, prices));
+      if (dp[index][buy] != -1) {
+         return dp[index][buy];
       }
-      return profit;
+      if (buy == 1) {
+         dp[index][buy] = Math.max(-prices[index] + helper(index + 1, 0, n, prices, dp),
+            helper(index + 1, buy, n, prices, dp));
+      } else {
+         dp[index][buy] = Math.max(prices[index] + helper(index + 2, 1, n, prices, dp),
+            helper(index + 1, buy, n, prices, dp));
+      }
+      return dp[index][buy];
    }
 }
