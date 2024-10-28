22ed36790c88e4132a91897081d61a734cc13974
23076 <23076@omnicell.com>
2024-10-28 23:47:49 +0530

    [practice/binary-search]: buy and sell stock with cool down with tabulation

--- src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithCoolDown.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithCoolDown.java
@@ -4,6 +4,23 @@ import java.util.Arrays;
 
 public class BestTimeToBuySellStockWithCoolDown {
 
+   public int stockProfitTabulation(int[] prices) {
+      int n = prices.length;
+      int[][] dp = new int[n + 2][2];
+      for (int index = n - 1; index >= 0; index--) {
+         for (int buy = 0; buy <= 1; buy++) {
+            if (buy == 1) {
+               dp[index][buy] = Math.max(-prices[index] + dp[index + 1][0],
+                  dp[index + 1][1]);
+            } else {
+               dp[index][buy] = Math.max(prices[index] + dp[index + 2][1],
+                  dp[index + 1][0]);
+            }
+         }
+      }
+      return dp[0][1];
+   }
+
    public int stockProfit(int[] prices) {
       int n = prices.length;
       int[][] dp = new int[n + 1][2];
--- src/test/java/practice/dynamicprogrammingandrecursion/BestTimeToBuyAndSellStockWithCoolDownTest.java
+++ src/test/java/practice/dynamicprogrammingandrecursion/BestTimeToBuyAndSellStockWithCoolDownTest.java
@@ -11,17 +11,20 @@ public class BestTimeToBuyAndSellStockWithCoolDownTest {
    void test_maximumProfit_expect11_whenNIs5() {
       int[] values = {4, 9, 0, 4, 10};
       Assertions.assertEquals(11, classObj.stockProfit(values));
+      Assertions.assertEquals(11, classObj.stockProfitTabulation(values));
    }
 
    @Test
    void test_maximumProfit_expect3_whenNIs4() {
       int[] values = {1, 2, 3, 4};
       Assertions.assertEquals(3, classObj.stockProfit(values));
+      Assertions.assertEquals(3, classObj.stockProfitTabulation(values));
    }
 
    @Test
    void test_maximumProfit_expect0_whenNIs3() {
       int[] values = {5, 4, 3};
       Assertions.assertEquals(0, classObj.stockProfit(values));
+      Assertions.assertEquals(0, classObj.stockProfitTabulation(values));
    }
 }
