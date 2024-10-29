4fb6d0a2f197bf34c785631025786032ed99b552
23076 <23076@omnicell.com>
2024-10-29 20:03:20 +0530

    [practice/binary-search]: buy and sell stock with transaction fee with tabulation and space optimisation

--- src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithTransactionFee.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithTransactionFee.java
@@ -4,6 +4,42 @@ import java.util.Arrays;
 
 public class BestTimeToBuySellStockWithTransactionFee {
 
+   public int maximumProfitTabulation(int[] prices, int n, int fee) {
+      if (n == 0) {
+         return 0;
+      }
+      int[][] dp = new int[n + 1][2];
+      for (int index = n - 1; index >= 0; index--) {
+         for (int buy = 0; buy <= 1; buy++) {
+            if (buy == 1) {
+               dp[index][buy] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+            } else {
+               dp[index][buy] = Math.max(prices[index] - fee + dp[index + 1][1], dp[index + 1][0]);
+            }
+         }
+      }
+      return dp[0][1];
+   }
+
+   public int maximumProfitSpaceOptimisation(int[] prices, int n, int fee) {
+      if (n == 0) {
+         return 0;
+      }
+      int[] current = new int[2];
+      int[] ahead = new int[2];
+      for (int index = n - 1; index >= 0; index--) {
+         for (int buy = 0; buy <= 1; buy++) {
+            if (buy == 1) {
+               current[buy] = Math.max(-prices[index] + ahead[0], ahead[1]);
+            } else {
+               current[buy] = Math.max(prices[index] - fee + ahead[1], ahead[0]);
+            }
+         }
+         ahead = current;
+      }
+      return current[1];
+   }
+
    public long maximumProfit(int[] prices, int n, int fee) {
       if (n == 0) {
          return 0;
--- src/test/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithTransactionFeeTest.java
+++ src/test/java/practice/dynamicprogrammingandrecursion/BestTimeToBuySellStockWithTransactionFeeTest.java
@@ -11,11 +11,15 @@ public class BestTimeToBuySellStockWithTransactionFeeTest {
    void test_maximumProfit_expect1_whenNIs7AndFeeIs1() {
       int[] values = {1, 2, 3};
       Assertions.assertEquals(1, classObj.maximumProfit(values, 3, 1));
+      Assertions.assertEquals(1, classObj.maximumProfitTabulation(values, 3, 1));
+      Assertions.assertEquals(1, classObj.maximumProfitSpaceOptimisation(values, 3, 1));
    }
 
    @Test
    void test_maximumProfit_expect3_whenNIs7AndFeeIs2() {
       int[] values = {1, 3, 5, 6};
       Assertions.assertEquals(3, classObj.maximumProfit(values, 4, 2));
+      Assertions.assertEquals(3, classObj.maximumProfitTabulation(values, 4, 2));
+      Assertions.assertEquals(3, classObj.maximumProfitSpaceOptimisation(values, 4, 2));
    }
 }
