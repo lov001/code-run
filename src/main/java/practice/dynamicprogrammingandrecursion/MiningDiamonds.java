78b18c2f7b47fa1da92445ebee31a3257998e69a
23076 <23076@omnicell.com>
2024-11-05 17:51:52 +0530

    [practice/binary-search]: cost to mine the diamonds using tabulation

--- src/main/java/practice/dynamicprogrammingandrecursion/MiningDiamonds.java
+++ src/main/java/practice/dynamicprogrammingandrecursion/MiningDiamonds.java
@@ -4,6 +4,30 @@ import java.util.Arrays;
 
 public class MiningDiamonds {
 
+   public int maxCoinsTabulation(int[] a) {
+      int n = a.length;
+      int[] diamonds = new int[n + 2];
+      diamonds[0] = 1;
+      diamonds[diamonds.length - 1] = 1;
+      System.arraycopy(a, 0, diamonds, 1, n);
+      int[][] dp = new int[n + 2][n + 2];
+      for (int i = n; i >= 1; i--) {
+         for (int j = 1; j <= n; j++) {
+            if (i > j) {
+               continue;
+            }
+            int maxCoins = Integer.MIN_VALUE;
+            for (int index = i; index <= j; index++) {
+               int cost = diamonds[i - 1] * diamonds[index] * diamonds[j + 1]
+                  + dp[i][index - 1] + dp[index + 1][j];
+               maxCoins = Math.max(maxCoins, cost);
+            }
+            dp[i][j] = maxCoins;
+         }
+      }
+      return dp[1][n];
+   }
+
    public int maxCoins(int[] a) {
       int n = a.length;
       int[] updatedArray = new int[n + 2];
--- src/test/java/practice/dynamicprogrammingandrecursion/MiningDiamondsTest.java
+++ src/test/java/practice/dynamicprogrammingandrecursion/MiningDiamondsTest.java
@@ -11,23 +11,27 @@ public class MiningDiamondsTest {
    void test_maxCoins_expect18_whenNIs2() {
       int[] input = {9, 1};
       Assertions.assertEquals(18, classObj.maxCoins(input));
+      Assertions.assertEquals(18, classObj.maxCoinsTabulation(input));
    }
 
    @Test
    void test_maxCoins_expect120_whenNIs3() {
       int[] input = {7, 1, 8};
       Assertions.assertEquals(120, classObj.maxCoins(input));
+      Assertions.assertEquals(120, classObj.maxCoinsTabulation(input));
    }
 
    @Test
    void test_maxCoins_expect110_whenNIs5() {
       int[] input = {1, 2, 3, 4, 5};
       Assertions.assertEquals(110, classObj.maxCoins(input));
+      Assertions.assertEquals(110, classObj.maxCoinsTabulation(input));
    }
 
    @Test
    void test_maxCoins_expect136_whenNIs4() {
       int[] input = {1, 5, 2, 8};
       Assertions.assertEquals(136, classObj.maxCoins(input));
+      Assertions.assertEquals(136, classObj.maxCoinsTabulation(input));
    }
 }
