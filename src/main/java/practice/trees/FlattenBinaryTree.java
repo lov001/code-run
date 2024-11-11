79b612be8b142c6f7aef8ec4acf0b729bb7f6ded
23076 <23076@omnicell.com>
2024-11-11 19:06:44 +0530

    [practice/binary-trees]: flatten a binary tree using iterative approach

--- src/main/java/practice/trees/FlattenBinaryTree.java
+++ src/main/java/practice/trees/FlattenBinaryTree.java
@@ -1,8 +1,29 @@
 package practice.trees;
 
+import java.util.Stack;
+
 public class FlattenBinaryTree {
 
    static BinaryTreeNode prev = null;
+
+   public BinaryTreeNode flattenBinaryTreeIterative(BinaryTreeNode root) {
+      Stack<BinaryTreeNode> stack = new Stack<>();
+      stack.push(root);
+      while (!stack.isEmpty()) {
+         BinaryTreeNode current = stack.pop();
+         if (current.right != null) {
+            stack.push(current.right);
+         }
+         if (current.left != null) {
+            stack.push(current.left);
+         }
+         if (!stack.isEmpty()) {
+            current.right = stack.peek();
+         }
+         current.left = null;
+      }
+      return root;
+   }
 
    public BinaryTreeNode flattenBinaryTree(BinaryTreeNode root) {
       traverse(root);
