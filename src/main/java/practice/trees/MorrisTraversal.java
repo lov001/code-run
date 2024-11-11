45f829840f3841173aa23cce4fe7e7adf2f91931
23076 <23076@omnicell.com>
2024-11-11 18:19:29 +0530

    [practice/binary-trees]: pre-order traversal via morris traversal

--- src/main/java/practice/trees/MorrisTraversal.java
+++ src/main/java/practice/trees/MorrisTraversal.java
@@ -29,4 +29,29 @@ public class MorrisTraversal {
       }
       return inorder;
    }
+
+   public static List<Integer> getPreOrderTraversal(BinaryTreeNode root) {
+      BinaryTreeNode current = root;
+      List<Integer> inorder = new ArrayList<>();
+      while (current != null) {
+         if (current.left == null) {
+            inorder.add(current.data);
+            current = current.right;
+         } else {
+            BinaryTreeNode prev = current.left;
+            while (prev.right != null && prev.right != current) {
+               prev = prev.right;
+            }
+            if (prev.right == null) {
+               prev.right = current;
+               inorder.add(current.data);
+               current = current.left;
+            } else {
+               prev.right = null;
+               current = current.right;
+            }
+         }
+      }
+      return inorder;
+   }
 }
