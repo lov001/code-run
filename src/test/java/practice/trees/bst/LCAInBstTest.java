9c97d3b2ff4bf17b11d4a1ba3ac1fec2db791e2e
23076 <23076@omnicell.com>
2024-11-22 19:52:53 +0530

    [practice/binary-trees]: LCA in a BST iterative approach.

--- src/test/java/practice/trees/bst/LCAInBstTest.java
+++ src/test/java/practice/trees/bst/LCAInBstTest.java
@@ -36,4 +36,33 @@ public class LCAInBstTest {
       BinaryTreeNode actual = classObj.LCAinaBST(tree, left1, left2);
       Assertions.assertEquals(left1, actual);
    }
+
+   @Test
+   void test_LCAinaBSTIterative_expectFalse_whenTreeDoesNotContainNode() {
+      BinaryTreeNode left2 = new BinaryTreeNode(3, null, null);
+      BinaryTreeNode left1 = new BinaryTreeNode(1, null, null);
+      BinaryTreeNode right1 = new BinaryTreeNode(4, left2, null);
+      BinaryTreeNode tree = new BinaryTreeNode(2, left1, right1);
+      BinaryTreeNode actual = classObj.LCAinaBSTIterative(tree, left1, left2);
+      Assertions.assertEquals(tree, actual);
+   }
+
+   @Test
+   void test_LCAinaBSTIterative_expectFalse_whenEmptyTree() {
+      BinaryTreeNode left2 = new BinaryTreeNode(5, null, null);
+      BinaryTreeNode left1 = new BinaryTreeNode(1, null, null);
+      BinaryTreeNode right1 = new BinaryTreeNode(3, null, left2);
+      BinaryTreeNode tree = new BinaryTreeNode(2, left1, right1);
+      BinaryTreeNode actual = classObj.LCAinaBSTIterative(tree, right1, left2);
+      Assertions.assertEquals(right1, actual);
+   }
+
+   @Test
+   void test_LCAinaBSTIterative_expectTrue_whenDifferenceIs3_() {
+      BinaryTreeNode left2 = new BinaryTreeNode(1, null, null);
+      BinaryTreeNode left1 = new BinaryTreeNode(2, left2, null);
+      BinaryTreeNode tree = new BinaryTreeNode(3, left1, null);
+      BinaryTreeNode actual = classObj.LCAinaBSTIterative(tree, left1, left2);
+      Assertions.assertEquals(left1, actual);
+   }
 }
