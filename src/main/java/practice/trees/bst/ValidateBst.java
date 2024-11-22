428b3cc5f7b0dbce5319fc3a253fddf6f4e620ca
23076 <23076@omnicell.com>
2024-11-22 17:38:07 +0530

    [practice/binary-trees]: check if a binary tree is a BST or not - optimised to use variables instead of an array

--- src/main/java/practice/trees/bst/ValidateBst.java
+++ src/main/java/practice/trees/bst/ValidateBst.java
@@ -1,6 +1,5 @@
 package practice.trees.bst;
 
-import java.util.Arrays;
 import practice.trees.BinaryTreeNode;
 
 public class ValidateBst {
@@ -9,21 +8,16 @@ public class ValidateBst {
       if (root == null) {
          return true;
       }
-      int[] range = {Integer.MIN_VALUE, Integer.MAX_VALUE};
-      return helper(root, range);
+      return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
    }
 
-   private boolean helper(BinaryTreeNode node, int[] range) {
+   private boolean helper(BinaryTreeNode node, int leftRange, int rightRange) {
       if (node == null) {
          return true;
       }
-      if (node.data >= range[0] && node.data < range[1]) {
-         int[] leftCopy = Arrays.copyOf(range, range.length);
-         leftCopy[1] = node.data;
-         boolean left = helper(node.left, leftCopy);
-         int[] rightCopy = Arrays.copyOf(range, range.length);
-         rightCopy[0] = node.data;
-         boolean right = helper(node.right, rightCopy);
+      if (node.data >= leftRange && node.data < rightRange) {
+         boolean left = helper(node.left, leftRange, node.data);
+         boolean right = helper(node.right, node.data, rightRange);
          return left && right;
       } else {
          return false;
