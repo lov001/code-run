54d7969ad40cf5da621403cfd8d6ddaa3a442b34
23076 <23076@omnicell.com>
2024-11-25 19:39:53 +0530

    [practice/binary-trees]: Two sum in BST via BST iterator using single next implementation

--- src/main/java/practice/trees/bst/TwoSumInBst.java
+++ src/main/java/practice/trees/bst/TwoSumInBst.java
@@ -1,5 +1,6 @@
 package practice.trees.bst;
 
+import java.util.Stack;
 import practice.trees.BinaryTreeNode;
 
 public class TwoSumInBst {
@@ -19,5 +20,59 @@ public class TwoSumInBst {
          }
       }
       return false;
+   }
+
+   public boolean twoSumInBSTOptimised(BinaryTreeNode root, int target) {
+      if (root == null) {
+         return false;
+      }
+      BstIteratorOptimised leftIterator = new BstIteratorOptimised(root, false);
+      BstIteratorOptimised rightIterator = new BstIteratorOptimised(root, true);
+      int left = leftIterator.next();
+      int right = rightIterator.next();
+      while (left < right) {
+         int current = left + right;
+         if (current == target) {
+            return true;
+         } else if (current < target) {
+            left = leftIterator.next();
+         } else {
+            right = rightIterator.next();
+         }
+      }
+      return false;
+   }
+
+   public static class BstIteratorOptimised {
+
+      private final Stack<BinaryTreeNode> stack;
+      private final boolean reverse;
+
+      BstIteratorOptimised(BinaryTreeNode root, boolean isReverse) {
+         reverse = isReverse;
+         stack = new Stack<>();
+         pushAll(root);
+      }
+
+      public int next() {
+         BinaryTreeNode node = stack.pop();
+         if (!reverse) {
+            pushAll(node.right);
+         } else {
+            pushAll(node.left);
+         }
+         return node.data;
+      }
+
+      private void pushAll(BinaryTreeNode node) {
+         while (node != null) {
+            stack.push(node);
+            if (reverse) {
+               node = node.right;
+            } else {
+               node = node.left;
+            }
+         }
+      }
    }
 }
--- src/test/java/practice/trees/bst/TwoSumInBstTest.java
+++ src/test/java/practice/trees/bst/TwoSumInBstTest.java
@@ -31,4 +31,28 @@ public class TwoSumInBstTest {
 
       Assertions.assertFalse(classObj.twoSumInBST(tree, 20));
    }
+
+   @Test
+   void test_twoSumInBSTOptimised_expectTrue_whenTargetIs14() {
+      BinaryTreeNode left4 = new BinaryTreeNode(11, null, null);
+      BinaryTreeNode right4 = new BinaryTreeNode(15, null, null);
+      BinaryTreeNode left3 = new BinaryTreeNode(2, null, null);
+      BinaryTreeNode right3 = new BinaryTreeNode(8, null, null);
+      BinaryTreeNode left1 = new BinaryTreeNode(6, left3, right3);
+      BinaryTreeNode right1 = new BinaryTreeNode(12, left4, right4);
+      BinaryTreeNode tree = new BinaryTreeNode(10, left1, right1);
+
+      Assertions.assertTrue(classObj.twoSumInBSTOptimised(tree, 14));
+   }
+
+   @Test
+   void test_twoSumInBSTOptimised_expectFalse_whenTargetIs20() {
+      BinaryTreeNode left3 = new BinaryTreeNode(6, null, null);
+      BinaryTreeNode right3 = new BinaryTreeNode(8, null, null);
+      BinaryTreeNode left1 = new BinaryTreeNode(3, null, null);
+      BinaryTreeNode right1 = new BinaryTreeNode(7, left3, right3);
+      BinaryTreeNode tree = new BinaryTreeNode(5, left1, right1);
+
+      Assertions.assertFalse(classObj.twoSumInBSTOptimised(tree, 20));
+   }
 }
