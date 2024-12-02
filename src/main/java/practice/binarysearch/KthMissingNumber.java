781a28e4dd8d3fcf44e879579921e9e65a8332f9
23076 <23076@omnicell.com>
2024-12-02 10:31:10 +0530

    [practice/binary-search]: Kth missing number using binary search

--- src/main/java/practice/binarysearch/KthMissingNumber.java
+++ src/main/java/practice/binarysearch/KthMissingNumber.java
@@ -4,7 +4,38 @@ import practice.utils.IntegerUtils;
 
 public class KthMissingNumber {
 
-   public int missingK(int[] array, int n, int k) {
+   public int missingKBinarySearch(int[] array, int n, int k) {
+      int low = 0;
+      int high = n - 1;
+      while (low <= high) {
+         int middle = low + (high - low) / 2;
+         int missing = array[middle] - (middle + 1);
+         if (missing < k) {
+            low = middle + 1;
+         } else {
+            high = middle - 1;
+         }
+      }
+      /*
+         Missing = array[high] - (high + 1)
+         More = (k - missing)
+         Answer = array[high] + more
+       */
+      return 1 + k + high;
+   }
+
+   public int missingKBruteForce2(int[] array, int n, int k) {
+      for (int i = 0; i < n; i++) {
+         if (array[i] <= k) {
+            k++;
+         } else {
+            break;
+         }
+      }
+      return k;
+   }
+
+   public int missingK(int[] array, int k) {
       int low = 1;
       int high = IntegerUtils.findMax(array);
       int index = 0;
