dd710cebae984fcbc7d6a5bd7b1e1404f5dadc28
23076 <23076@omnicell.com>
2024-12-03 21:20:34 +0530

    [practice/binary-search]: aggressive cows using binary search

--- src/test/java/practice/binarysearch/AggressiveCowsTest.java
+++ src/test/java/practice/binarysearch/AggressiveCowsTest.java
@@ -10,18 +10,21 @@ public class AggressiveCowsTest {
    @Test
    void test_aggressiveCows_expect2_whenKIs2() {
       int[] input = {1, 2, 3};
+      Assertions.assertEquals(2, classObj.aggressiveCowsBruteForce(input, 2));
       Assertions.assertEquals(2, classObj.aggressiveCows(input, 2));
    }
 
    @Test
    void test_aggressiveCows_expect3_whenKIs4() {
       int[] input = {0, 3, 4, 7, 10, 9};
+      Assertions.assertEquals(3, classObj.aggressiveCowsBruteForce(input, 4));
       Assertions.assertEquals(3, classObj.aggressiveCows(input, 4));
    }
 
    @Test
    void test_aggressiveCows_expect5_whenKIs2() {
       int[] input = {4, 2, 1, 3, 6};
+      Assertions.assertEquals(5, classObj.aggressiveCowsBruteForce(input, 2));
       Assertions.assertEquals(5, classObj.aggressiveCows(input, 2));
    }
 }
