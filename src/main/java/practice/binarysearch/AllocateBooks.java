1bcb79532be29d248a2abeb3102aacb912b84094
23076 <23076@omnicell.com>
2024-12-06 22:44:20 +0530

    [practice/binary-search]: allocate books using binary search

--- src/main/java/practice/binarysearch/AllocateBooks.java
+++ src/main/java/practice/binarysearch/AllocateBooks.java
@@ -1,6 +1,7 @@
 package practice.binarysearch;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class AllocateBooks {
@@ -9,11 +10,30 @@ public class AllocateBooks {
       if (m > n) {
          return -1;
       }
+      int low = Collections.max(arr);
+      int high = arr.stream().mapToInt(Integer::intValue).sum();
+      while (low <= high) {
+         int middle = low + (high - low) / 2;
+         int allocatedStudents = allocateBooks(arr, middle);
+         if (allocatedStudents > m) {
+            low = middle + 1;
+         } else {
+            high = middle - 1;
+         }
+      }
+      return low;
+   }
+
+   public int findPagesBruteForce(ArrayList<Integer> arr, int n, int m) {
+      if (m > n) {
+         return -1;
+      }
       int low = arr.stream().max(Comparator.naturalOrder()).orElse(-1);
       int high = arr.stream().reduce(Integer::sum).orElse(-1);
       int minPages = -1;
       for (int pages = low; pages <= high; pages++) {
-         if (allocateBooks(arr, pages, m)) {
+         int allocatedStudents = allocateBooks(arr, pages);
+         if (allocatedStudents == m) {
             minPages = pages;
             break;
          }
@@ -21,17 +41,17 @@ public class AllocateBooks {
       return minPages;
    }
 
-   private boolean allocateBooks(ArrayList<Integer> arr, int pages, int m) {
+   private int allocateBooks(ArrayList<Integer> arr, int pages) {
       int studentsAllocated = 1;
-      int noOfPagesAllocated = arr.get(0);
-      for (int i = 1; i < arr.size(); i++) {
-         if (noOfPagesAllocated + arr.get(i) <= pages) {
-            noOfPagesAllocated += arr.get(i);
+      int noOfPagesAllocated = 0;
+      for (Integer entry : arr) {
+         if (noOfPagesAllocated + entry <= pages) {
+            noOfPagesAllocated += entry;
          } else {
             studentsAllocated++;
-            noOfPagesAllocated = arr.get(i);
+            noOfPagesAllocated = entry;
          }
       }
-      return studentsAllocated == m;
+      return studentsAllocated;
    }
 }
