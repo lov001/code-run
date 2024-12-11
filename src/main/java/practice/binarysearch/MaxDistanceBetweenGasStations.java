946f25f41644c9d964a8c1575a19f6382c5e2576
23076 <23076@omnicell.com>
2024-12-11 19:34:00 +0530

    [practice/binary-search]: minimise max distance between gas stations using priority queue

--- src/main/java/practice/binarysearch/MaxDistanceBetweenGasStations.java
+++ src/main/java/practice/binarysearch/MaxDistanceBetweenGasStations.java
@@ -1,5 +1,7 @@
 package practice.binarysearch;
 
+import java.util.PriorityQueue;
+
 public class MaxDistanceBetweenGasStations {
 
    public double minimiseMaxDistance(int[] arr, int K) {
@@ -25,5 +27,35 @@ public class MaxDistanceBetweenGasStations {
          maxAns = Math.max(maxAns, sectionLength);
       }
       return maxAns;
+   }
+
+   public double minimiseMaxDistanceUsingPriorityQueue(int[] arr, int K) {
+      int n = arr.length;
+      int[] howMany = new int[n - 1];
+      PriorityQueue<Pair> priorityQueue = new PriorityQueue<>(
+         (a, b) -> Double.compare(b.first, a.first));
+      for (int i = 0; i < n - 1; i++) {
+         priorityQueue.offer(new Pair(arr[i + 1] - arr[i], i));
+      }
+      for (int gasStations = 1; gasStations <= K; gasStations++) {
+         Pair pair = priorityQueue.poll();
+         int sectionIndex = pair.second;
+         howMany[sectionIndex]++;
+         double difference = arr[sectionIndex + 1] - arr[sectionIndex];
+         double sectionLength = difference / (double) (howMany[sectionIndex] + 1);
+         priorityQueue.offer(new Pair(sectionLength, sectionIndex));
+      }
+      return priorityQueue.peek().first;
+   }
+
+   private static class Pair {
+
+      double first;
+      int second;
+
+      public Pair(double first, int second) {
+         this.first = first;
+         this.second = second;
+      }
    }
 }
