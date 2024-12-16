782b5e42d293433050a3c821f2c8c1a57b973eb6
23076 <23076@omnicell.com>
2024-12-16 09:24:11 +0530

    [practice/binary-search]: maximum points from cards optimised solution

--- src/main/java/practice/slidingwindow/MaximumPointsFromCards.java
+++ src/main/java/practice/slidingwindow/MaximumPointsFromCards.java
@@ -2,6 +2,23 @@ package practice.slidingwindow;
 
 public class MaximumPointsFromCards {
 
+   public int maxPoints(int[] arr, int K) {
+      int leftSum = 0, rightSum = 0;
+      int n = arr.length;
+      for (int i = 0; i < K; i++) {
+         leftSum += arr[i];
+      }
+      int maxPoints = leftSum;
+      int rightIndex = n - 1;
+      for (int i = K - 1; i >= 0; i--) {
+         leftSum -= arr[i];
+         rightSum += arr[rightIndex];
+         rightIndex--;
+         maxPoints = Math.max(maxPoints, leftSum + rightSum);
+      }
+      return maxPoints;
+   }
+
    public int maxPointsBruteForce(int[] arr, int K) {
       int left = K, right = 0;
       int maxPoints = Integer.MIN_VALUE;
