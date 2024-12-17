01063a8f90745f5b64e7379d7cd6f065f4177dc1
23076 <23076@omnicell.com>
2024-12-17 21:54:01 +0530

    [practice/binary-search]: number of substrings containing all 3 characters using brute force

---
+++ src/test/java/practice/slidingwindow/NumberOfSubstringsContainingAll3CharactersTest.java
@@ -0,0 +1,27 @@
+package practice.slidingwindow;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NumberOfSubstringsContainingAll3CharactersTest {
+
+   NumberOfSubStringsContainingAll3Characters classObj = new NumberOfSubStringsContainingAll3Characters();
+
+   @Test
+   void test_numberOfSubstrings_expect10_whenStringOfSize6() {
+      String input = "abcabc";
+      Assertions.assertEquals(10, classObj.numberOfSubstringsBruteForce(input));
+   }
+
+   @Test
+   void test_numberOfSubstrings_expect3_whenStringOfSize5() {
+      String input = "aaacb";
+      Assertions.assertEquals(3, classObj.numberOfSubstringsBruteForce(input));
+   }
+
+   @Test
+   void test_numberOfSubstrings_expect1_whenStringOfSize3() {
+      String input = "abc";
+      Assertions.assertEquals(1, classObj.numberOfSubstringsBruteForce(input));
+   }
+}
