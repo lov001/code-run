e0b4bafbb4105017b5957063ab9b9cc3aec0cb83
23076 <23076@omnicell.com>
2024-12-21 16:45:06 +0530

    [practice/binary-search]: jump game 2 using recursion

---
+++ src/test/java/practice/greedy/JumpGame2Test.java
@@ -0,0 +1,21 @@
+package practice.greedy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JumpGame2Test {
+
+   JumpGame2 classObj = new JumpGame2();
+
+   @Test
+   void test_canJump_expect2_whenNIs5() {
+      int[] input = {2, 3, 1, 1, 4};
+      Assertions.assertEquals(2, classObj.jumpBruteForceWithRecursion(input));
+   }
+
+   @Test
+   void test_canJump_expect2_whenNIs5_() {
+      int[] input = {2, 3, 0, 1, 4};
+      Assertions.assertEquals(2, classObj.jumpBruteForceWithRecursion(input));
+   }
+}
