99a3f472b151a66abdc71a75ea7864249c6ac91a
23076 <23076@omnicell.com>
2024-12-25 10:39:37 +0530

    [practice/greedy]: valid parenthesis string with * using range based approach

--- src/test/java/practice/greedy/ValidParenthesisStringTest.java
+++ src/test/java/practice/greedy/ValidParenthesisStringTest.java
@@ -11,17 +11,20 @@ public class ValidParenthesisStringTest {
    void test_checkValidString_expectTrue_whenStringOfLength4() {
       String input = "*())";
       Assertions.assertTrue(classObj.checkValidStringUsingRecursion(input));
+      Assertions.assertTrue(classObj.checkValidString(input));
    }
 
    @Test
    void test_checkValidString_expectTrue_whenStringOfLength3() {
       String input = "(*)";
       Assertions.assertTrue(classObj.checkValidStringUsingRecursion(input));
+      Assertions.assertTrue(classObj.checkValidString(input));
    }
 
    @Test
    void test_checkValidString_expectFalse_whenStringOfLength4() {
       String input = "())*";
       Assertions.assertFalse(classObj.checkValidStringUsingRecursion(input));
+      Assertions.assertFalse(classObj.checkValidString(input));
    }
 }
