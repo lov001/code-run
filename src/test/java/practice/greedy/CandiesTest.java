963abfa8f308be7971a4f51e7ca267a71fd1b452
23076 <23076@omnicell.com>
2024-12-26 09:19:36 +0530

    [practice/greedy]: candies distribution using peak and down approach without extra space

--- src/test/java/practice/greedy/CandiesTest.java
+++ src/test/java/practice/greedy/CandiesTest.java
@@ -11,29 +11,38 @@ public class CandiesTest {
    void test_requiredCandies_expect10_whenNIs6() {
       int[] input = {5, 8, 1, 5, 9, 4};
       Assertions.assertEquals(10, classObj.requiredCandies(input));
+      Assertions.assertEquals(10, classObj.requiredCandiesOptimised(input));
+//      Assertions.assertEquals(10, classObj.requiredCandiesOptimised(input));
    }
 
    @Test
    void test_requiredCandies_expect3_whenNIs2() {
       int[] input = {1, 5};
       Assertions.assertEquals(3, classObj.requiredCandies(input));
+      Assertions.assertEquals(3, classObj.requiredCandiesBFOptimised(input));
+      Assertions.assertEquals(3, classObj.requiredCandiesOptimised(input));
    }
 
    @Test
    void test_requiredCandies_expect5_whenNIs5() {
       int[] input = {1, 1, 1, 1, 1};
       Assertions.assertEquals(5, classObj.requiredCandies(input));
+      Assertions.assertEquals(5, classObj.requiredCandiesBFOptimised(input));
+      Assertions.assertEquals(5, classObj.requiredCandiesOptimised(input));
    }
 
    @Test
    void test_requiredCandies_expect9_whenNIs5() {
       int[] input = {1, 5, 3, 4, 6};
       Assertions.assertEquals(9, classObj.requiredCandies(input));
+      Assertions.assertEquals(9, classObj.requiredCandiesBFOptimised(input));
+      Assertions.assertEquals(9, classObj.requiredCandiesOptimised(input));
    }
 
    @Test
    void test_requiredCandies_expect9_whenNIs6() {
       int[] input = {1, 9, 1, 3, 2, 4};
       Assertions.assertEquals(9, classObj.requiredCandies(input));
+//      Assertions.assertEquals(9, classObj.requiredCandiesOptimised(input));
    }
 }
