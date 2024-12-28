ec82c0787d0927bcfc0d1b9d96d29f9d88168276
23076 <23076@omnicell.com>
2024-12-28 12:42:06 +0530

    [practice/arrays-and-strings]: second-largest and smallest element in array optimal approach

--- src/main/java/practice/arrays/SecondLargestAndSmallestElementInArray.java
+++ src/main/java/practice/arrays/SecondLargestAndSmallestElementInArray.java
@@ -5,7 +5,23 @@ public class SecondLargestAndSmallestElementInArray {
    public int[] getSecondOrderElements(int n, int[] a) {
       int[] output = new int[2];
       output[0] = -1;
-      output[1] = -1;
+      output[1] = Integer.MAX_VALUE;
+      int largest = a[0];
+      int smallest = a[0];
+      for (int i = 1; i < n; i++) {
+         if (a[i] > largest) {
+            output[0] = largest;
+            largest = a[i];
+         } else if (a[i] < largest && a[i] > output[0]) {
+            output[0] = a[i];
+         }
+         if (a[i] < smallest) {
+            output[1] = smallest;
+            smallest = a[i];
+         } else if (a[i] != smallest && a[i] < output[1]) {
+            output[1] = a[i];
+         }
+      }
       return output;
    }
 }
