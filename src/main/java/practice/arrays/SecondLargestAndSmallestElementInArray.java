b10b26c0cace30409eeaab8e49229e094453105e
23076 <23076@omnicell.com>
2024-12-28 12:18:22 +0530

    [practice/arrays-and-strings]: second largest element in array

---
+++ src/main/java/practice/arrays/SecondLargestAndSmallestElementInArray.java
@@ -0,0 +1,11 @@
+package practice.arrays;
+
+public class SecondLargestAndSmallestElementInArray {
+
+   public int[] getSecondOrderElements(int n, int[] a) {
+      int[] output = new int[2];
+      output[0] = -1;
+      output[1] = -1;
+      return output;
+   }
+}
