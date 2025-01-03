a9f88971f751d5694e35085cc0699d3c67166a55
23076 <23076@omnicell.com>
2025-01-03 16:19:09 +0530

    [practice/arrays-and-strings]: missing number with xor

--- src/main/java/practice/arrays/MissingNumber.java
+++ src/main/java/practice/arrays/MissingNumber.java
@@ -10,4 +10,15 @@ public class MissingNumber {
       }
       return expectedSum - actualSum;
    }
+
+   public int missingNumberWithXor(int[] a, int N) {
+      int xor1 = 0;
+      int xor2 = 0;
+      for (int i = 0; i < N - 1; i++) {
+         xor2 = xor2 ^ a[i];
+         xor1 = xor1 ^ (i + 1);
+      }
+      xor2 = xor2 ^ N;
+      return xor1 ^ xor2;
+   }
 }
--- src/test/java/practice/arrays/MissingNumberTest.java
+++ src/test/java/practice/arrays/MissingNumberTest.java
@@ -11,17 +11,20 @@ public class MissingNumberTest {
    void test_missingNumber_expect3_whenNIs5() {
       int[] input = {1, 2, 4, 5};
       Assertions.assertEquals(3, classObj.missingNumber(input, 5));
+      Assertions.assertEquals(3, classObj.missingNumberWithXor(input, 5));
    }
 
    @Test
    void test_missingNumber_expect4_whenNIs4() {
       int[] input = {1, 2, 3};
       Assertions.assertEquals(4, classObj.missingNumber(input, 4));
+      Assertions.assertEquals(4, classObj.missingNumberWithXor(input, 4));
    }
 
    @Test
    void test_missingNumber_expect4_whenNIs8() {
       int[] input = {1, 2, 3, 5, 6, 7, 8};
       Assertions.assertEquals(4, classObj.missingNumber(input, 8));
+      Assertions.assertEquals(4, classObj.missingNumberWithXor(input, 8));
    }
 }
