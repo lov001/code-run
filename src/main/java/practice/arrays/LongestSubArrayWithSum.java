1aa9730b788ed7c90800a7551d79659c834385b5
23076 <23076@omnicell.com>
2025-01-04 13:21:31 +0530

    [practice/arrays-and-strings]: longest sub-array with sum k with prefix sum

--- src/main/java/practice/arrays/LongestSubArrayWithSum.java
+++ src/main/java/practice/arrays/LongestSubArrayWithSum.java
@@ -1,8 +1,30 @@
 package practice.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithSum {
 
    public int longestSubArrayWithSumKWithPrefixSum(int[] a, long k) {
+      int maxLength = 0;
+      int n = a.length;
+      Map<Long, Integer> prefixSumMap = new HashMap<>();
+      long sum = 0;
+      for (int i = 0; i < n; i++) {
+         sum += a[i];
+         if (sum == k) {
+            maxLength = Math.max(maxLength, i + 1);
+         }
+         long difference = sum - k;
+         if (prefixSumMap.get(difference) != null) {
+            maxLength = Math.max(maxLength, i - prefixSumMap.get(difference));
+         }
+         prefixSumMap.putIfAbsent(sum, i);
+      }
+      return maxLength;
+   }
+
+   public int longestSubArrayWithSumKBruteForce(int[] a, long k) {
       int maxLength = 0;
       int n = a.length;
       for (int i = 0; i < n; i++) {
