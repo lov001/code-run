ce5f7e367d7d1930f5dfcc469bc13a32d8d9c438
23076 <23076@omnicell.com>
2025-01-06 10:20:48 +0530

    [practice/arrays-and-strings]: majority element using moore's voting algorithm

--- src/main/java/practice/arrays/MajorityElement.java
+++ src/main/java/practice/arrays/MajorityElement.java
@@ -5,6 +5,33 @@ import java.util.Map;
 
 public class MajorityElement {
 
+   public int majorityElementMooreAlgorithm(int[] v) {
+      int element = 0;
+      int count = 0;
+      for (int j : v) {
+         if (count == 0) {
+            element = j;
+            count++;
+            continue;
+         }
+         if (j == element) {
+            count++;
+         } else {
+            count--;
+         }
+      }
+      int verifyCount = 0;
+      for (int j : v) {
+         if (j == element) {
+            verifyCount++;
+         }
+      }
+      if (verifyCount > (v.length / 2)) {
+         return element;
+      }
+      return -1;
+   }
+
    public int majorityElement(int[] v) {
       Map<Integer, Integer> map = new HashMap<>();
       for (int element : v) {
