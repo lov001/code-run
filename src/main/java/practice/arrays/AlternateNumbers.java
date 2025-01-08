13a9561ae5944665f1de261fa0b698924f74ee61
23076 <23076@omnicell.com>
2025-01-08 08:36:49 +0530

    [practice/arrays-and-strings]: alternate numbers with order positive and negative with unequal counts

--- src/main/java/practice/arrays/AlternateNumbers.java
+++ src/main/java/practice/arrays/AlternateNumbers.java
@@ -1,5 +1,8 @@
 package practice.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlternateNumbers {
 
 
@@ -36,6 +39,38 @@ public class AlternateNumbers {
       for (int i = 0; i < n / 2; i++) {
          a[2 * i] = positive[i];
          a[2 * i + 1] = negative[i];
+      }
+      return a;
+   }
+
+   public int[] alternateNumbersWithoutEqualCount(int[] a) {
+      List<Integer> positive = new ArrayList<>();
+      List<Integer> negative = new ArrayList<>();
+      for (int element : a) {
+         if (element < 0) {
+            negative.add(element);
+         } else {
+            positive.add(element);
+         }
+      }
+      int smallerSize = Math.min(positive.size(), negative.size());
+      int i = 0;
+      for (; i < smallerSize; i++) {
+         a[2 * i] = positive.get(i);
+         a[2 * i + 1] = negative.get(i);
+      }
+      i = smallerSize * 2;
+      if (positive.size() > negative.size()) {
+         while (smallerSize < positive.size()) {
+            a[i] = positive.get(smallerSize);
+            i++;
+            smallerSize++;
+         }
+      } else {
+         while (smallerSize < negative.size()) {
+            a[i] = negative.get(smallerSize);
+            smallerSize++;
+         }
       }
       return a;
    }
