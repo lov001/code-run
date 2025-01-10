4ce757d272f03774773f157a9c7c7cc8296a9a0b
23076 <23076@omnicell.com>
2025-01-10 18:15:50 +0530

    [practice/arrays-and-strings]: zero matrix using better solution

--- src/main/java/practice/arrays/ZeroMatrix.java
+++ src/main/java/practice/arrays/ZeroMatrix.java
@@ -4,13 +4,38 @@ import java.util.ArrayList;
 
 public class ZeroMatrix {
 
-   public ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix,
+   public ArrayList<ArrayList<Integer>> zeroMatrixBetter(ArrayList<ArrayList<Integer>> matrix,
+      Integer n, Integer m) {
+      int[] row = new int[n];
+      int[] column = new int[m];
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < m; j++) {
+            if (matrix.get(i).get(j) == 0) {
+               row[i] = 1;
+               column[j] = 1;
+            }
+         }
+      }
+      for (int i = 0; i < n; i++) {
+         if (row[i] == 1) {
+            fillRow(i, matrix, m, 0);
+         }
+      }
+      for (int j = 0; j < m; j++) {
+         if (column[j] == 1) {
+            fillColumn(j, matrix, n, 0);
+         }
+      }
+      return matrix;
+   }
+
+   public ArrayList<ArrayList<Integer>> zeroMatrixBruteForce(ArrayList<ArrayList<Integer>> matrix,
       Integer n, Integer m) {
       for (int i = 0; i < n; i++) {
          for (int j = 0; j < m; j++) {
             if (matrix.get(i).get(j) == 0) {
-               fillRow(i, matrix, m);
-               fillColumn(j, matrix, n);
+               fillRow(i, matrix, m, -1);
+               fillColumn(j, matrix, n, -1);
             }
          }
       }
@@ -24,18 +49,18 @@ public class ZeroMatrix {
       return matrix;
    }
 
-   private void fillRow(int row, ArrayList<ArrayList<Integer>> matrix, Integer m) {
+   private void fillRow(int row, ArrayList<ArrayList<Integer>> matrix, Integer m, int value) {
       for (int j = 0; j < m; j++) {
          if (matrix.get(row).get(j) != 0) {
-            matrix.get(row).set(j, -1);
+            matrix.get(row).set(j, value);
          }
       }
    }
 
-   private void fillColumn(int column, ArrayList<ArrayList<Integer>> matrix, Integer n) {
+   private void fillColumn(int column, ArrayList<ArrayList<Integer>> matrix, Integer n, int value) {
       for (int i = 0; i < n; i++) {
          if (matrix.get(i).get(column) != 0) {
-            matrix.get(i).set(column, -1);
+            matrix.get(i).set(column, value);
          }
       }
    }
