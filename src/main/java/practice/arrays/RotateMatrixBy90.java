6a6a946bbc3eeaac6333a9fac23c4accc58979fe
23076 <23076@omnicell.com>
2025-01-11 09:26:39 +0530

    [practice/arrays-and-strings]: rotate matrix by 90 degree brute force

--- src/main/java/practice/arrays/RotateMatrixBy90.java
+++ src/main/java/practice/arrays/RotateMatrixBy90.java
@@ -1,6 +1,22 @@
 package practice.arrays;
 
+import practice.utils.SwapNumbersInArray;
+
 public class RotateMatrixBy90 {
+
+   public void rotateMatrix(int[][] mat) {
+      int n = mat.length;
+      for (int i = 0; i < n - 1; i++) {
+         for (int j = i + 1; j < n; j++) {
+            int temp = mat[i][j];
+            mat[i][j] = mat[j][i];
+            mat[j][i] = temp;
+         }
+      }
+      for (int[] ints : mat) {
+         sort(ints);
+      }
+   }
 
    public void rotateMatrixBruteForce(int[][] mat) {
       int n = mat.length;
@@ -13,6 +29,18 @@ public class RotateMatrixBy90 {
       }
       for (int i = 0; i < n; i++) {
          System.arraycopy(ans[i], 0, mat[i], 0, m);
+      }
+   }
+
+   private void sort(int[] row) {
+      int start = 0;
+      int end = row.length - 1;
+      while (start < end) {
+         int temp = row[start];
+         row[start] = row[end];
+         row[end] = temp;
+         start++;
+         end--;
       }
    }
 }
