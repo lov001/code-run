f7cf9d31cfdee48051b6a7f06d7c5b032c17d003
23076 <23076@omnicell.com>
2025-01-16 09:26:27 +0530

    [practice/arrays-and-strings]: count subArrays with xor k with optimal approach

--- src/main/java/practice/arrays/SubArraysWithXorK.java
+++ src/main/java/practice/arrays/SubArraysWithXorK.java
@@ -1,8 +1,26 @@
 package practice.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubArraysWithXorK {
 
-   public int subArraysWithSumKBruteForce(int[] a, int b) {
+   public int subArraysWithXorK(int[] a, int b) {
+      int count = 0;
+      Map<Integer, Integer> map = new HashMap<>();
+      int xor = 0;
+      map.put(0, 1);
+      for (int element : a) {
+         xor = xor ^ element;
+         int remainingXor = xor ^ b;
+         count += map.getOrDefault(remainingXor, 0);
+         map.put(xor, map.getOrDefault(xor, 0) + 1);
+
+      }
+      return count;
+   }
+
+   public int subArraysWithXorKBruteForce(int[] a, int b) {
       int n = a.length;
       int count = 0;
       for (int i = 0; i < n; i++) {
--- src/test/java/practice/arrays/SubArraysWithXorKTest.java
+++ src/test/java/practice/arrays/SubArraysWithXorKTest.java
@@ -10,12 +10,14 @@ public class SubArraysWithXorKTest {
    @Test
    void test_subArraysWithSumKBruteForce_expect3_whenNIs4AndKIs2() {
       int[] input = {1, 2, 3, 2};
-      Assertions.assertEquals(3, classObj.subArraysWithSumKBruteForce(input, 2));
+      Assertions.assertEquals(3, classObj.subArraysWithXorKBruteForce(input, 2));
+      Assertions.assertEquals(3, classObj.subArraysWithXorK(input, 2));
    }
 
    @Test
    void test_subArraysWithSumKBruteForce_expect2_whenNIs4AndGoalIs3() {
       int[] input = {1, 2, 3, 3};
-      Assertions.assertEquals(4, classObj.subArraysWithSumKBruteForce(input, 3));
+      Assertions.assertEquals(4, classObj.subArraysWithXorKBruteForce(input, 3));
+      Assertions.assertEquals(4, classObj.subArraysWithXorK(input, 3));
    }
 }
