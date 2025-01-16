12196045ba9a2f124fa0a6f1464b7ce907451a39
23076 <23076@omnicell.com>
2025-01-16 07:30:28 +0530

    [practice/arrays-and-strings]: 4 sum problem using hashing and two pointer approach

--- src/test/java/practice/arrays/FourSumTest.java
+++ src/test/java/practice/arrays/FourSumTest.java
@@ -42,7 +42,7 @@ public class FourSumTest {
       int[] input = {1, 2, 1, 0, 1};
       List<List<Integer>> expected = new ArrayList<>();
       expected.add(Arrays.asList(1, 1, 1, 2));
-      List<List<Integer>> actual = classObj.fourSumBruteForce(input, 5);
+      List<List<Integer>> actual = classObj.fourSumHashing(input, 5);
       Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
    }
 
@@ -52,7 +52,7 @@ public class FourSumTest {
       List<List<Integer>> expected = new ArrayList<>();
       expected.add(Arrays.asList(1, 2, 2, 3));
       expected.add(Arrays.asList(2, 2, 2, 2));
-      List<List<Integer>> actual = classObj.fourSumBruteForce(input, 8);
+      List<List<Integer>> actual = classObj.fourSumHashing(input, 8);
       Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
    }
 
@@ -60,7 +60,34 @@ public class FourSumTest {
    void test_fourSumHashing_expect0_whenNIs6() {
       int[] input = {1, 1, 1, 0};
       List<List<Integer>> expected = new ArrayList<>();
-      List<List<Integer>> actual = classObj.fourSumBruteForce(input, 4);
+      List<List<Integer>> actual = classObj.fourSumHashing(input, 4);
+      Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
+   }
+
+   @Test
+   void test_fourSumTwoPointer_expect1_whenNIs5() {
+      int[] input = {1, 2, 1, 0, 1};
+      List<List<Integer>> expected = new ArrayList<>();
+      expected.add(Arrays.asList(1, 1, 1, 2));
+      List<List<Integer>> actual = classObj.fourSumTwoPointer(input, 5);
+      Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
+   }
+
+   @Test
+   void test_fourSumTwoPointer_expect2_whenNIs6() {
+      int[] input = {2, 2, 2, 2, 1, 3};
+      List<List<Integer>> expected = new ArrayList<>();
+      expected.add(Arrays.asList(1, 2, 2, 3));
+      expected.add(Arrays.asList(2, 2, 2, 2));
+      List<List<Integer>> actual = classObj.fourSumTwoPointer(input, 8);
+      Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
+   }
+
+   @Test
+   void test_fourSumTwoPointer_expect0_whenNIs6() {
+      int[] input = {1, 1, 1, 0};
+      List<List<Integer>> expected = new ArrayList<>();
+      List<List<Integer>> actual = classObj.fourSumTwoPointer(input, 4);
       Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
    }
 }
