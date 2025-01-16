be1566f77868c40f90d34ff241e64855acbd4db9
23076 <23076@omnicell.com>
2025-01-16 09:11:10 +0530

    [practice/arrays-and-strings]: count subArrays with xor k

---
+++ src/main/java/practice/arrays/SubArraysWithXorK.java
@@ -0,0 +1,19 @@
+package practice.arrays;
+
+public class SubArraysWithXorK {
+
+   public int subArraysWithSumKBruteForce(int[] a, int b) {
+      int n = a.length;
+      int count = 0;
+      for (int i = 0; i < n; i++) {
+         int xor = 0;
+         for (int j = i; j < n; j++) {
+            xor = xor ^ a[j];
+            if (xor == b) {
+               count++;
+            }
+         }
+      }
+      return count;
+   }
+}
