caba7bdbe1bcdda29b611973ce0676886b825970
23076 <23076@omnicell.com>
2025-01-19 06:50:04 +0530

    [practice/arrays-and-strings]: finding missing and repeating numbers using hashing

--- src/test/java/practice/arrays/MissingAndRepeatingNumbersTest.java
+++ src/test/java/practice/arrays/MissingAndRepeatingNumbersTest.java
@@ -22,4 +22,20 @@ public class MissingAndRepeatingNumbersTest {
       int[] actual = classObj.findMissingRepeatingNumbersBruteForce(input);
       Assertions.assertArrayEquals(expected, actual);
    }
+
+   @Test
+   void test_findMissingRepeatingNumbersHashing_expect2And4_whenNIs4() {
+      int[] input = {1, 2, 3, 2};
+      int[] expected = {2, 4};
+      int[] actual = classObj.findMissingRepeatingNumbersHashing(input);
+      Assertions.assertArrayEquals(expected, actual);
+   }
+
+   @Test
+   void test_findMissingRepeatingNumbersHashing_expect1And3_whenNIs3() {
+      int[] input = {1, 2, 1};
+      int[] expected = {1, 3};
+      int[] actual = classObj.findMissingRepeatingNumbersHashing(input);
+      Assertions.assertArrayEquals(expected, actual);
+   }
 }
