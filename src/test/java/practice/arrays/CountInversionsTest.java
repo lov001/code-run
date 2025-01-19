dc6c5f43795c2a680855700f0bb402afd739807e
23076 <23076@omnicell.com>
2025-01-19 17:08:53 +0530

    [practice/arrays-and-strings]: count inversions using brute force

--- src/test/java/practice/arrays/CountInversionsTest.java
+++ src/test/java/practice/arrays/CountInversionsTest.java
@@ -1,4 +1,4 @@
-package practice.dynamicprogrammingandrecursion;
+package practice.arrays;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -35,5 +35,36 @@ public class CountInversionsTest {
    void test_numberOfInversions_expect10_whenNIs5() {
       int[] input = {5, 4, 3, 2, 1};
       Assertions.assertEquals(10, classObj.numberOfInversions(input, 5));
+   }
+
+
+   @Test
+   void test_numberOfInversionsBruteForce_expect0_whenNIs5() {
+      int[] input = {1, 2, 3, 4, 5};
+      Assertions.assertEquals(0, classObj.numberOfInversionsBruteForce(input, 5));
+   }
+
+   @Test
+   void test_numberOfInversionsBruteForce_expect7_whenNIs5() {
+      int[] input = {5, 3, 2, 1, 4};
+      Assertions.assertEquals(7, classObj.numberOfInversionsBruteForce(input, 5));
+   }
+
+   @Test
+   void test_numberOfInversionsBruteForce_expect6_whenNIs4() {
+      int[] input = {4, 3, 2, 1};
+      Assertions.assertEquals(6, classObj.numberOfInversionsBruteForce(input, 4));
+   }
+
+   @Test
+   void test_numberOfInversionsBruteForce_expect5_whenNIs5() {
+      int[] input = {1, 20, 6, 4, 5};
+      Assertions.assertEquals(5, classObj.numberOfInversionsBruteForce(input, 5));
+   }
+
+   @Test
+   void test_numberOfInversionsBruteForce_expect10_whenNIs5() {
+      int[] input = {5, 4, 3, 2, 1};
+      Assertions.assertEquals(10, classObj.numberOfInversionsBruteForce(input, 5));
    }
 }
