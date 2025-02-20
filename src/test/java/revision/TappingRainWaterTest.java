46dbccde43c1fda4ef8d42a1b8c96654955d5de9
23076 <23076@omnicell.com>
2025-02-20 10:11:29 +0530

    [revision]:maximum tapped water using brute force

---
+++ src/test/java/revision/TappingRainWaterTest.java
@@ -0,0 +1,27 @@
+package revision;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TappingRainWaterTest {
+
+   TappingRainWater classObj = new TappingRainWater();
+
+   @Test
+   void test_getTrappedWater_expect10_whenNIs6() {
+      long[] input = {3, 0, 0, 2, 0, 4};
+      Assertions.assertEquals(10, classObj.getTrappedWater(input, 6));
+   }
+
+   @Test
+   void test_getTrappedWater_expect2_whenNIs4() {
+      long[] input = {2, 1, 1, 4};
+      Assertions.assertEquals(2, classObj.getTrappedWater(input, 4));
+   }
+
+   @Test
+   void test_getTrappedWater_expect9_whenNIs5() {
+      long[] input = {8, 1, 8, 2, 4};
+      Assertions.assertEquals(9, classObj.getTrappedWater(input, 5));
+   }
+}
