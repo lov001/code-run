b0751c8d5254b48da33636694946628cd56dc82f
23076 <23076@omnicell.com>
2025-04-08 22:05:47 +0530

    [revision]: LRU cache using doubly linked list

--- src/test/java/practice/linkedlist/LRUCacheUsingLinkedHashMapTest.java
+++ src/test/java/practice/linkedlist/LRUCacheUsingLinkedHashMapTest.java
@@ -6,16 +6,31 @@ import org.junit.jupiter.api.Test;
 public class LRUCacheUsingLinkedHashMapTest {
 
    @Test
-   void test_LRUCacheUsingLinkedHashMap_expectResult_whenAsserted(){
+   void test_LRUCacheUsingLinkedHashMap_expectResult_whenAsserted() {
       LRUCacheUsingLinkedHashMap cache = new LRUCacheUsingLinkedHashMap(2);
       cache.put(1, 1);
       cache.put(2, 2);
       Assertions.assertEquals(1, cache.get(1));
-      cache.put(3, 3);                    // evicts key 2
-      Assertions.assertEquals(-1, cache.get(2));   // returns -1 (not found)
-      cache.put(4, 4);                    // evicts key 1
-      Assertions.assertEquals(-1, cache.get(1));   // returns -1 (not found)
-      Assertions.assertEquals(3, cache.get(3));   // returns 3
+      cache.put(3, 3);
+      Assertions.assertEquals(-1, cache.get(2));
+      cache.put(4, 4);
+      Assertions.assertEquals(-1, cache.get(1));
+      Assertions.assertEquals(3, cache.get(3));
+      Assertions.assertEquals(4, cache.get(4));
+   }
+
+   @Test
+   void test_LRUCacheUsingDoublyLinkedList_expectResult_whenAsserted() {
+      LRUCacheUsingDoublyLinkedList cache = new LRUCacheUsingDoublyLinkedList(2);
+
+      cache.put(1, 1);
+      cache.put(2, 2);
+      Assertions.assertEquals(1, cache.get(1));
+      cache.put(3, 3);
+      Assertions.assertEquals(-1, cache.get(2));
+      cache.put(4, 4);
+      Assertions.assertEquals(-1, cache.get(1));
+      Assertions.assertEquals(3, cache.get(3));
       Assertions.assertEquals(4, cache.get(4));
    }
 }
