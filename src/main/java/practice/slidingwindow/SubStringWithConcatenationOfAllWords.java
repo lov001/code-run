db83c2962dc08b864defc5b358d2622e27d92ea2
23076 <23076@omnicell.com>
2025-05-17 15:16:19 +0530

    [revision]: Sub String With Concatenation Of All Words using sliding window optimal solution

--- src/main/java/practice/slidingwindow/SubStringWithConcatenationOfAllWords.java
+++ src/main/java/practice/slidingwindow/SubStringWithConcatenationOfAllWords.java
@@ -7,6 +7,48 @@ import java.util.Map;
 
 public class SubStringWithConcatenationOfAllWords {
 
+   public List<Integer> findSubstringOptimal(String s, String[] words) {
+      List<Integer> ans = new ArrayList<>();
+      if (s == null || s.length() == 0 || words == null || words.length == 0) {
+         return ans;
+      }
+      Map<String, Integer> wordMap = new HashMap<>();
+      for (String word : words) {
+         wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+      }
+      int wordSize = words[0].length();
+      int wordCount = words.length;
+      int N = s.length();
+
+      for (int offset = 0; offset < wordSize; offset++) {
+         Map<String, Integer> copyMap = new HashMap<>();
+         int left = offset;
+         int count = 0;
+         for (int end = offset; end + wordSize <= N; end += wordSize) {
+            String subString = s.substring(end, end + wordSize);
+            if (wordMap.containsKey(subString)) {
+               copyMap.put(subString, copyMap.getOrDefault(subString, 0) + 1);
+               count++;
+
+               while (copyMap.get(subString) > wordMap.get(subString)) {
+                  String startWord = s.substring(left, left + wordSize);
+                  copyMap.put(startWord, copyMap.get(startWord) - 1);
+                  left += wordSize;
+                  count--;
+               }
+               if (count == wordCount) {
+                  ans.add(left);
+               }
+            } else {
+               count = 0;
+               left = end + wordSize;
+               copyMap.clear();
+            }
+         }
+      }
+      return ans;
+   }
+
    public List<Integer> findSubstring(String s, String[] words) {
       List<Integer> ans = new ArrayList<>();
       if (s == null || s.length() == 0 || words == null || words.length == 0) {
--- src/test/java/practice/slidingwindow/SubStringWithConcatenationOfAllWordsTest.java
+++ src/test/java/practice/slidingwindow/SubStringWithConcatenationOfAllWordsTest.java
@@ -50,4 +50,46 @@ public class SubStringWithConcatenationOfAllWordsTest {
       List<Integer> actual = classObj.findSubstring(inputString, words);
       Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
    }
+
+   @Test
+   void test_findSubstringOptimal_expectOutputOfSize2_whenWordsSizeIs2AndWordSize3() {
+      String inputString = "barfoothefoobarman";
+      String[] words = {"foo", "bar"};
+      List<Integer> expected = new ArrayList<>();
+      expected.add(0);
+      expected.add(9);
+      List<Integer> actual = classObj.findSubstringOptimal(inputString, words);
+      Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
+   }
+
+   @Test
+   void test_findSubstringOptimal_expectOutputOfSize0_whenWordsSizeIs4AndWordSize4() {
+      String inputString = "wordgoodgoodgoodbestword";
+      String[] words = {"word", "good", "best", "word"};
+      List<Integer> expected = new ArrayList<>();
+      List<Integer> actual = classObj.findSubstringOptimal(inputString, words);
+      Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
+   }
+
+   @Test
+   void test_findSubstringOptimal_expectOutputOfSize3_whenWordsSizeIs3AndWordSize3() {
+      String inputString = "barfoofoobarthefoobarman";
+      String[] words = {"bar", "foo", "the"};
+      List<Integer> expected = new ArrayList<>();
+      expected.add(6);
+      expected.add(9);
+      expected.add(12);
+      List<Integer> actual = classObj.findSubstringOptimal(inputString, words);
+      Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
+   }
+
+   @Test
+   void test_findSubstringOptimal_expectOutputOfSize1_whenWordsSizeIs4AndWordSize4() {
+      String inputString = "wordgoodgoodgoodbestword";
+      String[] words = {"word", "good", "best", "good"};
+      List<Integer> expected = new ArrayList<>();
+      expected.add(8);
+      List<Integer> actual = classObj.findSubstringOptimal(inputString, words);
+      Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
+   }
 }
