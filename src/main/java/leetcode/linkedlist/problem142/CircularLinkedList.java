9231fd0b2edae2c684bb052f285cce682ad55902
23076 <23076@omnicell.com>
2025-05-25 19:05:45 +0530

    [revision]: sorting stack using recursion

--- src/main/java/leetcode/linkedlist/problem142/CircularLinkedList.java
+++ src/main/java/leetcode/linkedlist/problem142/CircularLinkedList.java
@@ -2,6 +2,21 @@ package leetcode.linkedlist.problem142;
 
 public class CircularLinkedList {
 
+    /**
+     Detect Loop (Cycle):
+     Use two pointers (slow and fast).
+     Move slow by one node and fast by two nodes.
+     If they meet, a loop exists.
+
+     Find the Start of the Loop:
+     Once a loop is detected (when slow == fast), move one pointer to the head of the list.
+     Keep the other at the meeting point.
+     Move both one step at a time. The point they meet again is the start of the loop.
+
+     Remove the Loop:
+     Once the start of the loop is found, traverse from there to find the node that links back to the loop start.
+     Set that node’s next to null to break the loop.
+     */
     public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) {
             return null;
