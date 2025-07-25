b609d7a712ccb522e52e4b9a3c072b9f1ba20380
23076 <23076@omnicell.com>
2025-07-25 10:12:41 +0530

    [revision]: brute force approach using sorting for finding first missing positive number

--- src/main/java/practice/arrays/FirstMissingPositiveNumber.java
+++ src/main/java/practice/arrays/FirstMissingPositiveNumber.java
@@ -1,9 +1,31 @@
 package practice.arrays;
 
+import java.util.Arrays;
+
 public class FirstMissingPositiveNumber {
 
-   public int firstMissingPositive(int[] nums) {
+   public int firstMissingPositiveUsingSorting(int[] nums) {
+      int min = 1;
+      int n = nums.length;
+      if (n == 1) {
+         return nums[0] == 1 ? 2 : 1;
+      }
+      Arrays.sort(nums);
+      for (int i = 0; i < n; i++) {
+         int current = nums[i];
+         if (current <= 0) {
+            continue;
+         }
+         if (i > 0 && current == nums[i - 1]) {
+            continue;
+         }
 
-      return 0;
+         if (current == min) {
+            min++;
+         } else if (current > min) {
+            return min;
+         }
+      }
+      return min;
    }
 }
--- src/test/java/practice/arrays/FirstMissingPositiveNumberTest.java
+++ src/test/java/practice/arrays/FirstMissingPositiveNumberTest.java
@@ -10,18 +10,36 @@ public class FirstMissingPositiveNumberTest {
    @Test
    void test_missingNumber_expect3_whenNIs3() {
       int[] input = {1, 2, 0};
-      Assertions.assertEquals(3, classObj.firstMissingPositive(input));
+      Assertions.assertEquals(3, classObj.firstMissingPositiveUsingSorting(input));
    }
 
    @Test
    void test_missingNumber_expect2_whenNIs4() {
       int[] input = {3, 4, -1, 1};
-      Assertions.assertEquals(2, classObj.firstMissingPositive(input));
+      Assertions.assertEquals(2, classObj.firstMissingPositiveUsingSorting(input));
    }
 
    @Test
    void test_missingNumber_expect1_whenNIs5() {
       int[] input = {7, 8, 9, 11, 12};
-      Assertions.assertEquals(1, classObj.firstMissingPositive(input));
+      Assertions.assertEquals(1, classObj.firstMissingPositiveUsingSorting(input));
+   }
+
+   @Test
+   void test_missingNumber_expect2_whenNIs1() {
+      int[] input = {1};
+      Assertions.assertEquals(2, classObj.firstMissingPositiveUsingSorting(input));
+   }
+
+   @Test
+   void test_missingNumber_expect1_whenNIs1() {
+      int[] input = {2};
+      Assertions.assertEquals(1, classObj.firstMissingPositiveUsingSorting(input));
+   }
+
+   @Test
+   void test_missingNumber_expect3_whenNIs2() {
+      int[] input = {2,1};
+      Assertions.assertEquals(3, classObj.firstMissingPositiveUsingSorting(input));
    }
 }
