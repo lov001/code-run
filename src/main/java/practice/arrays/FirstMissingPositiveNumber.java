49002221fd0f865da3c91107f7c206c6b45758c9
23076 <23076@omnicell.com>
2025-08-02 10:42:44 +0530

    [revision]: brute force approach using hashset and same array iteration

--- src/main/java/practice/arrays/FirstMissingPositiveNumber.java
+++ src/main/java/practice/arrays/FirstMissingPositiveNumber.java
@@ -1,6 +1,8 @@
 package practice.arrays;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FirstMissingPositiveNumber {
 
@@ -27,5 +29,40 @@ public class FirstMissingPositiveNumber {
          }
       }
       return min;
+   }
+
+   public int firstMissingPositiveUsingHashSet(int[] nums) {
+      int min = 1;
+      Set<Integer> integerSet = new HashSet<>();
+      for (int number : nums) {
+         if (number > 0) {
+            integerSet.add(number);
+         }
+      }
+      while (integerSet.contains(min)) {
+         min++;
+      }
+      return min;
+   }
+
+   public int firstMissingPositiveOptimal(int[] nums) {
+      int n = nums.length;
+      for (int i = 0; i < n; i++) {
+         if (nums[i] <= 0 || nums[i] > n) {
+            nums[i] = n + 1;
+         }
+      }
+      for (int i = 0; i < n; i++) {
+         int abs = Math.abs(nums[i]);
+         if (abs <= n) {
+            nums[abs - 1] = -Math.abs(nums[abs - 1]);
+         }
+      }
+      for (int i = 0; i < n; i++) {
+         if (nums[i] > 0) {
+            return i + 1;
+         }
+      }
+      return n + 1;
    }
 }
